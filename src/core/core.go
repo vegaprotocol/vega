dc28ca772668504326f5b4070558b1ad1af00061
Barnaby Mannerings <b.mannerings@gmail.com>
2018-03-13 12:42:58 +0000

    Put config wrapper in core.go

--- src/core/core.go
+++ src/core/core.go
@@ -10,10 +10,20 @@ type Vega struct {
 	orders  map[string]*matching.OrderEntry
 }
 
+type Config struct {
+	Matching matching.Config
+}
+
 func New(config Config) *Vega {
 	return &Vega{
 		config:  config,
 		markets: make(map[string]*matching.OrderBook),
 		orders:  make(map[string]*matching.OrderEntry),
+	}
+}
+
+func DefaultConfig() Config {
+	return Config{
+		Matching: matching.DefaultConfig(),
 	}
 }
