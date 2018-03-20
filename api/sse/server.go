dd6a61b9d27fc4d40cc088178c1087c784bf4074
Barnaby Mannerings <b.mannerings@gmail.com>
2018-03-20 23:18:36 +0000

    Use jsonpb to properly marshal msgs to JSON for the SSE

--- api/sse/server.go
+++ api/sse/server.go
@@ -1,7 +1,6 @@
 package sse
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,10 +8,12 @@ import (
 	"vega/proto"
 
 	sse "github.com/alexandrevicenzi/go-sse"
+	"github.com/golang/protobuf/jsonpb"
 )
 
 type Server struct {
 	server sse.Server
+	jsonConfig jsonpb.Marshaler
 }
 
 func NewServer(orderChan chan msg.Order, tradeChan chan msg.Trade) Server {
@@ -24,8 +25,12 @@ func NewServer(orderChan chan msg.Order, tradeChan chan msg.Trade) Server {
 				"Access-Control-Allow-Methods": "GET, OPTIONS",
 				"Access-Control-Allow-Headers": "Keep-Alive,X-Requested-With,Cache-Control,Content-Type,Last-Event-ID",
 			},
+		}),
+		jsonConfig: jsonpb.Marshaler{
+		 	EmitDefaults: true,
+		 	//EnumsAsInts: false,
 		},
-	)}
+	}
 	go s.handleOrders(orderChan)
 	go s.handleTrades(tradeChan)
 	return s
@@ -44,14 +49,14 @@ func (s *Server) Start() {
 
 func (s *Server) handleOrders(orders chan msg.Order) {
 	for order := range orders {
-		orderJson, _ := json.Marshal(order)
+		orderJson, _ := s.jsonConfig.MarshalToString(&order)
 		s.server.SendMessage("/events/orders", sse.SimpleMessage(string(orderJson)))
 	}
 }
 
 func (s *Server) handleTrades(trades chan msg.Trade) {
 	for trade := range trades {
-		tradeJson, _ := json.Marshal(trade)
+		tradeJson, _ := s.jsonConfig.MarshalToString(&trade)
 		s.server.SendMessage("/events/trades", sse.SimpleMessage(string(tradeJson)))
 	}
 }
