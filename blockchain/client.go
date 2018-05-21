f977432ade8e1099b5465c785782e30e9400f216
@cdm <chris.moores@gmail.com>
2018-05-21 17:23:37 +0100

    Small tidyup before merge to feature branch

--- blockchain/client.go
+++ blockchain/client.go
@@ -1,23 +1,8 @@
 package blockchain
 
-type HttpClient struct {
+type BlockchainClient struct {
 }
 
-func NewHttpClient() HttpClient {
-	return HttpClient{}
+func NewBlockchainClient() BlockchainClient {
+	return BlockchainClient{}
 }
-
-//func (c HttpClient) Send(nodeId int, tx models.Tx, stage string) {
-//	b := jsonize(tx)
-//	go http.Post(url(nodeId, stage), "application/json", b)
-//}
-//
-//func url(nodeId int, stage string) string {
-//	return fmt.Sprintf("http://localhost:808%d", nodeId) + "/consensus/" + stage
-//}
-
-//func jsonize(tx models.Tx) *bytes.Buffer {
-//	b := new(bytes.Buffer)
-//	json.NewEncoder(b).Encode(tx)
-//	return b
-//}
