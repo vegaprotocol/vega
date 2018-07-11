3675b91e74fdaf43f630eb160be220b72ee01848
Max Pachucki <maxpachucki@Maxs-MacBook.local>
2018-07-11 18:12:15 +0100

    log added to blockchain server

--- blockchain/server.go
+++ blockchain/server.go
@@ -1,7 +1,7 @@
 package blockchain
 
 import (
-	"fmt"
+	"vega/log"
 	"vega/core"
 
 	"github.com/tendermint/tendermint/abci/server"
@@ -10,7 +10,7 @@ import (
 
 // Starts up a Vega blockchain server.
 func Start(vega *core.Vega) error {
-	fmt.Println("Starting Vega blockchain socket...")
+	log.Infof("Starting Vega blockchain socket...")
 	blockchain := NewBlockchain(vega)
 	srv, err := server.NewServer("127.0.0.1:26658", "socket", blockchain)
 	if err != nil {
