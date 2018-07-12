bcd206b079ed43c1919dd9c6bc36970ce3639a44
Dan Garland <dan@dangarland.co.uk>
2018-07-12 16:14:08 +0100

    WIP currently the gateway is loading the wrong package path

--- scripts/gen_api.sh
+++ scripts/gen_api.sh
@@ -8,29 +8,35 @@ fi
 
 echo "Using ${GOPATH} for GOPATH"
 
-SERVICE_HOME=services/msg
-PROTO_DEF=$SERVICE_HOME/trading.proto
+for PROTO_DEF in `ls -R services/**/*.proto`
+do
+    echo "Generating $PROTO_DEF"
+    SERVICE_HOME=$(dirname "${PROTO_DEF}")
+    
+    # This first protoc command creates a gRPC stub:
+    # - All the protocol buffer code to populate, serialize, and retrieve our request and response message
+    # - An interface type (or stub) for clients to call with the methods defined in the RouteGuide service.
+    # - An interface type for servers to implement, also with the methods defined in the RouteGuide service.
+    protoc -I/usr/local/include -I. \
+	   -I$GOPATH/src \
+	   -Ivendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+	   --go_out=plugins=grpc,paths=source_relative:. \
+	   $PROTO_DEF
 
-# This first protoc command creates a gRPC stub:
-# - All the protocol buffer code to populate, serialize, and retrieve our request and response message
-# - An interface type (or stub) for clients to call with the methods defined in the RouteGuide service.
-# - An interface type for servers to implement, also with the methods defined in the RouteGuide service.
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -Ivendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --go_out=plugins=grpc:. \
-  $PROTO_DEF
+    if [ -e $SERVICE_HOME/rest-bindings.yml ]
+    then
+	# This creates a reverse proxy to forward HTTP requests into gRPC requests
+	protoc -I/usr/local/include -I. \
+	       -I$GOPATH/src \
+	       -Ivendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+	       --grpc-gateway_out=logtostderr=true,grpc_api_configuration=$SERVICE_HOME/rest-bindings.yml,import_path=foo:. \
+	       $PROTO_DEF
 
-# This creates a reverse proxy to forward HTTP requests into gRPC requests
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -Ivendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --grpc-gateway_out=logtostderr=true,grpc_api_configuration=$SERVICE_HOME/rest-bindings.yml:. \
-  $PROTO_DEF
-
-# Generates Swagger documentation
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -Ivendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --swagger_out=logtostderr=true,grpc_api_configuration=$SERVICE_HOME/rest-bindings.yml:. \
-  $PROTO_DEF
+	# Generates Swagger documentation
+	protoc -I/usr/local/include -I. \
+	       -I$GOPATH/src \
+	       -Ivendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+	       --swagger_out=logtostderr=true,grpc_api_configuration=$SERVICE_HOME/rest-bindings.yml:. \
+	       $PROTO_DEF
+    fi
+done
