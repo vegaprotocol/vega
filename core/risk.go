5992ffc5ad0bfe907b6a6a3c2a74e59086297b84
Dan Garland <dan@dangarland.co.uk>
2018-07-19 16:00:41 +0100

    Using a mock command to return 20

--- core/risk.go
+++ core/risk.go
@@ -1,11 +1,30 @@
 package core
 
-import "vega/msg"
+import (
+	"encoding/binary"
+	"os/exec"
+	"vega/msg"
+)
 
 type RiskEngine interface {
 	Assess(*msg.Order)
 }
 
-func Assess(order *msg.Order) {
-	order.RiskFactor = 20
+type riskEngine struct {
+	Command RiskCommand
+}
+
+type RiskCommand interface {
+	Output(command string, args ...string) ([]byte, error)
+}
+
+type ExecCommand struct{}
+
+func (ec ExecCommand) Output(command string, args ...string) ([]byte, error) {
+	return exec.Command(command, args...).Output()
+}
+
+func (re riskEngine) Assess(order *msg.Order) {
+	returnValue, _ := re.Command.Output("echo", "20")
+	order.RiskFactor, _ = binary.Uvarint(returnValue)
 }
--- core/risk_test.go
+++ core/risk_test.go
@@ -1,16 +1,32 @@
 package core
 
 import (
+	"encoding/binary"
 	"testing"
 	"vega/msg"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type MockCommand struct {
+	desiredValue string
+}
+
+func (mc MockCommand) Output(command string, args ...string) ([]byte, error) {
+	buf := make([]byte, binary.MaxVarintLen64)
+	binary.PutUvarint(buf, 20)
+	return buf, nil
+}
+
 func TestAssess(t *testing.T) {
+
+	riskEngine := &riskEngine{
+		Command: &MockCommand{},
+	}
+
 	order := &msg.Order{}
 
-	Assess(order)
+	riskEngine.Assess(order)
 
 	assert.Equal(t, uint64(20), order.RiskFactor)
 }
