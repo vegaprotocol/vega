bb8041f31bcec3da294565bdf1faf1a77577a840
cdm <chris.moores@gmail.com>
2018-08-06 16:54:01 +0100

    Tests for common query* WIP

---
+++ common/queryfilter_test.go
@@ -0,0 +1,16 @@
+package common
+
+import "testing"
+
+func TestQueryFilter_ApplyEqualFilter(t *testing.T) {
+	queryFilter := &QueryFilter{}
+	queryFilter.ApplyEqualFilter()
+}
+
+func TestQueryFilter_ApplyNotEqualFilter(t *testing.T) {
+}
+
+func TestQueryFilter_ApplyRangeFilter(t *testing.T) {
+	
+}
+
