132e1c6d7fc9847e6a0eb98a7ebd23ed46e42104
cdm <chris.moores@gmail.com>
2018-11-08 13:22:32 +0000

    Log size limit to 1GB on tendermint script

--- start-tendermint.sh
+++ start-tendermint.sh
@@ -2,4 +2,4 @@
 PATH=/usr/local/bin:/usr/bin:/bin
 mkdir -p ./log/tendermint
 chown -R vega:vega ./log/tendermint
-exec ./tendermint node 2>&1 | multilog t s10485760 n200 ./log/tendermint &
+exec ./tendermint node 2>&1 | multilog t s10485760 n100 ./log/tendermint &
