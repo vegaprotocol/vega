4122bbd7d6041b46f6983ee0bc24957cf0301e16
cdm <chris.moores@gmail.com>
2018-12-11 10:26:55 +0000

    Tidy up tests and add more comments for go-doc

--- datastore/orderstore.go
+++ datastore/orderstore.go
@@ -15,11 +15,11 @@ type OrderStore interface {
 	Subscribe(orders chan<- []msg.Order) uint64
 	Unsubscribe(id uint64) error
 
-	// Post adds an order to the store, with the ability
+	// Post adds an order to the store, adds
 	// to queue the operation to be committed later.
 	Post(order *msg.Order) error
 
-	// Put updates an order in the store, with the ability
+	// Put updates an order in the store, adds
 	// to queue the operation to be committed later.
 	Put(order *msg.Order) error
 
@@ -33,17 +33,18 @@ type OrderStore interface {
 
 	// GetByMarket retrieves all orders for a given Market.
 	GetByMarket(market string, filters *filters.OrderQueryFilters) ([]*msg.Order, error)
-	// Get retrieves an order for a given Market and id.
+	// GetByMarketAndId retrieves an order for a given Market and id.
 	GetByMarketAndId(market string, id string) (*msg.Order, error)
 	// GetByParty retrieves trades for a given party.
 	GetByParty(party string, filters *filters.OrderQueryFilters) ([]*msg.Order, error)
-	// Get retrieves a trade for a given id.
+	// GetByPartyAndId retrieves a trade for a given Party and id.
 	GetByPartyAndId(party string, id string) (*msg.Order, error)
-	// Returns Order Book Depth for a market
+	
+	// GetMarketDepth calculates and returns order book depth for a given market.
 	GetMarketDepth(market string) (*msg.MarketDepth, error)
 }
 
-// orderStore should implement OrderStore interface.
+// badgerOrderStore is a package internal data struct that implements the OrderStore interface.
 type badgerOrderStore struct {
 	badger         *badgerStore
 	subscribers    map[uint64]chan<- []msg.Order
@@ -53,6 +54,9 @@ type badgerOrderStore struct {
 	mu             sync.Mutex
 }
 
+// NewOrderStore is used to initialise and create a OrderStore, this implementation is currently
+// using the badger k-v persistent storage engine under the hood. The caller will specify a dir to
+// use as the storage location on disk for any stored files.
 func NewOrderStore(dir string) OrderStore {
 	db, err := badger.Open(customBadgerOptions(dir))
 	if err != nil {
@@ -67,6 +71,8 @@ func NewOrderStore(dir string) OrderStore {
 	}
 }
 
+// Subscribe to a stream of new or updated orders. The subscriber id will be returned as a uint64 value
+// and must be retained for future reference and to unsubscribe.
 func (os *badgerOrderStore) Subscribe(orders chan<- []msg.Order) uint64 {
 	os.mu.Lock()
 	defer os.mu.Unlock()
@@ -78,6 +84,7 @@ func (os *badgerOrderStore) Subscribe(orders chan<- []msg.Order) uint64 {
 	return os.subscriberId
 }
 
+// Unsubscribe from an orders stream. Provide the subscriber id you wish to stop receiving new events for.
 func (os *badgerOrderStore) Unsubscribe(id uint64) error {
 	os.mu.Lock()
 	defer os.mu.Unlock()
@@ -95,40 +102,16 @@ func (os *badgerOrderStore) Unsubscribe(id uint64) error {
 	return errors.New(fmt.Sprintf("OrderStore subscriber does not exist with id: %d", id))
 }
 
-func (os *badgerOrderStore) notify(items []msg.Order) error {
-	if len(items) == 0 {
-		return nil
-	}
-
-	if os.subscribers == nil || len(os.subscribers) == 0 {
-		log.Debugf("OrderStore: No subscribers connected")
-		return nil
-	}
-
-	var ok bool
-	for id, sub := range os.subscribers {
-		select {
-		case sub <- items:
-			ok = true
-			break
-		default:
-			ok = false
-		}
-		if ok {
-			log.Debugf("OrderStore: send on channel success for subscriber %d", id)
-		} else {
-			log.Infof("OrderStore: channel could not been updated for subscriber %d", id)
-		}
-	}
-	return nil
-}
-
+// Post adds an order to the badger store, adds
+// to queue the operation to be committed later.
 func (os *badgerOrderStore) Post(order *msg.Order) error {
-	// With badger we always buffer for future batch insert via Commit()
+	// with badger we always buffer for future batch insert via Commit()
 	os.addToBuffer(*order)
 	return nil
 }
 
+// Put updates an order in the badger store, adds
+// to queue the operation to be committed later.
 func (os *badgerOrderStore) Put(order *msg.Order) error {
 
 	var orderBeforeUpdate msg.Order
@@ -146,7 +129,7 @@ func (os *badgerOrderStore) Put(order *msg.Order) error {
 	}
 
 	if !recordExistsInBuffer {
-		// We tried to update a record that is not in our buffer, validate it exists
+		// we tried to update a record that is not in our buffer, validate it exists
 		// with a read transaction lookup and add to write queue if exists
 		if order.Status != msg.Order_Cancelled && order.Status != msg.Order_Expired {
 			o, err := os.GetByMarketAndId(order.Market, order.Id)
@@ -167,6 +150,8 @@ func (os *badgerOrderStore) Put(order *msg.Order) error {
 	return nil
 }
 
+// Commit saves any operations that are queued to badger store, and includes all updates.
+// It will also call notify() to push updated data to any subscribers.
 func (os *badgerOrderStore) Commit() error {
 	if len(os.buffer) == 0 {
 		return nil
@@ -188,12 +173,14 @@ func (os *badgerOrderStore) Commit() error {
 	return nil
 }
 
+// Close our connection to the badger database
+// ensuring errors will be returned up the stack.
 func (os *badgerOrderStore) Close() error {
-	// Close our connection to the badger database
-	// ensuring errors will be returned up the stack.
 	return os.badger.db.Close()
 }
 
+// GetByMarket retrieves all orders for a given Market. Provide optional query filters to
+// refine the data set further (if required), any errors will be returned immediately.
 func (os *badgerOrderStore) GetByMarket(market string, queryFilters *filters.OrderQueryFilters) ([]*msg.Order, error) {
 	var result []*msg.Order
 	if queryFilters == nil {
@@ -228,6 +215,7 @@ func (os *badgerOrderStore) GetByMarket(market string, queryFilters *filters.Ord
 	return result, nil
 }
 
+// GetByMarketAndId retrieves an order for a given Market and id, any errors will be returned immediately.
 func (os *badgerOrderStore) GetByMarketAndId(market string, id string) (*msg.Order, error) {
 	var order msg.Order
 
@@ -247,6 +235,8 @@ func (os *badgerOrderStore) GetByMarketAndId(market string, id string) (*msg.Ord
 	return &order, nil
 }
 
+// GetByParty retrieves trades for a given party. Provide optional query filters to
+// refine the data set further (if required), any errors will be returned immediately.
 func (os *badgerOrderStore) GetByParty(party string, queryFilters *filters.OrderQueryFilters) ([]*msg.Order, error) {
 	var result []*msg.Order
 
@@ -287,6 +277,7 @@ func (os *badgerOrderStore) GetByParty(party string, queryFilters *filters.Order
 	return result, nil
 }
 
+// GetByPartyAndId retrieves a trade for a given Party and id, any errors will be returned immediately.
 func (os *badgerOrderStore) GetByPartyAndId(party string, id string) (*msg.Order, error) {
 	var order msg.Order
 
@@ -322,12 +313,14 @@ func (os *badgerOrderStore) GetByPartyAndId(party string, id string) (*msg.Order
 	return &order, nil
 }
 
+// GetMarketDepth calculates and returns order book depth for a given market.
 func (os *badgerOrderStore) GetMarketDepth(market string) (*msg.MarketDepth, error) {
-	// get from store, recalculate accumulated volume and respond
+	// load from store
 	buy := os.orderBookDepth.getBuySide()
 	sell := os.orderBookDepth.getSellSide()
 
 	// recalculate accumulated volume
+	// --- buy side ---
 	for idx := range buy {
 		if idx == 0 {
 			buy[idx].CumulativeVolume = buy[idx].Volume
@@ -335,7 +328,7 @@ func (os *badgerOrderStore) GetMarketDepth(market string) (*msg.MarketDepth, err
 		}
 		buy[idx].CumulativeVolume = buy[idx-1].CumulativeVolume + buy[idx].Volume
 	}
-
+	// --- sell side ---
 	for idx := range sell {
 		if idx == 0 {
 			sell[idx].CumulativeVolume = sell[idx].Volume
@@ -344,17 +337,48 @@ func (os *badgerOrderStore) GetMarketDepth(market string) (*msg.MarketDepth, err
 		sell[idx].CumulativeVolume = sell[idx-1].CumulativeVolume + sell[idx].Volume
 	}
 
+	// return new re-calculated market depth for each side of order book
 	orderBookDepth := msg.MarketDepth{Name: market, Buy: buy, Sell: sell}
-
 	return &orderBookDepth, nil
 }
 
+// add an order to the write-batch/notify buffer.
 func (os *badgerOrderStore) addToBuffer(o msg.Order) {
 	os.mu.Lock()
 	os.buffer = append(os.buffer, o)
 	os.mu.Unlock()
 }
 
+// notify any subscribers of order updates.
+func (os *badgerOrderStore) notify(items []msg.Order) error {
+	if len(items) == 0 {
+		return nil
+	}
+
+	if os.subscribers == nil || len(os.subscribers) == 0 {
+		log.Debugf("OrderStore: No subscribers connected")
+		return nil
+	}
+
+	var ok bool
+	for id, sub := range os.subscribers {
+		select {
+		case sub <- items:
+			ok = true
+			break
+		default:
+			ok = false
+		}
+		if ok {
+			log.Debugf("OrderStore: send on channel success for subscriber %d", id)
+		} else {
+			log.Infof("OrderStore: channel could not been updated for subscriber %d", id)
+		}
+	}
+	return nil
+}
+
+// writeBatch flushes a batch of orders (create/update) to the underlying badger store.
 func (os *badgerOrderStore) writeBatch(batch []msg.Order) error {
 
 	wb := os.badger.db.NewWriteBatch()
@@ -383,16 +407,18 @@ func (os *badgerOrderStore) writeBatch(batch []msg.Order) error {
 	}
 	if err := insertBatchAtomically(); err == nil {
 		if err := wb.Flush(); err != nil {
-			log.Errorf("failed to flush batch %+v \n", err)
+			// todo: can we handle flush errors in a similar way to below?
+			log.Errorf("failed to flush batch: %s", err)
 		}
 	} else {
 		wb.Cancel()
-		// TODO: implement retry mechanism
+		// todo: retry mechanism, also handle badger txn too large errors
+		log.Errorf("failed to insert order batch atomically, %s", err)
 		return nil
 	}
 
+	// update order book depth
 	for idx := range batch {
-		// Update orderBookDepth
 		if batch[idx].Remaining != uint64(0) {
 			os.orderBookDepth.Add(&batch[idx])
 		}
@@ -401,6 +427,8 @@ func (os *badgerOrderStore) writeBatch(batch []msg.Order) error {
 	return nil
 }
 
+// OrderFilter is the order specific filter query data holder. It includes the raw filters
+// and helper methods that are used internally to apply and track filter state.
 type OrderFilter struct {
 	queryFilter *filters.OrderQueryFilters
 	skipped     uint64
@@ -440,3 +468,4 @@ func (f *OrderFilter) isFull() bool {
 	}
 	return false
 }
+
