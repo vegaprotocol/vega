29700938ad16c1e45e435f43e5388cac2b7595b4
Ashley C <ashley@vegaprotocol.io>
2019-03-15 11:39:27 +0000

    Fix race in internal/monitoring/status (#198)

--- internal/monitoring/status.go
+++ internal/monitoring/status.go
@@ -2,6 +2,7 @@ package monitoring
 
 import (
 	"context"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -22,6 +23,7 @@ type ChainStatus struct {
 	log      *logging.Logger
 	interval time.Duration
 	client   blockchain.Client
+	clientMu sync.Mutex
 	status   uint32
 	cancel   func()
 }
@@ -29,13 +31,14 @@ type ChainStatus struct {
 // NewStatusChecker creates a Status checker, currently this is limited to the underlying blockchain status, but
 // will be expanded over time to include other services. Once created, a go-routine will start up and
 // immediately begin checking at an interval, currently defined internally and set to every 500 milliseconds.
-func NewStatusChecker(log *logging.Logger, clt blockchain.Client) *Status {
+func NewStatusChecker(log *logging.Logger, clt blockchain.Client, interval time.Duration) *Status {
 	ctx, cancel := context.WithCancel(context.Background())
 	s := &Status{
 		log: log,
 		Blockchain: &ChainStatus{
-			interval: 500 * time.Millisecond,
+			interval: interval, // 500 * time.Millisecond,
 			client:   clt,
+			clientMu: sync.Mutex{},
 			status:   uint32(types.ChainStatus_DISCONNECTED),
 			cancel:   cancel,
 			log:      log,
@@ -57,8 +60,48 @@ func (cs *ChainStatus) Status() types.ChainStatus {
 	return types.ChainStatus(atomic.LoadUint32(&cs.status))
 }
 
+func (cs *ChainStatus) SetClient(clt blockchain.Client) {
+	cs.clientMu.Lock()
+	defer cs.clientMu.Unlock()
+
+	cs.client = clt
+}
+
 func (cs *ChainStatus) setStatus(status types.ChainStatus) {
 	atomic.StoreUint32(&cs.status, uint32(status))
+}
+
+func (cs *ChainStatus) tick(status types.ChainStatus) types.ChainStatus {
+	cs.clientMu.Lock()
+	defer cs.clientMu.Unlock()
+	newStatus := status
+	_, err := cs.client.Health()
+	if status == types.ChainStatus_DISCONNECTED && err == nil {
+		// node is connected, now let's check if we are replaying
+		res, err2 := cs.client.GetStatus(context.Background())
+		if err2 != nil {
+			// error while getting the status, maybe we are not
+			// really connected, let's not change the status then
+			return status
+		}
+		if res.SyncInfo.CatchingUp {
+			newStatus = types.ChainStatus_REPLAYING
+		} else {
+			newStatus = types.ChainStatus_CONNECTED
+		}
+	} else if status == types.ChainStatus_CONNECTED && err != nil {
+		newStatus = types.ChainStatus_DISCONNECTED
+	}
+
+	if status == newStatus {
+		return status
+	}
+
+	cs.setStatus(newStatus)
+	cs.log.Debug("Blockchain status updated",
+		logging.String("status-old", status.String()),
+		logging.String("status-new", newStatus.String()))
+	return newStatus
 }
 
 // Calling start ideally by go-routine will start periodic checking at interval specified by config
@@ -69,34 +112,7 @@ func (cs *ChainStatus) start(ctx context.Context) {
 	for {
 		select {
 		case <-ticker.C:
-			oldStatus := currentStatus
-			_, err := cs.client.Health()
-			if currentStatus == types.ChainStatus_DISCONNECTED && err == nil {
-				// node is connected, now let's check if we are replaying
-				res, err := cs.client.GetStatus(context.Background())
-				if err != nil {
-					// error while getting the status, maybe we are not
-					// really connected, let's not change the status then
-					continue
-				}
-				if res.SyncInfo.CatchingUp {
-					currentStatus = types.ChainStatus_REPLAYING
-					cs.setStatus(types.ChainStatus_REPLAYING)
-					continue
-				}
-				currentStatus = types.ChainStatus_CONNECTED
-				cs.setStatus(types.ChainStatus_CONNECTED)
-			} else if currentStatus == types.ChainStatus_CONNECTED && err != nil {
-				currentStatus = types.ChainStatus_DISCONNECTED
-				cs.setStatus(types.ChainStatus_DISCONNECTED)
-			} else {
-				// no status changes
-				continue
-			}
-
-			cs.log.Debug("Blockchain status updated",
-				logging.String("status-old", oldStatus.String()),
-				logging.String("status-new", currentStatus.String()))
+			currentStatus = cs.tick(currentStatus)
 
 		case <-ctx.Done():
 			return
