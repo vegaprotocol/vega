c4dd5850dcf0463fd1040162fdb4107b6ffb78a6
Elias Van Ootegem <eliasvo@gmail.com>
2019-03-26 17:37:53 +0000

    Ensure filtering by market/party works

--- internal/orders/observe_orders_test.go
+++ internal/orders/observe_orders_test.go
@@ -12,6 +12,7 @@ import (
 
 func TestObserveOrders(t *testing.T) {
 	t.Run("Observe orders - all markets/parties success", testObserveAllOrdersSuccess)
+	t.Run("Observe orders - some markets/parties success", testObservePartialSuccess)
 }
 
 func testObserveAllOrdersSuccess(t *testing.T) {
@@ -57,4 +58,48 @@ func testObserveAllOrdersSuccess(t *testing.T) {
 	for i := range orders {
 		assert.Equal(t, orders[i], gotOrders[i])
 	}
+}
+
+func testObservePartialSuccess(t *testing.T) {
+	svc := getTestService(t)
+	defer svc.ctrl.Finish()
+	ctx, cfunc := context.WithCancel(context.Background())
+	wg := sync.WaitGroup{}
+	done := make(chan struct{})
+	subRef := uint64(1)
+	market, party := "market1", "party1"
+	orders := []proto.Order{
+		{
+			Id:     "order_id1",
+			Market: "market1",
+			Party:  "party1",
+		},
+		{
+			Id:     "order_id2",
+			Market: "market2",
+			Party:  "party2",
+		},
+	}
+
+	wg.Add(1)
+	subscriber := func(ch chan<- []proto.Order) {
+		defer wg.Done()
+		ch <- orders
+	}
+	svc.orderStore.EXPECT().Subscribe(gomock.Any()).Times(1).Return(subRef).Do(func(ch chan<- []proto.Order) {
+		go subscriber(ch)
+	})
+	svc.orderStore.EXPECT().Unsubscribe(subRef).Times(1).Return(nil).Do(func(_ uint64) {
+		done <- struct{}{}
+	})
+	// all orders
+	ch, ref := svc.svc.ObserveOrders(ctx, 0, &market, &party)
+	assert.Equal(t, subRef, ref)
+	gotOrders := <-ch
+
+	wg.Wait()
+	cfunc() // cancel context
+	<-done
+	assert.Equal(t, 1, len(gotOrders))
+	assert.Equal(t, orders[0], gotOrders[0])
 }
