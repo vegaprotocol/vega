53b8d12badb236d78839c022cc2feea8549c86ed
Elias Van Ootegem <eliasvo@gmail.com>
2019-05-03 18:29:46 +0100

    We cannot filter out positions that match trade price, fix positions tests

--- internal/engines/position/position_test.go
+++ internal/engines/position/position_test.go
@@ -1,6 +1,7 @@
 package position_test
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -13,7 +14,7 @@ import (
 )
 
 func TestUpdatePosition(t *testing.T) {
-	ch := make(chan settlement.MarketPosition, 1)
+	ch := make(chan settlement.MarketPosition, 2)
 	engine := getTestEngine(t)
 	assert.Empty(t, engine.Positions())
 	buyer := "buyer_id"
@@ -30,11 +31,22 @@ func TestUpdatePosition(t *testing.T) {
 		SellOrder: "sell_order_id",
 		Timestamp: time.Now().Unix(),
 	}
+	wg := sync.WaitGroup{}
+	positions := make([]settlement.MarketPosition, 0, 2)
+	wg.Add(1)
+	go func() {
+		for p := range ch {
+			positions = append(positions, p)
+		}
+		wg.Done()
+	}()
 	engine.Update(&trade, ch)
 	close(ch)
+	wg.Wait()
 	assert.Empty(t, ch)
 	pos := engine.Positions()
 	assert.Equal(t, 2, len(pos))
+	assert.Equal(t, 2, len(positions))
 	for _, p := range pos {
 		if p.Party() == buyer {
 			assert.Equal(t, size, p.Size())
