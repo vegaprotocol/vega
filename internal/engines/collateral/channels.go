6b89fc51658a133eac4689dee6777a920ecba8b9
Elias Van Ootegem <eliasvo@gmail.com>
2019-05-16 16:12:05 +0100

    Add test coverage for channel flow

--- internal/engines/collateral/channels.go
+++ internal/engines/collateral/channels.go
@@ -8,6 +8,10 @@ import (
 	types "code.vegaprotocol.io/vega/proto"
 )
 
+// generate this mock so we can write tests more easilyh
+//go:generate go run github.com/golang/mock/mockgen -destination mocks/mtm_transfer_mock.go -package mocks code.vegaprotocol.io/vega/internal/engines/collateral MTMTransfer
+type MTMTransfer events.MTMTransfer
+
 type processF func(t *transferT) (*types.TransferResponse, error)
 
 type collectF func(t *transferT) error
