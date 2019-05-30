112512425859c3c21d4ac30176b3facb1f41501e
Elias Van Ootegem <eliasvo@gmail.com>
2019-05-30 15:02:05 +0100

    Use badger for accounts storage

--- internal/storage/accounts.go
+++ internal/storage/accounts.go
@@ -1,13 +1,12 @@
 package storage
 
 import (
-	"sync"
-
 	"code.vegaprotocol.io/vega/internal/logging"
 	types "code.vegaprotocol.io/vega/proto"
 
+	"github.com/dgraph-io/badger"
+	"github.com/gogo/protobuf/proto"
 	"github.com/pkg/errors"
-	uuid "github.com/satori/go.uuid"
 )
 
 var (
@@ -20,6 +19,7 @@ var (
 
 const (
 	SystemOwner = "system"
+	NoMarket    = "general"
 )
 
 type accountRecord struct {
@@ -30,11 +30,8 @@ type accountRecord struct {
 type Account struct {
 	Config
 
-	log           *logging.Logger
-	mu            *sync.RWMutex
-	byMarketOwner map[string]map[string][]*accountRecord
-	byOwner       map[string][]*accountRecord
-	byID          map[string]*accountRecord
+	log    *logging.Logger
+	badger *badgerStore
 }
 
 func NewAccounts(log *logging.Logger, c Config) (*Account, error) {
@@ -42,13 +39,18 @@ func NewAccounts(log *logging.Logger, c Config) (*Account, error) {
 	log = log.Named(namedLogger)
 	log.SetLevel(c.Level.Get())
 
+	if err := InitStoreDirectory(c.AccountStoreDirPath); err != nil {
+		return nil, errors.Wrap(err, "error on init badger database for account storage")
+	}
+	db, err := badger.Open(badgerOptionsFromConfig(c.BadgerOptions, c.AccountStoreDirPath, log))
+	if err != nil {
+		return nil, errors.Wrap(err, "error opening badger database for account storage")
+	}
+
 	return &Account{
-		log:           log,
-		Config:        c,
-		mu:            &sync.RWMutex{},
-		byMarketOwner: map[string]map[string][]*accountRecord{},
-		byOwner:       map[string][]*accountRecord{},
-		byID:          map[string]*accountRecord{},
+		log:    log,
+		Config: c,
+		badger: &badgerStore{db: db},
 	}, nil
 }
 
@@ -67,78 +69,148 @@ func (a *Account) ReloadConf(cfg Config) {
 
 // Create an account, adds in all lists simultaneously
 func (a *Account) Create(rec *types.Account) error {
-	// default to new ID
-	if rec.Id == "" {
-		rec.Id = uuid.NewV4().String()
+	records, err := a.createAccountRecords(rec)
+	if err != nil {
+		return err
 	}
-	a.mu.Lock()
-	if _, ok := a.byID[rec.Id]; ok {
-		a.mu.Unlock()
-		return ErrDuplicateAccount
+	if _, err := a.badger.writeBatch(records); err != nil {
+		a.log.Error(
+			"Failed to create the given account",
+			logging.String("account-id", rec.Id),
+			logging.Error(err),
+		)
+		return err
 	}
-	cpy := *rec
-	// pass a copy, avoid working on the argument from caller directly
-	a.createAccount(&cpy)
-	a.mu.Unlock()
 	return nil
 }
 
-// internal create function, assumes mutex is locked correctly by caller
-func (a *Account) createAccount(cpy *types.Account) {
-	rec := &accountRecord{
-		Account: cpy,
+func (a *Account) createAccounts(accounts ...*types.Account) error {
+	records, err := a.createAccountRecords(accounts...)
+	if err != nil {
+		return err
 	}
-	a.byID[rec.Id] = rec
-	if _, ok := a.byOwner[rec.Owner]; !ok {
-		a.byOwner[rec.Owner] = []*accountRecord{}
+	if _, err := a.badger.writeBatch(records); err != nil {
+		a.log.Error(
+			"Failed to create accounts",
+			logging.Error(err),
+		)
+		return err
 	}
-	// use an embedded type here to keep track of this
-	rec.ownerIdx = len(a.byOwner[rec.Owner])
-	a.byOwner[rec.Owner] = append(a.byOwner[rec.Owner], rec)
-	if rec.MarketID != "" {
-		if _, ok := a.byMarketOwner[rec.MarketID]; !ok {
-			a.byMarketOwner[rec.MarketID] = map[string][]*accountRecord{
-				rec.Owner: []*accountRecord{},
-			}
-		}
-		if _, ok := a.byMarketOwner[rec.MarketID][rec.Owner]; !ok {
-			a.byMarketOwner[rec.MarketID][rec.Owner] = []*accountRecord{}
-		}
-		a.byMarketOwner[rec.MarketID][rec.Owner] = append(a.byMarketOwner[rec.MarketID][rec.Owner], rec)
-	}
+	return nil
 }
 
-// CreateMarketIDAccounts - shortcut to quickly add the system balances for a market
+func (a *Account) hasAccount(acc *types.Account) (bool, error) {
+	market := acc.MarketID
+	if market == "" {
+		market = NoMarket
+	}
+	key := a.badger.accountKey(acc.Owner, acc.Asset, market, acc.Type)
+	err := a.badger.db.View(func(txn *badger.Txn) error {
+		account, err := txn.Get(key)
+		if err != nil {
+			return err
+		}
+		buf, err := account.ValueCopy(nil)
+		if err != nil {
+			return err
+		}
+		if err := proto.Unmarshal(buf, acc); err != nil {
+			a.log.Error(
+				"Failed to unmarshal account",
+				logging.Error(err),
+				logging.String("badger-key", string(key)),
+				logging.String("raw-bytes", string(buf)))
+			return err
+		}
+		return nil
+	})
+	// no errors, so key exists, and we got the account
+	if err == nil {
+		return true, nil
+	}
+	// key not found, account doesn't exist
+	if err == badger.ErrKeyNotFound {
+		return false, nil
+	}
+	// something went wrong
+	return false, err
+}
+
+func (a *Account) createAccountRecords(accounts ...*types.Account) (map[string][]byte, error) {
+	m := make(map[string][]byte, len(accounts)*4) // each account has its key + 1 reference key, so map == nr of accounts * 2
+	for _, acc := range accounts {
+		// for general accounts, a market isn't specified
+		market := acc.MarketID
+		if market == "" {
+			market = NoMarket
+		}
+		accKey := a.badger.accountKey(
+			acc.Owner, acc.Asset, market, acc.Type,
+		)
+		acc.Id = string(accKey)
+		refKey := a.badger.accountReferenceKey(
+			acc.Owner, market, acc.Asset, acc.Type,
+		)
+		mrefKey := a.badger.accountMarketReferenceKey(
+			market, acc.Owner, acc.Asset, acc.Type,
+		)
+		trefKey := a.badger.accountTypeReferenceKey(
+			acc.Owner, market, acc.Asset, acc.Type,
+		)
+		buf, err := proto.Marshal(acc)
+		if err != nil {
+			a.log.Error("unable to marshal account",
+				logging.String("account-id", acc.Id),
+				logging.Error(err),
+			)
+			return nil, err
+		}
+		// id is the key here
+		m[acc.Id] = buf
+		// reference key points to actual ID
+		m[string(refKey)] = accKey
+		m[string(mrefKey)] = accKey
+		m[string(trefKey)] = accKey
+	}
+	return m, nil
+}
+
 func (a *Account) CreateMarketAccounts(market string, insuranceBalance int64) error {
-	owner := SystemOwner
-	a.mu.Lock()
-	// add market entry, but do not set system accounts here, yet... ensure they don't exist yet
-	if _, ok := a.byMarketOwner[market]; !ok {
-		a.byMarketOwner[market] = map[string][]*accountRecord{}
-	}
-	if _, ok := a.byMarketOwner[market][owner]; ok {
-		a.mu.Unlock()
-		return ErrMarketAccountsExist
-	}
-	a.byMarketOwner[market][owner] = []*accountRecord{}
+	// all market accounts that the system should have available to it
 	accounts := []*types.Account{
 		{
+			Owner:    SystemOwner,
 			MarketID: market,
-			Owner:    owner,
+			Asset:    string(market[:3]),
 			Type:     types.AccountType_INSURANCE,
 			Balance:  insuranceBalance,
 		},
 		{
+			Owner:    SystemOwner,
+			MarketID: NoMarket,
+			Asset:    string(market[:3]),
+			Type:     types.AccountType_GENERAL,
+		},
+		{
+			Owner:    SystemOwner,
 			MarketID: market,
-			Owner:    owner,
+			Asset:    string(market[:3]),
 			Type:     types.AccountType_SETTLEMENT,
 		},
 	}
+	create := make([]*types.Account, 0, len(accounts))
 	for _, acc := range accounts {
-		a.createAccount(acc)
+		ok, err := a.hasAccount(acc)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			create = append(create, acc)
+		}
 	}
-	// we can unlock here, we've set the byMarketIDOwner keys, duplicates are impossible
-	a.mu.Unlock()
+	if len(create) > 0 {
+		return a.createAccounts(create...)
+	}
 	return nil
 }
 
@@ -148,164 +220,182 @@ func (a *Account) CreateTraderMarketAccounts(owner, market string) error {
 	// does this trader actually have any accounts yet?
 	accounts := []*types.Account{
 		{
-			Id:       uuid.NewV4().String(),
 			MarketID: market,
 			Owner:    owner,
+			Asset:    string(market[:3]),
 			Type:     types.AccountType_MARKET,
 		},
 		{
-			Id:       uuid.NewV4().String(),
 			MarketID: market,
+			Asset:    string(market[:3]),
 			Owner:    owner,
 			Type:     types.AccountType_MARGIN,
 		},
+		{
+			MarketID: NoMarket,
+			Owner:    owner,
+			Asset:    string(market[:3]),
+			Type:     types.AccountType_GENERAL,
+		},
 	}
-	a.mu.Lock()
-	if _, ok := a.byOwner[owner]; !ok {
-		// add general + margin account for trader
-		accounts = append(
-			accounts,
-			&types.Account{
-				Id:    uuid.NewV4().String(),
-				Owner: owner,
-				Type:  types.AccountType_GENERAL,
-			},
-		)
-	}
+	create := make([]*types.Account, 0, len(accounts))
 	for _, acc := range accounts {
-		a.createAccount(acc)
+		ok, err := a.hasAccount(acc)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			// no errors returned by check, no account found
+			create = append(create, acc)
+		}
 	}
-	a.mu.Unlock()
 	return nil
 }
 
 func (a *Account) GetMarketAccounts(market string) ([]*types.Account, error) {
-	a.mu.RLock()
-	byOwner, ok := a.byMarketOwner[market]
-	if !ok {
-		a.mu.RUnlock()
-		return nil, ErrMarketNotFound
-	}
-	accounts := make([]*types.Account, 0, len(a.byMarketOwner)*2) // each owner has 2 accounts -> for market, and margin, system has 2 (insurance + settlement)
-	for owner, records := range byOwner {
-		// this shouldn't be possible, but you never know
-		if len(records) == 0 {
-			continue
-		}
-		// system accounts are appended as they are
-		if owner == SystemOwner {
-			for _, r := range records {
-				cpy := *r.Account
-				accounts = append(accounts, &cpy)
-			}
-			continue
-		}
-		var mTrader *types.Account
-		// there should only be 1 here
-		for _, r := range records {
-			if r.Type == types.AccountType_MARKET {
-				cpy := *r.Account
-				mTrader = &cpy
-				break
-			}
-		}
-		if mTrader == nil {
-			continue
-		}
-		accounts = append(accounts, mTrader)
-		// get margin account
-		ownerAcc := a.byOwner[owner]
-		for _, acc := range ownerAcc {
-			if acc.Type == types.AccountType_MARGIN {
-				cpy := *acc.Account
-				accounts = append(accounts, &cpy)
-				break
-			}
-		}
-	}
-	a.mu.RUnlock()
-	return accounts, nil
+	keyPrefix, validFor := a.badger.accountMarketPrefix(market, false)
+	return a.getByReference(keyPrefix, validFor, 0)
 }
 
 func (a *Account) GetMarketAccountsForOwner(market, owner string) ([]*types.Account, error) {
-	a.mu.RLock()
-	owners, ok := a.byMarketOwner[market]
-	if !ok {
-		a.mu.RUnlock()
-		return nil, ErrMarketNotFound
-	}
-	records, ok := owners[owner]
-	if !ok {
-		a.mu.RUnlock()
-		return nil, ErrOwnerNotFound
-	}
-	accounts := make([]*types.Account, 0, 2) // there's always 2 accounts given the market + owner
-	// system owner -> copy both, non-system, there's only 1
-	for _, record := range records {
-		cpy := *record.Account
-		accounts = append(accounts, &cpy)
-	}
-	a.mu.RUnlock()
-	return accounts, nil
+	keyPrefix, validFor := a.badger.accountReferencePrefix(owner, market, false)
+	// an owner will have 3 accounts in a market at most, or a multiple thereof (based on assets), so cap of 3 is sensible
+	return a.getByReference(keyPrefix, validFor, 3)
 }
 
 func (a *Account) GetAccountsForOwner(owner string) ([]*types.Account, error) {
-	a.mu.RLock()
-	acc, ok := a.byOwner[owner]
-	if !ok {
-		a.mu.RUnlock()
-		return nil, ErrOwnerNotFound
-	}
-	ret := make([]*types.Account, 0, len(acc))
-	for _, r := range acc {
-		cpy := *r.Account
-		ret = append(ret, &cpy)
-	}
-	a.mu.RUnlock()
-	return ret, nil
+	keyPrefix, validFor := a.badger.accountOwnerPrefix(owner, false)
+	// again, cap of 3 is reasonable: 3 per asset, per market, regardless of system/trader ownership
+	return a.getByReference(keyPrefix, validFor, 3)
 }
 
 func (a *Account) GetAccountsForOwnerByType(owner string, accType types.AccountType) ([]*types.Account, error) {
-	a.mu.RLock()
-	acc, ok := a.byOwner[owner]
-	if !ok {
-		a.mu.RUnlock()
-		return nil, ErrOwnerNotFound
-	}
-	var ret []*types.Account
-	for _, ac := range acc {
-		if ac.Type == accType {
-			cpy := *ac.Account
-			ret = append(ret, &cpy)
+	keyPrefix, validFor := a.badger.accountTypePrefix(owner, accType, false)
+	return a.getByReference(keyPrefix, validFor, 0)
+}
+
+func (a *Account) getByReference(prefix, validFor []byte, capacity int) ([]*types.Account, error) {
+	var err error
+	ret := make([]*types.Account, 0, capacity)
+	txn := a.badger.readTransaction()
+	defer txn.Discard()
+	it := a.badger.getIterator(txn, false)
+	defer it.Close()
+	keyBuf, accountBuf := []byte{}, []byte{}
+	for it.Seek(prefix); it.ValidForPrefix(validFor); it.Next() {
+		if keyBuf, err = it.Item().ValueCopy(keyBuf); err != nil {
+			return nil, err
 		}
-	}
-	a.mu.RUnlock()
-	if len(ret) == 0 {
-		return nil, ErrAccountNotFound
+		item, err := txn.Get(keyBuf)
+		if err != nil {
+			return nil, err
+		}
+		if accountBuf, err = item.ValueCopy(accountBuf); err != nil {
+			return nil, err
+		}
+		var acc types.Account
+		if err := proto.Unmarshal(accountBuf, &acc); err != nil {
+			a.log.Error(
+				"Failed to unmarshal account",
+				logging.String("account-id", string(keyBuf)),
+				logging.Error(err),
+			)
+			return nil, err
+		}
+		ret = append(ret, &acc)
 	}
 	return ret, nil
 }
 
 func (a *Account) UpdateBalance(id string, balance int64) error {
-	a.mu.Lock()
-	acc, ok := a.byID[id]
-	if !ok {
-		a.mu.Unlock()
-		return ErrAccountNotFound
+	txn := a.badger.writeTransaction()
+	defer txn.Discard()
+	var account []byte
+	acc, err := a.getAccountByID(txn, id)
+	// internal func does the logging already
+	if err != nil {
+		return err
 	}
+	// update balance
 	acc.Balance = balance
-	a.mu.Unlock()
-	return nil
+	// can't see how this would fail to marshal, but best check...
+	if account, err = proto.Marshal(acc); err != nil {
+		a.log.Error(
+			"Failed to marshal valid account record",
+			logging.Error(err),
+		)
+		return err
+	}
+	if err = txn.Set([]byte(id), account); err != nil {
+		a.log.Error(
+			"Failed to save updated account balance",
+			logging.String("account-id", id),
+			logging.Error(err),
+		)
+		return err
+	}
+	return txn.Commit()
 }
 
 func (a *Account) IncrementBalance(id string, inc int64) error {
-	a.mu.Lock()
-	acc, ok := a.byID[id]
-	if !ok {
-		a.mu.Unlock()
-		return ErrAccountNotFound
+	txn := a.badger.writeTransaction()
+	defer txn.Discard()
+	var account []byte
+	acc, err := a.getAccountByID(txn, id)
+	if err != nil {
+		return err
 	}
+	// increment balance
 	acc.Balance += inc
-	a.mu.Unlock()
-	return nil
+	if account, err = proto.Marshal(acc); err != nil {
+		a.log.Error(
+			"Failed to marshal account record",
+			logging.Error(err),
+			logging.String("account-id", id),
+		)
+		return err
+	}
+	if err = txn.Set([]byte(id), account); err != nil {
+		a.log.Error(
+			"Failed to update account balance",
+			logging.String("account-id", id),
+			logging.Int64("account-balance", acc.Balance),
+			logging.Error(err),
+		)
+		return err
+	}
+	return txn.Commit()
+}
+
+func (a *Account) getAccountByID(txn *badger.Txn, id string) (*types.Account, error) {
+	if txn == nil {
+		// default to read txn if no txn was provided
+		txn = a.badger.readTransaction()
+		defer txn.Discard()
+	}
+	item, err := txn.Get([]byte(id))
+	if err != nil {
+		a.log.Error(
+			"Failed to get account by ID",
+			logging.String("account-id", id),
+			logging.Error(err),
+		)
+		return nil, err
+	}
+	account, err := item.ValueCopy(nil)
+	if err != nil {
+		a.log.Error("Failed to get value for account item", logging.Error(err))
+		return nil, err
+	}
+	var acc types.Account
+	if err := proto.Unmarshal(account, &acc); err != nil {
+		a.log.Error(
+			"Failed to unmarshal account",
+			logging.String("account-id", id),
+			logging.String("account-raw", string(account)),
+			logging.Error(err),
+		)
+		return nil, err
+	}
+	return &acc, nil
 }
