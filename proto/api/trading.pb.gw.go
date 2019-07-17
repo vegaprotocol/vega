8e4961479cc09c866076174f5d70a760d3b197e1
Ashley Chaloner <ashley@vegaprotocol.io>
2019-07-17 21:44:44 +0000

    Add NotifyTraderAccount to REST (#372)

--- proto/api/trading.pb.gw.go
+++ proto/api/trading.pb.gw.go
@@ -133,6 +133,23 @@ func request_Trading_SignIn_0(ctx context.Context, marshaler runtime.Marshaler, 
 
 }
 
+func request_Trading_NotifyTraderAccount_0(ctx context.Context, marshaler runtime.Marshaler, client TradingClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq NotifyTraderAccountRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.NotifyTraderAccount(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 var (
 	filter_TradingData_OrdersByMarket_0 = &utilities.DoubleArray{Encoding: map[string]int{"marketID": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
@@ -662,6 +679,26 @@ func RegisterTradingHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 
 	})
 
+	mux.Handle("POST", pattern_Trading_NotifyTraderAccount_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Trading_NotifyTraderAccount_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Trading_NotifyTraderAccount_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -673,6 +710,8 @@ var (
 	pattern_Trading_AmendOrder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"orders", "amendment.orderID"}, ""))
 
 	pattern_Trading_SignIn_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"auth", "token"}, ""))
+
+	pattern_Trading_NotifyTraderAccount_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"fountain"}, ""))
 )
 
 var (
@@ -683,6 +722,8 @@ var (
 	forward_Trading_AmendOrder_0 = runtime.ForwardResponseMessage
 
 	forward_Trading_SignIn_0 = runtime.ForwardResponseMessage
+
+	forward_Trading_NotifyTraderAccount_0 = runtime.ForwardResponseMessage
 )
 
 // RegisterTradingDataHandlerFromEndpoint is same as RegisterTradingDataHandler but
