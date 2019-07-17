0fabc9164f2943b93f4217c75ea2a9db170c3eed
Ashley C <ashley@vegaprotocol.io>
2019-07-17 16:05:32 +0100

    Remove dead code

--- internal/settlement/position.go
+++ internal/settlement/position.go
@@ -10,11 +10,7 @@ import (
 type pos struct {
 	party string
 	size  int64
-	// These are pointers because positions are updated after having beeng passed
-	// to engines further down the trade pipeline, and we want the data to remain
-	// up to date.
-	buy, sell *int64
-	price     uint64
+	price uint64
 }
 
 type mtmTransfer struct {
@@ -24,8 +20,6 @@ type mtmTransfer struct {
 
 func newPos(partyID string) *pos {
 	return &pos{
-		buy:   new(int64),
-		sell:  new(int64),
 		party: partyID,
 	}
 }
@@ -38,16 +32,6 @@ func (p pos) Party() string {
 // Size - part of the MarketPosition interface, used to update position after SettlePreTrade
 func (p pos) Size() int64 {
 	return p.size
-}
-
-// Buy - part of the MarketPosition interface, used to update position after SettlePreTrade
-func (p pos) Buy() int64 {
-	return *p.buy
-}
-
-// Sell - part of the MarketPosition interface, used to update position after SettlePreTrade
-func (p pos) Sell() int64 {
-	return *p.sell
 }
 
 // Price - part of the MarketPosition interface, used to update position after SettlePreTrade
