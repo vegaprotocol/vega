837979d2b95a78e785220d4130eb7c3a9ea76f97
Ashley C <ashley@vegaprotocol.io>
2019-07-23 11:20:40 +0100

    Implement unmarshalling of FileLoadingMode

--- internal/config/encoding/encoding.go
+++ internal/config/encoding/encoding.go
@@ -55,16 +55,28 @@ func (m *FileLoadingMode) Get() options.FileLoadingMode {
 	return m.FileLoadingMode
 }
 
-// UnmarshalText maps a string to a FileLoadingMode enum value
-func (m *FileLoadingMode) UnmarshalText(text []byte) error {
-	m.FileLoadingMode = options.FileIO
-	return nil
-}
-
 var (
 	// ErrCouldNotMarshal is to be used when marshalling failed
 	ErrCouldNotMarshal = errors.New("could not marshal value to string")
+
+	// ErrCouldNotUnmarshal is to be used when unmarshalling failed
+	ErrCouldNotUnmarshal = errors.New("could not unmarshal value from string")
 )
+
+// UnmarshalText maps a string to a FileLoadingMode enum value
+func (m *FileLoadingMode) UnmarshalText(text []byte) error {
+	switch string(text) {
+	case "FileIO":
+		m.FileLoadingMode = options.FileIO
+	case "LoadToRAM":
+		m.FileLoadingMode = options.LoadToRAM
+	case "MemoryMap":
+		m.FileLoadingMode = options.MemoryMap
+	default:
+		return ErrCouldNotUnmarshal
+	}
+	return nil
+}
 
 // MarshalText maps a FileLoadingMode enum value to a string
 func (m FileLoadingMode) MarshalText() ([]byte, error) {
