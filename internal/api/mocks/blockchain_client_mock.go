29105dfab272e899c2fe75ac66be7ee9971abea2
Ashley Chaloner <ashley@vegaprotocol.io>
2019-08-08 13:58:43 +0000

    API: Add check on MarketID during SubmitOrder (#356)

--- internal/api/mocks/blockchain_client_mock.go
+++ internal/api/mocks/blockchain_client_mock.go
@@ -5,6 +5,7 @@
 package mocks
 
 import (
+	proto "code.vegaprotocol.io/vega/proto"
 	context "context"
 	gomock "github.com/golang/mock/gomock"
 	types "github.com/tendermint/tendermint/rpc/core/types"
@@ -33,6 +34,51 @@ func NewMockBlockchainClient(ctrl *gomock.Controller) *MockBlockchainClient {
 // EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockBlockchainClient) EXPECT() *MockBlockchainClientMockRecorder {
 	return m.recorder
+}
+
+// AmendOrder mocks base method
+func (m *MockBlockchainClient) AmendOrder(arg0 context.Context, arg1 *proto.OrderAmendment) (bool, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "AmendOrder", arg0, arg1)
+	ret0, _ := ret[0].(bool)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// AmendOrder indicates an expected call of AmendOrder
+func (mr *MockBlockchainClientMockRecorder) AmendOrder(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AmendOrder", reflect.TypeOf((*MockBlockchainClient)(nil).AmendOrder), arg0, arg1)
+}
+
+// CancelOrder mocks base method
+func (m *MockBlockchainClient) CancelOrder(arg0 context.Context, arg1 *proto.Order) (bool, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CancelOrder", arg0, arg1)
+	ret0, _ := ret[0].(bool)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// CancelOrder indicates an expected call of CancelOrder
+func (mr *MockBlockchainClientMockRecorder) CancelOrder(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CancelOrder", reflect.TypeOf((*MockBlockchainClient)(nil).CancelOrder), arg0, arg1)
+}
+
+// CreateOrder mocks base method
+func (m *MockBlockchainClient) CreateOrder(arg0 context.Context, arg1 *proto.Order) (*proto.PendingOrder, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CreateOrder", arg0, arg1)
+	ret0, _ := ret[0].(*proto.PendingOrder)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// CreateOrder indicates an expected call of CreateOrder
+func (mr *MockBlockchainClientMockRecorder) CreateOrder(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateOrder", reflect.TypeOf((*MockBlockchainClient)(nil).CreateOrder), arg0, arg1)
 }
 
 // GetGenesisTime mocks base method
@@ -65,6 +111,21 @@ func (mr *MockBlockchainClientMockRecorder) GetNetworkInfo(arg0 interface{}) *go
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetNetworkInfo", reflect.TypeOf((*MockBlockchainClient)(nil).GetNetworkInfo), arg0)
 }
 
+// GetStatus mocks base method
+func (m *MockBlockchainClient) GetStatus(arg0 context.Context) (*types.ResultStatus, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetStatus", arg0)
+	ret0, _ := ret[0].(*types.ResultStatus)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetStatus indicates an expected call of GetStatus
+func (mr *MockBlockchainClientMockRecorder) GetStatus(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetStatus", reflect.TypeOf((*MockBlockchainClient)(nil).GetStatus), arg0)
+}
+
 // GetUnconfirmedTxCount mocks base method
 func (m *MockBlockchainClient) GetUnconfirmedTxCount(arg0 context.Context) (int, error) {
 	m.ctrl.T.Helper()
@@ -78,4 +139,34 @@ func (m *MockBlockchainClient) GetUnconfirmedTxCount(arg0 context.Context) (int,
 func (mr *MockBlockchainClientMockRecorder) GetUnconfirmedTxCount(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetUnconfirmedTxCount", reflect.TypeOf((*MockBlockchainClient)(nil).GetUnconfirmedTxCount), arg0)
+}
+
+// Health mocks base method
+func (m *MockBlockchainClient) Health() (*types.ResultHealth, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Health")
+	ret0, _ := ret[0].(*types.ResultHealth)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Health indicates an expected call of Health
+func (mr *MockBlockchainClientMockRecorder) Health() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Health", reflect.TypeOf((*MockBlockchainClient)(nil).Health))
+}
+
+// NotifyTraderAccount mocks base method
+func (m *MockBlockchainClient) NotifyTraderAccount(arg0 context.Context, arg1 *proto.NotifyTraderAccount) (bool, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "NotifyTraderAccount", arg0, arg1)
+	ret0, _ := ret[0].(bool)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// NotifyTraderAccount indicates an expected call of NotifyTraderAccount
+func (mr *MockBlockchainClientMockRecorder) NotifyTraderAccount(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "NotifyTraderAccount", reflect.TypeOf((*MockBlockchainClient)(nil).NotifyTraderAccount), arg0, arg1)
 }
