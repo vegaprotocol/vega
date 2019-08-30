285b007aa21867b3ff63e0283644aed08599b9a5
Ashley Chaloner <ashley@vegaprotocol.io>
2019-08-30 08:25:01 +0000

    Add pipeline step to diff GraphQL schema (#417)

--- script/deploy.sh
+++ script/deploy.sh
@@ -17,6 +17,10 @@
 # Optional vars:
 # - "$SLACK_HOOK_URL" for sending Slack notifications
 
+pushd "$(realpath "$(dirname "$0")")" 1>/dev/null || exit 1
+source bash_functions.sh
+popd 1>/dev/null || exit 1
+
 check_apps() {
 	# Check required programs
 	apps=( rsync scp ssh )
@@ -130,30 +134,6 @@ traders_action() {
 	esac
 }
 
-json_escape() {
-	echo -n "$1" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'
-}
-
-slack_notify() {
-	slack_hook_url="${SLACK_HOOK_URL:-}"
-	if test -z "$slack_hook_url" ; then
-		return
-	fi
-	if ! which curl 1>/dev/null ; then
-		return
-	fi
-	channel="${1:-tradingcore-notify}"
-	icon_emoji="${2:-:thinking-face:}"
-	text="${3:-A slack notification}"
-	# Escape text in preparation for inclusion in JSON payload
-	channel="$(json_escape "$channel")"
-	icon_emoji="$(json_escape "$icon_emoji")"
-	text="$(json_escape "$text")"
-	curl -XPOST --silent -H 'Content-Type: application/json' \
-		--data '{"channel": '"$channel"', "icon_emoji": '"$icon_emoji"', "text": '"$text"', "username": "Autodeploy"}' \
-		"$slack_hook_url" >/dev/null
-}
-
 ssh_setup() {
 	eval "$(ssh-agent -s)"
 
@@ -216,7 +196,7 @@ success() {
 	if test "$gitlab_ci" == "true" ; then
 		commit_hash="${CI_COMMIT_SHORT_SHA:-[failed to get commit hash]}"
 		commit_msg="${CI_COMMIT_TITLE:-[failed to get commit message]}"
-		pipeline_url="${CI_PIPELINE_URL:-[failed to get piepline URL]}"
+		pipeline_url="${CI_PIPELINE_URL:-[failed to get pipeline URL]}"
 		msg="\`$net\` has been deployed at \`$commit_hash\` \"$commit_msg\" (see $pipeline_url for details)."
 	else
 		msg="\`$net\` has been deployed."
