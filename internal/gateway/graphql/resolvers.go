ce0fb83ab9c1c5a2d62cdbdd43031da481a489df
jeremyletang <letang.jeremy@gmail.com>
2019-09-18 16:12:10 +0100

    set OrderType to be nullable so FE is not broken

--- internal/gateway/graphql/resolvers.go
+++ internal/gateway/graphql/resolvers.go
@@ -823,7 +823,7 @@ type MyMutationResolver resolverRoot
 
 func (r *MyMutationResolver) OrderSubmit(ctx context.Context, market string, party string,
 	price string, size string, side Side, timeInForce OrderTimeInForce, expiration *string,
-	ty OrderType) (*types.PendingOrder, error) {
+	ty *OrderType) (*types.PendingOrder, error) {
 
 	order := &types.OrderSubmission{}
 
@@ -857,8 +857,16 @@ func (r *MyMutationResolver) OrderSubmit(ctx context.Context, market string, par
 	if order.Side, err = parseSide(&side); err != nil {
 		return nil, err
 	}
-	if order.Type, err = parseOrderType(ty); err != nil {
-		return nil, err
+	// TODO(jeremy): at the moment the ty parameter is maybe null as having a nullable gql
+	// field here ease the transition to set order type that was not required before.
+	// In the future this field will be non-nullable.
+	// In the meantime default to the old behaviour (LIMIT)
+	if ty == nil {
+		order.Type = types.Order_LIMIT
+	} else {
+		if order.Type, err = parseOrderType(*ty); err != nil {
+			return nil, err
+		}
 	}
 
 	// GTT must have an expiration value
