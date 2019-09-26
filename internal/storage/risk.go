58f898d3064926f0fd10974414e029d402625c46
jeremyletang <letang.jeremy@gmail.com>
2019-09-26 18:42:49 +0100

    run golint over risks / storage risk

--- internal/storage/risk.go
+++ internal/storage/risk.go
@@ -21,17 +21,17 @@ func (r *Risk) ReloadConf(config Config) {
 }
 
 // Post saves a given risk factor to the mem-store.
-func (ms *Risk) Post(risk *types.RiskFactor) error {
+func (r *Risk) Post(risk *types.RiskFactor) error {
 	return nil
 }
 
 // Put updates a given risk factor to the mem-store.
-func (ms *Risk) Put(risk *types.RiskFactor) error {
+func (r *Risk) Put(risk *types.RiskFactor) error {
 	return nil
 }
 
 // GetByMarket searches for the risk factors relating to the market param in the mem-store.
-func (ms *Risk) GetByMarket(name string) (*types.RiskFactor, error) {
+func (r *Risk) GetByMarket(name string) (*types.RiskFactor, error) {
 	return &types.RiskFactor{
 		Market: name,
 		Long:   1,
@@ -41,14 +41,14 @@ func (ms *Risk) GetByMarket(name string) (*types.RiskFactor, error) {
 
 // Commit typically saves any operations that are queued to underlying storage,
 // if supported by underlying storage implementation.
-func (ms *Risk) Commit() error {
+func (r *Risk) Commit() error {
 	// No work required with a mem-store implementation.
 	return nil
 }
 
 // Close can be called to clean up and close any storage
 // connections held by the underlying storage mechanism.
-func (ms *Risk) Close() error {
+func (r *Risk) Close() error {
 	// No work required with a mem-store implementation.
 	return nil
 }
