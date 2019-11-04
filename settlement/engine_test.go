1554edf423a82d1afe4fb13e8d691a82731ef77b
C Moores <chris.moores@gmail.com>
2019-11-04 15:19:06 +0000

    Remove ListenClosed function

--- settlement/engine_test.go
+++ settlement/engine_test.go
@@ -45,7 +45,7 @@ func testSettleExpiredSuccess(t *testing.T) {
 	engine := getTestEngine(t)
 	defer engine.Finish()
 	// these are mark prices, product will provide the actual value
-	data := []posValue{ // {{{2
+	data := []posValue{
 		{
 			trader: "trader1",
 			price:  1000,
@@ -87,7 +87,7 @@ func testSettleExpiredSuccess(t *testing.T) {
 			},
 			Type: types.TransferType_WIN,
 		},
-	} // }}}
+	}
 	oraclePrice := uint64(1100)
 	settleF := func(price uint64, size int64) (*types.FinancialAmount, error) {
 		sp := int64((oraclePrice - price)) * size
@@ -149,10 +149,12 @@ func testMarkToMarketEmpty(t *testing.T) {
 	pos.EXPECT().Party().MaxTimes(2).Return(data.trader)
 	pos.EXPECT().Size().MaxTimes(2).Return(data.size)
 	pos.EXPECT().Price().MaxTimes(2).Return(markPrice)
-	ch := make(chan events.MarketPosition, 1)
-	engine.ListenClosed(ch)
-	ch <- pos
-	close(ch)
+
+	//ch := make(chan events.MarketPosition, 1)
+	//engine.ListenClosed(ch)
+	//ch <- pos
+	//close(ch)
+
 	result := engine.SettleOrder(markPrice, []events.MarketPosition{pos})
 	assert.Empty(t, result)
 }
@@ -160,6 +162,7 @@ func testMarkToMarketEmpty(t *testing.T) {
 func testMarkToMarketOrdered(t *testing.T) {
 	engine := getTestEngine(t)
 	defer engine.Finish()
+
 	positions := []posValue{
 		{
 			price:  10000,
@@ -172,6 +175,7 @@ func testMarkToMarketOrdered(t *testing.T) {
 			trader: "trader2",
 		},
 	}
+
 	markPrice := uint64(10000 + 1000)
 	init := make([]settlement.MarketPosition, 0, 3)
 	long := make([]events.MarketPosition, 0, 3)
@@ -199,25 +203,29 @@ func testMarkToMarketOrdered(t *testing.T) {
 		short = append(short, s)
 	}
 	engine.Update(init) // setup the initial state
-	ch := make(chan events.MarketPosition, len(long))
-	engine.ListenClosed(ch)
-	for _, l := range long {
-		ch <- l
-	}
-	close(ch)
+
+	//ch := make(chan events.MarketPosition, len(long))
+	//engine.ListenClosed(ch)
+	//for _, l := range long {
+	//	ch <- l
+	//}
+	//close(ch)
+
 	// add neutral to position, this hasn't changed, but we need it processed anyway
 	long = append(long, neutral)
 	longTransfer := engine.SettleOrder(markPrice, long)
 	assert.NotEmpty(t, longTransfer)
-	t.Logf("%#v\n", longTransfer)
+	//t.Logf("%#v\n", longTransfer)
 	// now, let's update the state again as if the settlement hasn't happened
 	engine.Update(init)
-	ch = make(chan events.MarketPosition, len(short))
-	engine.ListenClosed(ch)
-	for _, s := range short {
-		ch <- s
-	}
-	close(ch)
+
+	//ch = make(chan events.MarketPosition, len(short))
+	//engine.ListenClosed(ch)
+	//for _, s := range short {
+	//	ch <- s
+	//}
+	//close(ch)
+
 	short = append(short, neutral)
 	shortTransfer := engine.SettleOrder(markPrice, short)
 	assert.NotEmpty(t, shortTransfer)
@@ -249,18 +257,18 @@ func testMTMSwitchPosition(t *testing.T) {
 			price:  10000,
 		},
 	}
-	update := []posValue{
-		{
-			trader: "trader1",
-			size:   -1,
-			price:  11000,
-		},
-		{
-			trader: "trader2",
-			size:   1,
-			price:  11000,
-		},
-	}
+	//update := []posValue{
+	//	{
+	//		trader: "trader1",
+	//		size:   -1,
+	//		price:  11000,
+	//	},
+	//	{
+	//		trader: "trader2",
+	//		size:   1,
+	//		price:  11000,
+	//	},
+	//}
 	final := []posValue{
 		{
 			trader: "trader1",
@@ -284,22 +292,24 @@ func testMTMSwitchPosition(t *testing.T) {
 		},
 	}
 	init, _ := engine.getMockMarketPositions(start)
-	_, change := engine.getMockMarketPositions(update)
+	//_, change := engine.getMockMarketPositions(update)
 	_, positions := engine.getMockMarketPositions(final)
+
 	// set the initial state
 	engine.Update(init)
-	ch := make(chan events.MarketPosition, len(update))
-	engine.ListenClosed(ch)
-	for _, c := range change {
-		ch <- c
-	}
-	close(ch)
+
+	//ch := make(chan events.MarketPosition, len(update))
+	//engine.ListenClosed(ch)
+	//for _, c := range change {
+	//	ch <- c
+	//}
+	//close(ch)
+
 	result := engine.SettleOrder(final[0].price, positions)
 	assert.NotEmpty(t, result)
 	assert.Equal(t, 3, len(result)) // one for each trader with an open position
 }
 
-// {{{
 func (te *testEngine) getExpiryPositions(positions ...posValue) []settlement.MarketPosition {
 	te.positions = make([]*mocks.MockMarketPosition, 0, len(positions))
 	mpSlice := make([]settlement.MarketPosition, 0, len(positions))
@@ -402,6 +412,4 @@ func getTestEngine(t *testing.T) *testEngine {
 		positions: nil,
 		market:    market,
 	}
-} // }}}
-
-//  vim: set ts=4 sw=4 tw=0 foldlevel=1 foldmethod=marker noet :
+}
