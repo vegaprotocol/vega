c08139fbe8ada0726a8d5eb40f70d4f7863a2be0
Witold Gawlikowicz <witold@vega.xyz>
2019-11-08 14:29:38 +0000

    Remove reundant variable

--- storage/trades.go
+++ storage/trades.go
@@ -153,9 +153,8 @@ func (ts *Trade) GetByMarket(ctx context.Context, market string, skip, limit uin
 	var (
 		err error
 	)
-	//TODO: (WG 05/11/2019): Bug: Setting limit to maximum value of uint64 causes results in l=-1
-	l := int(limit)
-	result := make([]*types.Trade, 0, l)
+	//TODO: (WG 05/11/2019): Bug: Setting limit to maximum value of uint64 results in l=-1
+	result := make([]*types.Trade, 0, int(limit))
 
 	ctx, cancel := context.WithTimeout(ctx, ts.Config.Timeout.Duration)
 	defer cancel()
