a4e8ce20a826761db069a2bc7aefe10ca12dccfa
Elias Van Ootegem <eliasvo@gmail.com>
2019-11-20 17:22:39 +0000

    remove dead config

--- collat/config.go
+++ collat/config.go
@@ -12,15 +12,12 @@ const namedLogger = "collateral"
 // Config represent the configuration of the collateral engine
 type Config struct {
 	Level encoding.LogLevel
-	// Auto-create trader accounts if needed?
-	TraderGeneralAccountBalance int64
 }
 
 // NewDefaultConfig creates an instance of the package specific configuration, given a
 // pointer to a logger instance to be used for logging within the package.
 func NewDefaultConfig() Config {
 	return Config{
-		Level:                       encoding.LogLevel{Level: logging.InfoLevel},
-		TraderGeneralAccountBalance: 100000000,
+		Level: encoding.LogLevel{Level: logging.InfoLevel},
 	}
 }
