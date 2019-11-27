46248e7c0a90ed3f6090cb3dbbef328661b224d9
Ashley C <ashley@vega.xyz>
2019-11-27 23:29:44 +0000

    Revert "Revert "Revert "Remove test gql schema content (#552)"""
    This reverts commit f0f04036fa7cedd1da261c7ff893aeb810633565.

--- gateway/graphql/generated.go
+++ gateway/graphql/generated.go
@@ -2657,6 +2657,11 @@ enum AccountType {
   # General account - the account containing "unused" collateral for traders
   General
 }
+
+enum extraEnumToTestIssue552__DO_NOT_MERGE__ {
+  Aaa
+  Bbb
+}
 `},
 )
 
