75ce9db88cc7178cf72674b969f28bac2bf38d7b
Ashley C <ashley@vega.xyz>
2019-11-29 16:30:21 +0000

    Use script for prto generation (#528)

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,3 +1,43 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
-protoc --go_out=. *.proto
+# --go_out:            Generate *.pb.go
+# --govalidators_out:  Generate *.validator.pb.go
+# --grpc-gateway_out:  Generate *.pb.gw.go
+# --swagger_out:       Generate *.swagger.json
+
+paths="paths=source_relative"
+
+find proto -maxdepth 1 -name '*.proto' | sort | while read -r protofile
+do
+	protoc \
+		-I. \
+		-Iproto \
+		-Ivendor \
+		-Ivendor/github.com/google/protobuf/src \
+		--go_out=plugins="grpc,$paths:." \
+		--govalidators_out="$paths:." \
+		"$protofile"
+done
+
+grpc_api_configuration="grpc_api_configuration=gateway/rest/grpc-rest-bindings.yml"
+
+find proto/api -maxdepth 1 -name '*.proto' | sort | while read -r protofile
+do
+	protoc \
+		-I. \
+		-Iproto \
+		-Ivendor \
+		-Ivendor/github.com/google/protobuf/src \
+		--go_out="plugins=grpc,$paths:." \
+		--govalidators_out="$paths:." \
+		--grpc-gateway_out="logtostderr=true,$grpc_api_configuration,$paths:." \
+		--swagger_out="logtostderr=true,$grpc_api_configuration:." \
+		"$protofile"
+done
+
+# Make *.validator.pb.go files deterministic.
+find proto -name '*.validator.pb.go' | sort | while read -r pbfile
+do
+	sed -i -re 's/this\.Size_/this.Size/' "$pbfile" \
+		&& ./script/fix_imports.sh "$pbfile"
+done
