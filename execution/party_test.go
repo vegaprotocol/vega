30958d209c6cbb6e367f20aa5abdac5837d1b295
C <chris.moores@gmail.com>
2019-12-02 16:23:37 +0000

    Adding extra coverage to execution party operations

---
+++ execution/party_test.go
@@ -0,0 +1,23 @@
+package execution
+
+import "testing"
+
+func TestNewParty(t *testing.T) {
+	t.Fail()
+}
+
+func TestParty_GetByMarket(t *testing.T) {
+	t.Fail()
+}
+
+func TestParty_GetByMarketAndID(t *testing.T) {
+	t.Fail()
+}
+
+func TestParty_NotifyTraderAccount(t *testing.T) {
+	t.Fail()
+}
+
+func TestParty_NotifyTraderAccountWithTopUpAmount(t *testing.T) {
+	t.Fail()
+}
