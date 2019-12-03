032a3a308819fd4399b7b45cada4efd0658694dd
Ashley C <ashley@vega.xyz>
2019-12-03 12:15:30 +0000

    Build GraphQL API docs (#566)

--- apidoc/build.sh
+++ apidoc/build.sh
@@ -3,8 +3,35 @@
 # When running on Netlify, can only use tools mentioned in:
 # https://github.com/netlify/build-image/blob/xenial/included_software.md
 
+generate_index() {
+	cat >"$dest_dir/index.html" <<EOZ
+<!DOCTYPE html>
+<html>
+<head>
+<title>Vega API Documentation</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body { margin: 0; padding: 0; }
+</style>
+</head>
+<body>
+<ul>
+<li><a href="/grpc/">gRPC</a></li>
+<li><a href="/graphql/">GraphQL</a></li>
+<li><a href="/rest/">REST</a></li>
+</ul>
+</body>
+</html>
+EOZ
+}
+
 generate_graphql() {
-	echo TBD
+	graphql_dir="$dest_dir/graphql"
+	mkdir -p "$graphql_dir"
+	pushd apidoc 1>/dev/null || exit 1
+	yarn install
+	yarn build
+	popd 1>/dev/null || exit 1
 }
 
 generate_grpc() {
@@ -43,5 +70,8 @@ dest_dir=apidoc/public
 rm -rf "$dest_dir"
 mkdir -p "$dest_dir"
 
+generate_index
+
+generate_graphql
 generate_grpc
 generate_rest
