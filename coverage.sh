cd8eea85e17be76438f8604958efd490e9786f3b
C <chris.moores@gmail.com>
2019-12-11 21:10:58 +0000

    Add double-quotes

--- coverage.sh
+++ coverage.sh
@@ -9,8 +9,8 @@ if ls $dir/*.go &> /dev/null; then
     go test -short -covermode=count -coverprofile=$dir/profile.tmp $dir
     if [ -f $dir/profile.tmp ]
     then
-        cat $dir/profile.tmp | tail -n +2 >> profile.cov
-        rm $dir/profile.tmp
+        cat "$dir/profile.tmp" | tail -n +2 >> profile.cov
+        rm "$dir/profile.tmp"
     fi
 fi
 done
