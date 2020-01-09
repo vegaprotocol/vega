604cf1f99a9cf9cc71ec62ec54b995666e3a623f
Andriy Pastoshchuk <andriy@vegaprotocol.io>
2020-01-09 13:53:33 +0000

    Correct invalid delete order function

--- matching/orderbook.go
+++ matching/orderbook.go
@@ -312,7 +312,13 @@ func (b *OrderBook) SubmitOrder(order *types.Order) (*types.OrderConfirmation, e
 
 // DeleteOrder remove a given order on a given side from the book
 func (b *OrderBook) DeleteOrder(order *types.Order) error {
-	if err := b.buy.RemoveOrder(order); err != nil {
+	var sideBook *OrderBookSide
+	if order.Side == types.Side_Buy {
+		sideBook = b.buy
+	} else {
+		sideBook = b.sell
+	}
+	if err := sideBook.RemoveOrder(order); err != nil {
 		b.log.Error("Failed to remove order",
 			logging.Order(*order),
 			logging.Error(err),
