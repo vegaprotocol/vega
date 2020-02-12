f0bdd9b3b8ea1296d9d9f908b4c4c1fb67297c16
jeremyletang <letang.jeremy@gmail.com>
2020-02-12 20:19:01 +0000

    fix tests, move from base64 to hex

--- wallet/wallet_marshal_test.go
+++ wallet/wallet_marshal_test.go
@@ -12,7 +12,7 @@ import (
 func TestMarshalWallet(t *testing.T) {
 	w := New("jeremy")
 	w.Keypairs = append(w.Keypairs, NewKeypair(crypto.NewEd25519(), []byte{1, 2, 3, 4}, []byte{4, 3, 2, 1}))
-	expected := `{"Owner":"jeremy","Keypairs":[{"Pub":"AQIDBA==","Priv":"BAMCAQ==","Algorithm":"ed25519","Tainted":false,"Meta":null}]}`
+	expected := `{"Owner":"jeremy","Keypairs":[{"Pub":"01020304","Priv":"04030201","Algorithm":"ed25519","Tainted":false,"Meta":null}]}`
 	m, err := json.Marshal(&w)
 	assert.NoError(t, err)
 	assert.Equal(t, expected, string(m))
@@ -20,7 +20,7 @@ func TestMarshalWallet(t *testing.T) {
 
 func TestUnMarshalWallet(t *testing.T) {
 	w := Wallet{}
-	marshalled := `{"Owner":"jeremy","Keypairs":[{"Pub":"AQIDBA==","Priv":"BAMCAQ==","Algorithm":"ed25519","Tainted":false,"Meta":null}]}`
+	marshalled := `{"Owner":"jeremy","Keypairs":[{"Pub":"01020304","Priv":"04030201","Algorithm":"ed25519","Tainted":false,"Meta":null}]}`
 	err := json.Unmarshal([]byte(marshalled), &w)
 	assert.NoError(t, err)
 	assert.Len(t, w.Keypairs, 1)
@@ -31,7 +31,7 @@ func TestUnMarshalWallet(t *testing.T) {
 
 func TestUnMarshalWalletErrorInvalidAlgorithm(t *testing.T) {
 	w := Wallet{}
-	marshalled := `{"Owner":"jeremy","Keypairs":[{"Pub":"AQIDBA==","Priv":"BAMCAQ==","Algorithm":"notanalgorithm","Tainted":false,"Meta":null}]}`
+	marshalled := `{"Owner":"jeremy","Keypairs":[{"Pub":"01020304","Priv":"04030201","Algorithm":"notanalgorithm","Tainted":false,"Meta":null}]}`
 	err := json.Unmarshal([]byte(marshalled), &w)
 	assert.EqualError(t, err, crypto.ErrUnsupportedSignatureAlgorithm.Error())
 }
