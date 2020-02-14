a2498d86cb52c10902c855b3e858052455a8d0a1
Pete Barrow <pete@vega.xyz>
2020-02-14 16:39:20 +0000

    Testing market order prices

--- matching/helpers_test.go
+++ matching/helpers_test.go
@@ -25,7 +25,7 @@ func getTestOrderBook(t *testing.T, market string) *tstOB {
 	tob := tstOB{
 		log: logging.NewTestLogger(),
 	}
-	tob.OrderBook = NewOrderBook(tob.log, NewDefaultConfig(), market, 100.0)
+	tob.OrderBook = NewOrderBook(tob.log, NewDefaultConfig(), market, 100)
 	return &tob
 }
 
