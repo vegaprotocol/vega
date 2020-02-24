fac1ea247bdcd2adcc804039766f3ecc5073a057
Ashley C <ashley@vega.xyz>
2020-02-24 15:57:38 +0000

    Remove dup generation of protot validators (#671)

--- proto/generate.sh
+++ proto/generate.sh
@@ -77,7 +77,6 @@ do
 		-Ivendor \
 		-Ivendor/github.com/google/protobuf/src \
 		--go_out="plugins=grpc,$paths:." \
-		--govalidators_out="$paths:." \
 		--grpc-gateway_out="logtostderr=true,$grpc_api_configuration,$paths:." \
 		"$protofile"
 done
