31cccdf2d61fc56b72ac2c8e6a2ce3b32a959ba0
Elias Van Ootegem <eliasvo@gmail.com>
2020-03-03 18:05:35 +0000

    update swagger

--- proto/api/trading.pb.gw.go
+++ proto/api/trading.pb.gw.go
@@ -1225,7 +1225,7 @@ var (
 
 	pattern_Trading_SubmitTransaction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"transaction"}, ""))
 
-	pattern_Trading_PrepareProposal_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"proposal"}, ""))
+	pattern_Trading_PrepareProposal_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"governance", "proposal"}, ""))
 )
 
 var (
