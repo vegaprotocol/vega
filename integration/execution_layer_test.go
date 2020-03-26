ed6a715b76e1e87dca8d8e5c3e36068a728b71f4
Andriy Pastoshchuk <andriy@vegaprotocol.io>
2020-03-26 02:17:55 +0000

    Add trades count check

--- integration/execution_layer_test.go
+++ integration/execution_layer_test.go
@@ -832,6 +832,10 @@ func baseMarket(row *gherkin.TableRow) proto.Market {
 
 func executedTrades(trades *gherkin.DataTable) error {
 	var err error
+	if expectedCount, actualCount := len(trades.Rows), len(execsetup.trades.data); expectedCount != actualCount {
+		return fmt.Errorf("Test case does not check all trades. There are %d, checking %d trades", actualCount, expectedCount)
+	}
+
 	for i, row := range trades.Rows {
 		if i > 0 {
 			trader := val(row, 0)
