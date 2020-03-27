1e8407d9e51047b57ed45e815511c4e519f95828
jeremyletang <letang.jeremy@gmail.com>
2020-03-27 13:32:42 +0000

    add validation to SubmitTransaction to ensure we do not use a nil request

--- api/trading.go
+++ api/trading.go
@@ -200,6 +200,9 @@ func (s *tradingService) PrepareAmendOrder(ctx context.Context, req *protoapi.Am
 }
 
 func (s *tradingService) SubmitTransaction(ctx context.Context, req *protoapi.SubmitTransactionRequest) (*protoapi.SubmitTransactionResponse, error) {
+	if req == nil || req.Tx == nil {
+		return nil, apiError(codes.InvalidArgument, ErrMalformedRequest)
+	}
 	if ok, err := s.tradeOrderService.SubmitTransaction(ctx, req.Tx); err != nil || !ok {
 		s.log.Error("unable to submit transaction", logging.Error(err))
 		return nil, apiError(codes.Internal, err)
