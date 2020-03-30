e5e0b15cd43292e9772797e6a1675bcf4781d21d
Andriy Pastoshchuk <andriy@vegaprotocol.io>
2020-03-30 03:12:47 +0100

    Remove invalid length check

--- integration/execution_layer_test.go
+++ integration/execution_layer_test.go
@@ -832,10 +832,6 @@ func baseMarket(row *gherkin.TableRow) proto.Market {
 
 func executedTrades(trades *gherkin.DataTable) error {
 	var err error
-	if expectedCount, actualCount := len(trades.Rows), len(execsetup.trades.data); expectedCount != actualCount {
-		return fmt.Errorf("test case does not check all trades. There are %d, checking %d trades", actualCount, expectedCount)
-	}
-
 	for i, row := range trades.Rows {
 		if i > 0 {
 			trader := val(row, 0)
