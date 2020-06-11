9eea31392df095c25fe4603052dfcddda2ab25bf
Ashley C <ashley@vega.xyz>
2020-06-11 10:45:49 +0100

    Bump tendermint to v0.33.5 (#1754)

--- blockchain/tm/server.go
+++ blockchain/tm/server.go
@@ -6,8 +6,8 @@ import (
 	"code.vegaprotocol.io/vega/logging"
 
 	"github.com/tendermint/tendermint/abci/server"
-	cmn "github.com/tendermint/tendermint/libs/common"
 	tmlog "github.com/tendermint/tendermint/libs/log"
+	"github.com/tendermint/tendermint/libs/service"
 )
 
 // Server is an abstraction over the abci server
@@ -15,7 +15,7 @@ type Server struct {
 	Config
 	log  *logging.Logger
 	abci *AbciApplication
-	srv  cmn.Service
+	srv  service.Service
 }
 
 // NewServer instantiate a new server
