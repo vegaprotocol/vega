fa10820fba6239da48122d92378aa2d93c2d3e3b
jeremyletang <letang.jeremy@gmail.com>
2020-08-10 16:42:29 +0100

    add test for wrong asset

--- banking/engine_test.go
+++ banking/engine_test.go
@@ -56,6 +56,7 @@ func getTestEngine(t *testing.T) *testEngine {
 func TestBanking(t *testing.T) {
 	t.Run("test deposit success", testDepositSuccess)
 	t.Run("test deposit failure", testDepositFailure)
+	t.Run("test deposit failure - not builtin", testDepositFailureNotBuiltin)
 	t.Run("test deposit error - start check fail", testDepositError)
 }
 
@@ -129,6 +130,25 @@ func testDepositError(t *testing.T) {
 	// set an error to be return by the fake erc
 	expectError := errors.New("bad bad bad")
 	eng.erc.err = expectError
+
+	// call the deposit function
+	err := eng.DepositBuiltinAsset(bad, 42)
+	assert.EqualError(t, err, expectError.Error())
+}
+
+func testDepositFailureNotBuiltin(t *testing.T) {
+	eng := getTestEngine(t)
+	defer eng.ctrl.Finish()
+
+	expectError := errors.New("bad bad bad")
+	eng.assets.EXPECT().Get(gomock.Any()).Times(1).Return(nil, expectError)
+	now := time.Now()
+	eng.tsvc.EXPECT().GetTimeNow().Times(1).Return(now, nil)
+	bad := &types.BuiltinAssetDeposit{
+		VegaAssetID: "VGT",
+		PartyID:     "someparty",
+		Amount:      42,
+	}
 
 	// call the deposit function
 	err := eng.DepositBuiltinAsset(bad, 42)
