392e1933f0ef453f7377fae9f72e44501f2d41db
Witold <witold@vega.xyz>
2020-09-01 12:21:55 +0200

    Add mock trigger

--- execution/engine.go
+++ execution/engine.go
@@ -42,6 +42,7 @@ type Broker interface {
 }
 
 // AuctionTrigger can be checked with time or price to see if argument should trigger entry to or exit from the auction mode
+//go:generate go run github.com/golang/mock/mockgen -destination mocks/auction_trigger_mock.go -package mocks code.vegaprotocol.io/vega/execution AuctionTrigger
 type AuctionTrigger interface {
 	EnterPerPrice(price uint64) bool
 	EnterPerTime(time time.Time) bool
