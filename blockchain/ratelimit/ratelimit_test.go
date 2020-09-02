6e9d6cbf1b992b3a24f291d7d3c133b85f678dff
Gustavo Chaín <gchain@pm.me>
2020-09-02 11:50:06 +0200

    Feature/2124 implement ratelimit (#2169)
    * Implement Ratelimit
    
    Add a basic Tx's implementation.
    
    Closes #2124.
    
    * move ratelimit tests into a separate package name
    
    * remove unused proto/proto.go
    
    * moved ratelimit config to blockchain/tm
    
    * use map literal rather than make(map)
    
    * Clean up unused key entries
    
    * add debug log for ratelimit allowance
    
    * make linter happy

---
+++ blockchain/ratelimit/ratelimit_test.go
@@ -0,0 +1,85 @@
+package ratelimit_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"code.vegaprotocol.io/vega/blockchain/ratelimit"
+)
+
+// runN executes the given `fn` func, `n` times.
+func runN(n int, fn func()) {
+	for {
+		if n == 0 {
+			return
+		}
+		n--
+		fn()
+	}
+}
+
+func TestRateLimits(t *testing.T) {
+	t.Run("Single Block", func(t *testing.T) {
+		r := ratelimit.New(10, 10) // 10 requests in the last 10 blocks
+
+		// make 9 requests, all should be allowed
+		runN(9, func() {
+			ok := r.Allow("test")
+			assert.True(t, ok)
+		})
+
+		// request number 10 should not be allowed
+		ok := r.Allow("test")
+		assert.False(t, ok)
+	})
+
+	t.Run("Even Blocks", func(t *testing.T) {
+		r := ratelimit.New(10, 10) // 10 requests in the last 10 blocks
+
+		// this will make 1 request and move to the next block.
+		runN(9, func() {
+			ok := r.Allow("test")
+			assert.True(t, ok)
+			r.NextBlock()
+		})
+
+		ok := r.Allow("test")
+		assert.False(t, ok)
+	})
+
+	t.Run("Uneven Blocks", func(t *testing.T) {
+		r := ratelimit.New(10, 3) // 10 request in the last 3 blocks
+
+		// let's fill the rate limiter
+		runN(100, func() {
+			_ = r.Allow("test")
+		})
+		require.False(t, r.Allow("test"))
+
+		r.NextBlock()
+		assert.False(t, r.Allow("test"))
+
+		r.NextBlock()
+		assert.False(t, r.Allow("test"))
+
+		r.NextBlock()
+		assert.True(t, r.Allow("test"))
+	})
+
+	t.Run("Clean up", func(t *testing.T) {
+		r := ratelimit.New(10, 10)
+		runN(10, func() {
+			r.Allow("test")
+		})
+		require.Equal(t, 10, r.Count("test"))
+
+		runN(10, func() {
+			r.NextBlock()
+		})
+		require.Equal(t, -1, r.Count("test"))
+
+	})
+
+}
--- blockchain/tm/abci.go
+++ blockchain/tm/abci.go
@@ -7,9 +7,11 @@ import (
 	"sync"
 	"time"
 
+	"code.vegaprotocol.io/vega/blockchain/ratelimit"
 	"code.vegaprotocol.io/vega/contextutil"
 	"code.vegaprotocol.io/vega/logging"
 	"code.vegaprotocol.io/vega/metrics"
+	"code.vegaprotocol.io/vega/proto"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/tendermint/tendermint/abci/types"
@@ -50,7 +52,8 @@ type AbciApplication struct {
 	// metrics
 	blockHeightCounter prometheus.Counter
 
-	ghandler GenesisHandler
+	ghandler  GenesisHandler
+	rateLimit *ratelimit.Rates
 }
 
 // NewApplication returns a new instance of the Abci application
@@ -71,6 +74,10 @@ func NewApplication(log *logging.Logger,
 		time:            time,
 		onCriticalError: onCriticalError,
 		ghandler:        ghandler,
+		rateLimit: ratelimit.New(
+			config.RateLimit.Requests,
+			config.RateLimit.PerNBlocks,
+		),
 	}
 	if err := app.setMetrics(); err != nil {
 		app.log.Panic(
@@ -144,6 +151,8 @@ func (a *AbciApplication) InitChain(req types.RequestInitChain) types.ResponseIn
 // BeginBlock is called by the chain once the new block is starting
 func (a *AbciApplication) BeginBlock(beginBlock types.RequestBeginBlock) types.ResponseBeginBlock {
 	a.blockHeightCounter.Inc()
+	a.rateLimit.NextBlock()
+
 	// We can log more gossiped time info (switchable in config)
 	a.cfgMu.Lock()
 	if a.LogTimeDebug {
@@ -201,8 +210,21 @@ func (a *AbciApplication) BeginBlock(beginBlock types.RequestBeginBlock) types.R
 // the rest using CheckTx against the post-Commit mempool state]
 //
 func (a *AbciApplication) CheckTx(txn types.RequestCheckTx) types.ResponseCheckTx {
-	err := a.processor.Validate(txn.Tx)
+	tx, err := proto.NewTxFromSignedBundlePayload(txn.Tx)
 	if err != nil {
+		a.log.Error("Error when decoding payload in CheckTx", logging.Error(err))
+		return types.ResponseCheckTx{Code: AbciTxnValidationFailure}
+	}
+
+	// Use the Tx's pubkey to verify its rate allowance
+	key := ratelimit.Key(tx.GetPubKey()).String()
+	if ok := a.rateLimit.Allow(key); !ok {
+		a.log.Error("Rate limit exceeded", logging.String("key", key))
+		return types.ResponseCheckTx{Code: AbciTxnValidationFailure}
+	}
+	a.log.Debug("RateLimit allowance", logging.String("key", key), logging.Int("count", a.rateLimit.Count(key)))
+
+	if err := a.processor.Validate(txn.Tx); err != nil {
 		a.log.Error("Error when validating payload in CheckTx", logging.Error(err))
 		return types.ResponseCheckTx{Code: AbciTxnValidationFailure}
 	}
