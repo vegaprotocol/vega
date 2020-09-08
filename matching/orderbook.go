36c8cfec59495f792ab7c73e17ba2656c431fe10
Witold <witold@vega.xyz>
2020-09-08 13:51:03 +0200

    Return early from GetTrades

--- matching/orderbook.go
+++ matching/orderbook.go
@@ -512,20 +512,20 @@ func (b *OrderBook) GetTrades(order *types.Order) ([]*types.Trade, error) {
 		b.latestTimestamp = order.CreatedAt
 	}
 
-	var trades []*types.Trade
-	var err error
-	if b.marketState != types.MarketState_MARKET_STATE_AUCTION {
-		_, trades, err = b.getOppositeSide(order.Side).fakeUncross(order)
+	if b.marketState == types.MarketState_MARKET_STATE_AUCTION {
+		return nil, nil
+	}
 
-		if err != nil {
-			if err == ErrWashTrade {
-				// we still want to submit this order, but know there will be no trades coming out of it
-				return nil, nil
-			}
-			// some random error happened, return both trades and error
-			// this is a case that isn't covered by the current SubmitOrder call
-			return trades, err
+	_, trades, err := b.getOppositeSide(order.Side).fakeUncross(order)
+
+	if err != nil {
+		if err == ErrWashTrade {
+			// we still want to submit this order, but know there will be no trades coming out of it
+			return nil, nil
 		}
+		// some random error happened, return both trades and error
+		// this is a case that isn't covered by the current SubmitOrder call
+		return trades, err
 	}
 	// no error uncrossing, in all other cases, return trades (could be empty) without an error
 	return trades, nil
