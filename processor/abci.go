86c33ebc55cb4f3318b5491008893c83e38faf0b
Gustavo Chain <gchain@pm.me>
2020-09-10 19:06:47 +0200

    move withdraw code to a separate file

--- processor/abci.go
+++ processor/abci.go
@@ -334,28 +334,7 @@ func (app *App) DeliverWithdraw(ctx context.Context, tx abci.Tx) error {
 		return err
 	}
 
-	asset, err := app.assets.Get(w.Asset)
-	if err != nil {
-		app.log.Error("invalid vega asset ID for withdrawal",
-			logging.Error(err),
-			logging.String("party-id", w.PartyID),
-			logging.Uint64("amount", w.Amount),
-			logging.String("asset-id", w.Asset))
-		return err
-	}
-
-	switch {
-	case asset.IsBuiltinAsset():
-		return app.banking.WithdrawalBuiltinAsset(ctx, w.PartyID, w.Asset, w.Amount)
-	case asset.IsERC20():
-		ext := w.Ext.GetErc20()
-		if ext == nil {
-			return ErrMissingWithdrawERC20Ext
-		}
-		return app.banking.LockWithdrawalERC20(ctx, w.PartyID, w.Asset, w.Amount, ext)
-	}
-
-	return errors.New("unimplemented withdrawal")
+	return app.processWithdraw(ctx, w)
 }
 
 func (app *App) DeliverPropose(ctx context.Context, tx abci.Tx) error {
