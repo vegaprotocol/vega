4fe9b45a889d45e28d5be619248ec4001a690a2e
Gustavo Chaín <gchain@pm.me>
2020-10-01 11:15:37 +0200

    Cache checked tx (#2324)

--- blockchain/abci/app.go
+++ blockchain/abci/app.go
@@ -5,6 +5,7 @@ import (
 
 	"code.vegaprotocol.io/vega/blockchain"
 
+	"github.com/tendermint/tendermint/abci/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
@@ -21,8 +22,13 @@ type App struct {
 	OnDeliverTx  OnDeliverTxHandler
 	OnCommit     OnCommitHandler
 
+	// These are Tx handlers
 	checkTxs   map[blockchain.Command]TxHandler
 	deliverTxs map[blockchain.Command]TxHandler
+
+	// checkedTxs holds a map of valid transactions (validated by CheckTx)
+	// They are consumed by DeliverTx to avoid double validation.
+	checkedTxs map[string]Tx
 }
 
 func New(codec Codec) *App {
@@ -30,6 +36,7 @@ func New(codec Codec) *App {
 		codec:      codec,
 		checkTxs:   map[blockchain.Command]TxHandler{},
 		deliverTxs: map[blockchain.Command]TxHandler{},
+		checkedTxs: map[string]Tx{},
 	}
 }
 
@@ -41,4 +48,39 @@ func (app *App) HandleCheckTx(cmd blockchain.Command, fn TxHandler) *App {
 func (app *App) HandleDeliverTx(cmd blockchain.Command, fn TxHandler) *App {
 	app.deliverTxs[cmd] = fn
 	return app
+}
+
+// decodeAndValidateTx tries to decode a Tendermint Tx and validate
+// it.  It returns the Tx if decoded and validated successfully,
+// otherwise it returns the Abci error code and the underlying
+// error.
+func (app *App) decodeAndValidateTx(bytes []byte) (Tx, uint32, error) {
+	tx, err := app.codec.Decode(bytes)
+	if err != nil {
+		return nil, AbciTxnDecodingFailure, err
+	}
+
+	if err := tx.Validate(); err != nil {
+		return nil, AbciTxnValidationFailure, err
+	}
+
+	return tx, 0, nil
+}
+
+// cacheTx adds a Tx to the cache.
+func (app *App) cacheTx(r *types.RequestCheckTx, tx Tx) {
+	app.checkedTxs[string(r.Tx)] = tx
+}
+
+// txFromCache retrieves (and remove if found) a Tx from the cache,
+// it returns the Tx or nil if not found.
+func (app *App) txFromCache(r *types.RequestDeliverTx) Tx {
+	key := string(r.Tx)
+	tx, ok := app.checkedTxs[key]
+	if !ok {
+		return nil
+	}
+	delete(app.checkedTxs, key)
+
+	return tx
 }
