7458e806a8b2089eb95a0439e69dc39d1eff3629
Witold <witold@vega.xyz>
2020-10-02 18:08:30 +0200

    Add default price monitoring settings &Min/MaxMove

--- integration/execution_layer_test.go
+++ integration/execution_layer_test.go
@@ -949,6 +949,8 @@ func baseMarket(row *gherkin.TableRow) proto.Market {
 					Params: &proto.SimpleModelParams{
 						FactorLong:  f64val(row, 6),
 						FactorShort: f64val(row, 7),
+						MaxMoveUp:   0, //TODO (WG 02/10/2020): Inject from .feature file
+						MinMoveDown: 0, //TODO (WG 02/10/2020): Inject from .feature file
 					},
 				},
 			},
@@ -962,6 +964,10 @@ func baseMarket(row *gherkin.TableRow) proto.Market {
 		},
 		TradingMode: &proto.Market_Continuous{
 			Continuous: &proto.ContinuousTrading{},
+		},
+		PriceMonitoringSettings: &proto.PriceMonitoringSettings{
+			PriceMonitoringParameters: []*proto.PriceMonitoringParameters{},
+			UpdateFrequency:           0,
 		},
 	}
 
