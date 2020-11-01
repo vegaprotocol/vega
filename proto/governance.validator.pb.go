92090bc28f2d87ae261bcb513f863441ef158a40
Witold <witold@vega.xyz>
2020-11-01 19:21:19 +0100

    Modify and regenerate governance.proto

--- proto/governance.validator.pb.go
+++ proto/governance.validator.pb.go
@@ -6,7 +6,6 @@ package proto
 import (
 	fmt "fmt"
 	math "math"
-
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/mwitkow/go-proto-validators"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
@@ -57,9 +56,9 @@ func (this *NewMarketConfiguration) Validate() error {
 	if !(this.DecimalPlaces < 150) {
 		return github_com_mwitkow_go_proto_validators.FieldError("DecimalPlaces", fmt.Errorf(`value '%v' must be less than '150'`, this.DecimalPlaces))
 	}
-	if this.PriceMonitoringSettings != nil {
-		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.PriceMonitoringSettings); err != nil {
-			return github_com_mwitkow_go_proto_validators.FieldError("PriceMonitoringSettings", err)
+	if this.PriceMonitoringParameters != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.PriceMonitoringParameters); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("PriceMonitoringParameters", err)
 		}
 	}
 	if oneOfNester, ok := this.GetRiskParameters().(*NewMarketConfiguration_Simple); ok {
