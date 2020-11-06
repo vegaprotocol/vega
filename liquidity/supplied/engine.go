31038455daecfa3196848d3fce720907b54c195c
Witold <witold@vega.xyz>
2020-11-06 22:08:01 +0100

    Add UpdateLiquidityImpliedSizes

--- liquidity/supplied/engine.go
+++ liquidity/supplied/engine.go
@@ -47,6 +47,60 @@ func NewEngine(orderProvider OrderProvider, riskModel RiskModel) (*Engine, error
 	}, nil
 }
 
+// UpdateLiquidityImpliedSizes updates the LiquidityImpliedSize fields in LiquidityOrderReference so that the liquidity commitment is met.
+// Note that due to integer order size the actual liquidity provided will be more than or equal to the commitment amount.
+func (e Engine) UpdateLiquidityImpliedSizes(liquidityObligation float64, liquidityProvision types.LiquidityProvision) error {
+	//get normalised fractions
+	//get probability of trading
+	updateNormalisedFractions(liquidityProvision.Buys)
+	updateNormalisedFractions(liquidityProvision.Sells)
+	min, max := e.rm.PriceRange()
+	err := e.updateSizes(liquidityObligation, liquidityProvision.Buys, true, min, max)
+	if err != nil {
+		return err
+	}
+	err = e.updateSizes(liquidityObligation, liquidityProvision.Sells, false, min, max)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e Engine) updateSizes(liquidityObligation float64, lors []*types.LiquidityOrderReference, isBuySide bool, minPrice, maxPrice float64) error {
+	for _, lor := range lors {
+		price, err := e.getPrice(*lor)
+		if err != nil {
+			return err
+		}
+		prob := e.rm.ProbabilityOfTrading(float64(price), isBuySide, true, minPrice, maxPrice)
+		lor.LiquidityOrder.LiquidityImpliedSize = uint64(math.Ceil(liquidityObligation * lor.LiquidityOrder.NormalisedFraction / prob))
+	}
+}
+
+// TODO: not sure if should get a price in this way, if do we need to assure that the order price has already been updated.
+func (e Engine) getPrice(lor types.LiquidityOrderReference) (uint64, error) {
+	o, err := e.op.GetOrderByID(lor.OrderID)
+	if err != nil {
+		return 0, err
+	}
+	return o.Price, nil
+}
+
+// TODO: This should be moved elsewhere an only called when Proportion in any of the LiquidityOrders changes
+func updateNormalisedFractions(lors []*types.LiquidityOrderReference) {
+
+	fractions := make([]float64, len(lors))
+	var sum uint32 = 0
+	for _, lor := range lors {
+		sum += lor.LiquidityOrder.Proportion
+	}
+	fpSum := float64(sum)
+
+	for _, lor := range lors {
+		lor.LiquidityOrder.NormalisedFraction = float64(lor.LiquidityOrder.Proportion) / fpSum
+	}
+}
+
 // CalculateSuppliedLiquidity returns the current supplied liquidity per market specified in the constructor
 func (e Engine) CalculateSuppliedLiquidity(liquidityProvisions ...types.LiquidityProvision) (float64, error) {
 	buys, sells, err := e.getLiquidityProvisionOrders(liquidityProvisions...)
