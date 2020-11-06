51ee176c285e908ee75bb33e72adb1586e6a3b65
Witold <witold@vega.xyz>
2020-11-06 21:03:44 +0100

    Extend LiquidityOrder proto + regenerate

--- proto/vega.validator.pb.go
+++ proto/vega.validator.pb.go
@@ -6,7 +6,6 @@ package proto
 import (
 	fmt "fmt"
 	math "math"
-
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/mwitkow/go-proto-validators"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
@@ -211,8 +210,8 @@ func (this *OrderSubmission) Validate() error {
 	if this.PartyID == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("PartyID", fmt.Errorf(`value '%v' must not be an empty string`, this.PartyID))
 	}
-	if !(this.Size > 0) {
-		return github_com_mwitkow_go_proto_validators.FieldError("Size_", fmt.Errorf(`value '%v' must be greater than '0'`, this.Size))
+	if !(this.Size_ > 0) {
+		return github_com_mwitkow_go_proto_validators.FieldError("Size_", fmt.Errorf(`value '%v' must be greater than '0'`, this.Size_))
 	}
 	if _, ok := Side_name[int32(this.Side)]; !ok {
 		return github_com_mwitkow_go_proto_validators.FieldError("Side", fmt.Errorf(`value '%v' must be a valid Side field`, this.Side))
