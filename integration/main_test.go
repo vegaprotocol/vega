f989d411a6f307a8680c981564a32c2aaa25ae58
Elias Van Ootegem <eliasvo@gmail.com>
2020-11-13 14:13:54 +0000

    Update event assertions after rebase

--- integration/main_test.go
+++ integration/main_test.go
@@ -100,7 +100,7 @@ func FeatureContext(s *godog.Suite) {
 	s.Step(`^executed trades:$`, executedTrades)
 	s.Step(`^dump orders$`, dumpOrders)
 	s.Step(`^clear order events$`, clearOrderEvents)
-	s.Step(`^trades place pegged orders:$`, tradersPlacePeggedOrders)
+	s.Step(`^traders place pegged orders:$`, tradersPlacePeggedOrders)
 	s.Step(`^I see the following order events:$`, seeTheFollowingOrderEvents)
 	s.Step(`^clear order events by reference:$`, clearOrdersByRef)
 }
