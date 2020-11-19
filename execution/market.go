30869ca1c3902af082a5c4c55e728f8e731d1aff
Pete Barrow <pete@vega.xyz>
2020-11-19 14:36:35 +0000

    Expire pegged orders correctly (#2577)

--- execution/market.go
+++ execution/market.go
@@ -343,7 +343,7 @@ func (m *Market) GetMarketData() types.MarketData {
 		BestStaticOfferPrice:  bestStaticOfferPrice,
 		BestStaticOfferVolume: bestStaticOfferVolume,
 		MidPrice:              midPrice,
-		StaticMidPrice:		   staticMidPrice,
+		StaticMidPrice:        staticMidPrice,
 		MarkPrice:             m.markPrice,
 		Timestamp:             m.currentTime.UnixNano(),
 		OpenInterest:          m.position.GetOpenInterest(),
@@ -2348,6 +2348,7 @@ func (m *Market) orderAmendWhenParked(originalOrder, amendOrder *types.Order) (*
 }
 
 // RemoveExpiredOrders remove all expired orders from the order book
+// and also any pegged orders that are parked
 func (m *Market) RemoveExpiredOrders(timestamp int64) ([]types.Order, error) {
 	timer := metrics.NewTimeCounter(m.mkt.Id, "market", "RemoveExpiredOrders")
 	defer timer.EngineTimeCounterAdd()
@@ -2356,10 +2357,18 @@ func (m *Market) RemoveExpiredOrders(timestamp int64) ([]types.Order, error) {
 		return nil, ErrMarketClosed
 	}
 
+	expiredPegs := []types.Order{}
 	for _, order := range m.expiringPeggedOrders.Expire(timestamp) {
 		order := order
 		m.removePeggedOrder(&order)
-		m.unregisterOrder(&order)
+
+		// The pegged expiry orders are copies and do not reflect the
+		// current state of the order, therefore we look it up
+		originalOrder, _, err := m.getOrderByID(order.Id)
+		if err == nil && originalOrder.Status != types.Order_STATUS_PARKED {
+			m.unregisterOrder(&order)
+		}
+		expiredPegs = append(expiredPegs, order)
 	}
 
 	orderList := m.matching.RemoveExpiredOrders(timestamp)
@@ -2368,6 +2377,8 @@ func (m *Market) RemoveExpiredOrders(timestamp int64) ([]types.Order, error) {
 		order := order
 		m.unregisterOrder(&order)
 	}
+
+	orderList = append(orderList, expiredPegs...)
 
 	return orderList, nil
 }
