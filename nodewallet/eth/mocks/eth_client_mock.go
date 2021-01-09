d4f0440e98283ea1401e93e6f2655ee32dfdc659
jeremyletang <letang.jeremy@gmail.com>
2021-01-09 14:01:18 +0000

    regen mocks

--- nodewallet/eth/mocks/eth_client_mock.go
+++ nodewallet/eth/mocks/eth_client_mock.go
@@ -127,6 +127,21 @@ func (mr *MockETHClientMockRecorder) HeaderByNumber(arg0, arg1 interface{}) *gom
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "HeaderByNumber", reflect.TypeOf((*MockETHClient)(nil).HeaderByNumber), arg0, arg1)
 }
 
+// NetworkID mocks base method
+func (m *MockETHClient) NetworkID(arg0 context.Context) (*big.Int, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "NetworkID", arg0)
+	ret0, _ := ret[0].(*big.Int)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// NetworkID indicates an expected call of NetworkID
+func (mr *MockETHClientMockRecorder) NetworkID(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "NetworkID", reflect.TypeOf((*MockETHClient)(nil).NetworkID), arg0)
+}
+
 // PendingCodeAt mocks base method
 func (m *MockETHClient) PendingCodeAt(arg0 context.Context, arg1 common.Address) ([]byte, error) {
 	m.ctrl.T.Helper()
