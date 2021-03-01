b9f7343226d6561d067fd9c7a927a9c93b3c07c5
Ashley C <ashley@vega.xyz>
2021-03-01 08:16:35 +0000

    Sort list

--- script/gettools.sh
+++ script/gettools.sh
@@ -21,7 +21,10 @@ gettools_develop() {
 	# These are all the tools required to develop trading-core.
 
 	# tools = "golocation@version"
-	tools="github.com/golang/protobuf@v$PROTOBUF_VER
+	tools="github.com/bufbuild/buf/cmd/buf@v0.36.0
+github.com/bufbuild/buf/cmd/protoc-gen-buf-check-breaking@v0.36.0
+github.com/bufbuild/buf/cmd/protoc-gen-buf-check-lint@v0.36.0
+github.com/golang/protobuf@v$PROTOBUF_VER
 github.com/golang/protobuf/protoc-gen-go@v$PROTOBUF_VER
 github.com/gordonklaus/ineffassign@v0.0.0-20190601041439-ed7b1b5ee0f8
 github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.8.5
@@ -30,10 +33,7 @@ github.com/mwitkow/go-proto-validators/protoc-gen-govalidators@v0.2.0
 github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.3.2
 golang.org/x/lint/golint
 golang.org/x/tools/cmd/goimports@v0.0.0-20190329200012-0ec5c269d481
-honnef.co/go/tools/cmd/staticcheck@2019.2.3
-github.com/bufbuild/buf/cmd/buf@v0.36.0
-github.com/bufbuild/buf/cmd/protoc-gen-buf-check-breaking@v0.36.0
-github.com/bufbuild/buf/cmd/protoc-gen-buf-check-lint@v0.36.0"
+honnef.co/go/tools/cmd/staticcheck@2019.2.3"
 	# Note: Make sure the above tools and versions match the ones in devops-infra/docker/cipipeline/Dockerfile
 	echo "$tools" | while read -r toolurl ; do
 		go get "$toolurl"
