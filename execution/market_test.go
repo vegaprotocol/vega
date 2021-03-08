0f68e19813dd7b9abc2af9c1fbae3feb7c9abec9
jeremyletang <letang.jeremy@gmail.com>
2021-03-08 11:16:34 +0000

    fix + cleanup clearing out market if rejected

--- execution/market_test.go
+++ execution/market_test.go
@@ -4704,3 +4704,90 @@ func TestAverageEntryValuation(t *testing.T) {
 		assert.True(t, v.found, "was not in the list of lp providers", k)
 	}
 }
+
+func TestBondAccountIsReleasedItMarketRejected(t *testing.T) {
+	now := time.Unix(10, 0)
+	closingAt := time.Unix(1000000000, 0)
+	ctx := context.Background()
+
+	mktCfg := getMarket(closingAt, defaultPriceMonitorSettings, &types.AuctionDuration{
+		Duration: 10000,
+	})
+	mktCfg.Fees = &types.Fees{
+		Factors: &types.FeeFactors{
+			LiquidityFee:      "0.001",
+			InfrastructureFee: "0.0005",
+			MakerFee:          "0.00025",
+		},
+	}
+	mktCfg.TradableInstrument.RiskModel = &types.TradableInstrument_LogNormalRiskModel{
+		LogNormalRiskModel: &types.LogNormalRiskModel{
+			RiskAversionParameter: 0.001,
+			Tau:                   0.00011407711613050422,
+			Params: &types.LogNormalModelParams{
+				Mu:    0,
+				R:     0.016,
+				Sigma: 20,
+			},
+		},
+	}
+
+	lpparty := "lp-party-1"
+
+	tm := newTestMarket(t, now).Run(ctx, mktCfg)
+	tm.WithAccountAndAmount(lpparty, 500000)
+
+	tm.market.OnSuppliedStakeToObligationFactorUpdate(0.20)
+	tm.market.OnChainTimeUpdate(ctx, now)
+
+	// Add a LPSubmission
+	// this is a log of stake, enough to cover all
+	// the required stake for the market
+	lpSubmission := &types.LiquidityProvisionSubmission{
+		MarketId:         tm.market.GetID(),
+		CommitmentAmount: 150000,
+		Fee:              "0.01",
+		Reference:        "ref-lp-submission-1",
+		Buys: []*types.LiquidityOrder{
+			{Reference: types.PeggedReference_PEGGED_REFERENCE_BEST_BID, Proportion: 2, Offset: -5},
+			{Reference: types.PeggedReference_PEGGED_REFERENCE_MID, Proportion: 2, Offset: -5},
+		},
+		Sells: []*types.LiquidityOrder{
+			{Reference: types.PeggedReference_PEGGED_REFERENCE_BEST_ASK, Proportion: 13, Offset: 5},
+			{Reference: types.PeggedReference_PEGGED_REFERENCE_BEST_ASK, Proportion: 13, Offset: 5},
+		},
+	}
+
+	// submit our lp
+	require.NoError(t,
+		tm.market.SubmitLiquidityProvision(
+			ctx, lpSubmission, lpparty, "liquidity-submission-1"),
+	)
+
+	t.Run("bond account is updated with the new commitment", func(t *testing.T) {
+		bacc, err := tm.collateralEngine.GetPartyBondAccount(
+			tm.market.GetID(), lpparty, tm.asset)
+		assert.NoError(t, err)
+		assert.Equal(t, 150000, int(bacc.Balance))
+		gacc, err := tm.collateralEngine.GetPartyGeneralAccount(
+			lpparty, tm.asset)
+		assert.NoError(t, err)
+		assert.Equal(t, 350000, int(gacc.Balance))
+	})
+
+	// now we reject the network and our party bond account should be released to general
+	assert.NoError(t,
+		tm.market.Reject(context.Background()),
+	)
+
+	t.Run("bond is released to general account", func(t *testing.T) {
+		// an error as the bon account is being deleted
+		_, err := tm.collateralEngine.GetPartyBondAccount(
+			tm.market.GetID(), lpparty, tm.asset)
+		assert.EqualError(t, err, collateral.ErrAccountDoesNotExist.Error())
+		gacc, err := tm.collateralEngine.GetPartyGeneralAccount(
+			lpparty, tm.asset)
+		assert.NoError(t, err)
+		assert.Equal(t, 500000, int(gacc.Balance))
+	})
+}
