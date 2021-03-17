139a6d3b7cfff14fd1dd739ca0cea6d3012cc799
jeremyletang <letang.jeremy@gmail.com>
2021-03-17 18:39:35 +0000

    fix integration test after rebase

--- integration/steps/traders_place_the_following_pegged_orders.go
+++ integration/steps/traders_place_the_following_pegged_orders.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 
 	"code.vegaprotocol.io/vega/execution"
-	types "code.vegaprotocol.io/vega/proto"
 	commandspb "code.vegaprotocol.io/vega/proto/commands/v1"
+	"code.vegaprotocol.io/vega/types"
+
 	"github.com/cucumber/godog/gherkin"
 )
 
