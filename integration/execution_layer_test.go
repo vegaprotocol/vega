1c4e258e6c99bc5706e0a1ed3b5c839c266edee7
Valentin Trinque <val.trinque@gmail.com>
2021-03-31 11:19:15 +0200

    Remove unused "missing traders place the following orders" step

--- integration/execution_layer_test.go
+++ integration/execution_layer_test.go
@@ -1,15 +1,8 @@
 package core_test
 
 import (
-	"context"
 	"fmt"
 	"strconv"
-	"time"
-
-	types "code.vegaprotocol.io/vega/proto"
-
-	"github.com/cucumber/godog/gherkin"
-	uuid "github.com/satori/go.uuid"
 )
 
 func theInsurancePoolInitialBalanceForTheMarketsIs(amountstr string) error {
@@ -31,48 +24,6 @@ func generalAccountForAssetBalanceIs(trader, asset, balancestr string) error {
 		)
 	}
 
-	return nil
-}
-
-func missingTradersPlaceFollowingOrdersWithReferences(orders *gherkin.DataTable) error {
-	for _, row := range orders.Rows {
-		if val(row, 0) == "trader" {
-			continue
-		}
-
-		oty, err := ordertypeval(row, 6)
-		if err != nil {
-			return err
-		}
-		tif, err := tifval(row, 7)
-		if err != nil {
-			return err
-		}
-
-		var expiresAt int64
-		if oty != types.Order_TYPE_MARKET {
-			expiresAt = time.Now().Add(24 * time.Hour).UnixNano()
-		}
-
-		order := types.Order{
-			Status:      types.Order_STATUS_ACTIVE,
-			Id:          uuid.NewV4().String(),
-			MarketId:    val(row, 1),
-			PartyId:     val(row, 0),
-			Side:        sideval(row, 2),
-			Price:       u64val(row, 4),
-			Size:        u64val(row, 3),
-			Remaining:   u64val(row, 3),
-			ExpiresAt:   expiresAt,
-			Type:        oty,
-			TimeInForce: tif,
-			CreatedAt:   time.Now().UnixNano(),
-			Reference:   val(row, 8),
-		}
-		if _, err := execsetup.engine.SubmitOrder(context.Background(), &order); err == nil {
-			return fmt.Errorf("expected trader %s to not exist", order.PartyId)
-		}
-	}
 	return nil
 }
 
