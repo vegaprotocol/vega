be75d51b88ee3a8db36ec7df2dfb1c03de1339bb
Valentin Trinque <val.trinque@gmail.com>
2021-04-21 14:52:51 +0200

    Add auction trigger to auction timer labels

--- monitor/auction.go
+++ monitor/auction.go
@@ -215,7 +215,7 @@ func (a *AuctionState) AuctionExtended(ctx context.Context) *events.Auction {
 
 // AuctionStarted is called by the execution package to set flags indicating the market has started the auction
 func (a *AuctionState) AuctionStarted(ctx context.Context) *events.Auction {
-	a.openingAuctionTimer = metrics.NewTimeCounter(a.m.Id, "auction", "Auction duration")
+	a.openingAuctionTimer = metrics.NewTimeCounter(a.m.Id, "auction", "Auction duration", a.trigger.String())
 	a.start = false
 	end := int64(0)
 	if a.begin == nil {
