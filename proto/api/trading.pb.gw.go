b79daa197e0600f6eab2eafd2a048f693d78d365
jeremyletang <letang.jeremy@gmail.com>
2021-04-29 19:27:34 +0100

    fix rest prepare proposal

--- proto/api/trading.pb.gw.go
+++ proto/api/trading.pb.gw.go
@@ -113,6 +113,23 @@ func request_TradingService_SubmitTransaction_0(ctx context.Context, marshaler r
 
 }
 
+func request_TradingService_PrepareProposalSubmission_0(ctx context.Context, marshaler runtime.Marshaler, client TradingServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq PrepareProposalSubmissionRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.PrepareProposalSubmission(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_TradingService_PrepareVoteSubmission_0(ctx context.Context, marshaler runtime.Marshaler, client TradingServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq PrepareVoteSubmissionRequest
 	var metadata runtime.ServerMetadata
@@ -1427,6 +1444,26 @@ func RegisterTradingServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 
 	})
 
+	mux.Handle("POST", pattern_TradingService_PrepareProposalSubmission_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TradingService_PrepareProposalSubmission_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TradingService_PrepareProposalSubmission_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_TradingService_PrepareVoteSubmission_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1481,6 +1518,8 @@ var (
 
 	pattern_TradingService_SubmitTransaction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"transaction"}, ""))
 
+	pattern_TradingService_PrepareProposalSubmission_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"governance", "prepare", "proposal"}, ""))
+
 	pattern_TradingService_PrepareVoteSubmission_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"governance", "prepare", "vote"}, ""))
 
 	pattern_TradingService_PrepareLiquidityProvision_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"liquidity-provisions", "prepare", "submit"}, ""))
@@ -1496,6 +1535,8 @@ var (
 	forward_TradingService_PrepareWithdraw_0 = runtime.ForwardResponseMessage
 
 	forward_TradingService_SubmitTransaction_0 = runtime.ForwardResponseMessage
+
+	forward_TradingService_PrepareProposalSubmission_0 = runtime.ForwardResponseMessage
 
 	forward_TradingService_PrepareVoteSubmission_0 = runtime.ForwardResponseMessage
 
