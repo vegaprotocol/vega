6c6cc7b72aaeec1c893164ca1b064a6d80278b68
jeremyletang <letang.jeremy@gmail.com>
2021-05-04 11:01:24 +0100

    fix test + bits of code mistake from rebase

--- events/asset_test.go
+++ events/asset_test.go
@@ -13,18 +13,14 @@ func TestAssetBuiltInAssetDeepClone(t *testing.T) {
 	ctx := context.Background()
 
 	a := proto.Asset{
-		Id:          "Id",
-		Name:        "Name",
-		Symbol:      "Symbol",
-		TotalSupply: "10000",
-		Decimals:    5,
-		Source: &proto.AssetSource{
-			Source: &proto.AssetSource_BuiltinAsset{
+		Id: "Id",
+		Details: &proto.AssetDetails{
+			Name:        "Name",
+			Symbol:      "Symbol",
+			TotalSupply: "10000",
+			Decimals:    5,
+			Source: &proto.AssetDetails_BuiltinAsset{
 				BuiltinAsset: &proto.BuiltinAsset{
-					Name:                "Name",
-					Symbol:              "Symbol",
-					TotalSupply:         "100000",
-					Decimals:            5,
 					MaxFaucetAmountMint: "100000000",
 				},
 			},
@@ -36,32 +32,24 @@ func TestAssetBuiltInAssetDeepClone(t *testing.T) {
 
 	// Change the original and check we are not updating the wrapped event
 	a.Id = "Changed"
-	a.Name = "Changed"
-	a.Symbol = "Changed"
-	a.TotalSupply = "999"
-	a.Decimals = 999
+	a.Details.Name = "Changed"
+	a.Details.Symbol = "Changed"
+	a.Details.TotalSupply = "999"
+	a.Details.Decimals = 999
 
-	as := a.Source.Source.(*proto.AssetSource_BuiltinAsset)
+	as := a.Details.Source.(*proto.AssetDetails_BuiltinAsset)
 	bia := as.BuiltinAsset
-	bia.Name = "Changed"
-	bia.Symbol = "Changed"
-	bia.TotalSupply = "999"
-	bia.Decimals = 999
 	bia.MaxFaucetAmountMint = "999"
 
-	as2 := a2.Source.Source.(*proto.AssetSource_BuiltinAsset)
+	as2 := a2.Details.Source.(*proto.AssetDetails_BuiltinAsset)
 	bia2 := as2.BuiltinAsset
 
 	assert.NotEqual(t, a.Id, a2.Id)
-	assert.NotEqual(t, a.Name, a2.Name)
-	assert.NotEqual(t, a.Symbol, a2.Symbol)
-	assert.NotEqual(t, a.TotalSupply, a2.TotalSupply)
-	assert.NotEqual(t, a.Decimals, a2.Decimals)
+	assert.NotEqual(t, a.Details.Name, a2.Details.Name)
+	assert.NotEqual(t, a.Details.Symbol, a2.Details.Symbol)
+	assert.NotEqual(t, a.Details.TotalSupply, a2.Details.TotalSupply)
+	assert.NotEqual(t, a.Details.Decimals, a2.Details.Decimals)
 
-	assert.NotEqual(t, bia.Name, bia2.Name)
-	assert.NotEqual(t, bia.Symbol, bia2.Symbol)
-	assert.NotEqual(t, bia.TotalSupply, bia2.TotalSupply)
-	assert.NotEqual(t, bia.Decimals, bia2.Decimals)
 	assert.NotEqual(t, bia.MaxFaucetAmountMint, bia2.MaxFaucetAmountMint)
 }
 
@@ -69,13 +57,13 @@ func TestAssetERCDeepClone(t *testing.T) {
 	ctx := context.Background()
 
 	a := proto.Asset{
-		Id:          "Id",
-		Name:        "Name",
-		Symbol:      "Symbol",
-		TotalSupply: "10000",
-		Decimals:    5,
-		Source: &proto.AssetSource{
-			Source: &proto.AssetSource_Erc20{
+		Id: "Id",
+		Details: &proto.AssetDetails{
+			Name:        "Name",
+			Symbol:      "Symbol",
+			TotalSupply: "10000",
+			Decimals:    5,
+			Source: &proto.AssetDetails_Erc20{
 				Erc20: &proto.ERC20{
 					ContractAddress: "Contact Address",
 				},
@@ -88,23 +76,23 @@ func TestAssetERCDeepClone(t *testing.T) {
 
 	// Change the original and check we are not updating the wrapped event
 	a.Id = "Changed"
-	a.Name = "Changed"
-	a.Symbol = "Changed"
-	a.TotalSupply = "999"
-	a.Decimals = 999
+	a.Details.Name = "Changed"
+	a.Details.Symbol = "Changed"
+	a.Details.TotalSupply = "999"
+	a.Details.Decimals = 999
 
-	as := a.Source.Source.(*proto.AssetSource_Erc20)
+	as := a.Details.Source.(*proto.AssetDetails_Erc20)
 	erc := as.Erc20
 	erc.ContractAddress = "Changed"
 
-	as2 := a2.Source.Source.(*proto.AssetSource_Erc20)
+	as2 := a2.Details.Source.(*proto.AssetDetails_Erc20)
 	erc2 := as2.Erc20
 
 	assert.NotEqual(t, a.Id, a2.Id)
-	assert.NotEqual(t, a.Name, a2.Name)
-	assert.NotEqual(t, a.Symbol, a2.Symbol)
-	assert.NotEqual(t, a.TotalSupply, a2.TotalSupply)
-	assert.NotEqual(t, a.Decimals, a2.Decimals)
+	assert.NotEqual(t, a.Details.Name, a2.Details.Name)
+	assert.NotEqual(t, a.Details.Symbol, a2.Details.Symbol)
+	assert.NotEqual(t, a.Details.TotalSupply, a2.Details.TotalSupply)
+	assert.NotEqual(t, a.Details.Decimals, a2.Details.Decimals)
 
 	assert.NotEqual(t, erc.ContractAddress, erc2.ContractAddress)
 }
