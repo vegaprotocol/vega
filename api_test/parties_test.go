619e8548a5dcfff083fc26e560cf26279835a41f
Pravin Goomannee <pravin@vega.xyz>
2021-06-04 13:47:27 +0100

    test(parties): add test for Parties endpoint

---
+++ api_test/parties_test.go
@@ -0,0 +1,42 @@
+package api_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"code.vegaprotocol.io/vega/events"
+	pb "code.vegaprotocol.io/vega/proto"
+	apipb "code.vegaprotocol.io/vega/proto/api"
+	eventspb "code.vegaprotocol.io/vega/proto/events/v1"
+)
+
+func TestParties(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimout)
+	defer cancel()
+
+	conn, broker := NewTestServer(t, ctx, true)
+
+	PublishEvents(t, ctx, broker, func(be *eventspb.BusEvent) (events.Event, error) {
+		party := be.GetParty()
+		require.NotNil(t, party)
+		e := events.NewPartyEvent(ctx, pb.Party{
+			Id: party.Id,
+		})
+		return e, nil
+	}, "parties-events.golden")
+
+	client := apipb.NewTradingDataServiceClient(conn)
+	require.NotNil(t, client)
+
+	partyID := "c1f55d6be5dddbbff20312e1103a6f4b86ff4a798b74d7e9c980f98fb6747c11"
+
+	resp, err := client.Parties(ctx, &apipb.PartiesRequest{})
+
+	assert.NoError(t, err)
+	assert.Equal(t, "network", resp.Parties[0].Id)
+	assert.Equal(t, partyID, resp.Parties[1].Id)
+}
