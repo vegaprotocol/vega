3bf5a8d35102b5a0c89a0d258a78b149d2154d13
jeremyletang <letang.jeremy@gmail.com>
2021-06-07 16:29:45 +0100

    start uint256 type

---
+++ types/num/uint.go
@@ -0,0 +1,258 @@
+package num
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/holiman/uint256"
+)
+
+// Uint A wrapper for a big unsigned int
+type Uint struct {
+	u uint256.Int
+}
+
+// NewUint creates a new Uint with the value of the
+// uint64 passed as a paramter.
+func NewUint(val uint64) *Uint {
+	return &Uint{*uint256.NewInt(val)}
+}
+
+// FromBig construct a new Uint with a big.Int
+// returns true if overflow happened
+func UintFromBig(b *big.Int) (*Uint, bool) {
+	u, ok := uint256.FromBig(b)
+	// ok means an overflow happened
+	if ok {
+		return NewUint(0), true
+	}
+	return &Uint{*u}, false
+}
+
+// FromString created a new Uint from a string
+// interpreted using the give base.
+// A big.Int is used to read the string, so
+// all error related to big.Int parsing applied here.
+// will return true if an error/overflow happened
+func UintFromString(str string, base int) (*Uint, bool) {
+	b, ok := big.NewInt(0).SetString(str, base)
+	if !ok {
+		return NewUint(0), true
+	}
+	return UintFromBig(b)
+}
+
+func (z *Uint) Set(oth *Uint) *Uint {
+	z.u.Set(&oth.u)
+	return z
+}
+
+func (z *Uint) SetUint64(val uint64) *Uint {
+	z.u.SetUint64(val)
+	return z
+}
+
+func (z Uint) Uint64() uint64 {
+	return z.u.Uint64()
+}
+
+// Add will add x and y then store the result
+// into u
+// this is equivalent to:
+// `z = x + y`
+// u is returned for convenience, no
+// new variable is created.
+func (z *Uint) Add(x, y *Uint) *Uint {
+	z.u.Add(&x.u, &y.u)
+	return z
+}
+
+// AddOverflow will substract y to x then store the result
+// into u
+// this is equivalent to:
+// `z = x - y`
+// u is returned for convenience, no
+// new variable is created.
+// False is returned if an overflow occured
+func (z *Uint) AddOverflow(x, y *Uint) (*Uint, bool) {
+	_, ok := z.u.AddOverflow(&x.u, &y.u)
+	return z, ok
+}
+
+// Sub will substract y to x then store the result
+// into u
+// this is equivalent to:
+// `z = x - y`
+// u is returned for convenience, no
+// new variable is created.
+func (u *Uint) Sub(x, y *Uint) *Uint {
+	u.u.Sub(&x.u, &y.u)
+	return u
+}
+
+// SubOverflow will substract y to x then store the result
+// into u
+// this is equivalent to:
+// `z = x - y`
+// u is returned for convenience, no
+// new variable is created.
+// False is returned if an overflow occured
+func (z *Uint) SubOverflow(x, y *Uint) (*Uint, bool) {
+	_, ok := z.u.SubOverflow(&x.u, &y.u)
+	return z, ok
+}
+
+// Mul will substract y to x then store the result
+// into u
+// this is equivalent to:
+// `z = x * y`
+// u is returned for convenience, no
+// new variable is created.
+func (z *Uint) Mul(x, y *Uint) *Uint {
+	z.u.Mul(&x.u, &y.u)
+	return z
+}
+
+// Div will substract y to x then store the result
+// into u
+// this is equivalent to:
+// `z = x / y`
+// u is returned for convenience, no
+// new variable is created.
+func (u *Uint) Div(x, y *Uint) *Uint {
+	u.u.Div(&x.u, &y.u)
+	return u
+}
+
+// LT with check if the value stored in u is
+// lesser than oth
+// this is equivalent to:
+// `u < oth`
+func (u Uint) LT(oth *Uint) bool {
+	return u.u.Lt(&oth.u)
+}
+
+// LTUint64 with check if the value stored in u is
+// lesser than oth
+// this is equivalent to:
+// `u < oth`
+func (u Uint) LTUint64(oth uint64) bool {
+	return u.u.LtUint64(oth)
+}
+
+// LTE with check if the value stored in u is
+// lesser than or equal to oth
+// this is equivalent to:
+// `u <= oth`
+func (u Uint) LTE(oth *Uint) bool {
+	return u.u.Lt(&oth.u) || u.u.Eq(&oth.u)
+}
+
+// LTEUint64 with check if the value stored in u is
+// lesser than or equal to oth
+// this is equivalent to:
+// `u <= oth`
+func (u Uint) LTEUint64(oth uint64) bool {
+	return u.u.LtUint64(oth) || u.EQUint64(oth)
+}
+
+// EQ with check if the value stored in u is
+// equal to oth
+// this is equivalent to:
+// `u == oth`
+func (u Uint) EQ(oth *Uint) bool {
+	return u.u.Eq(&oth.u)
+}
+
+// EQUint64 with check if the value stored in u is
+// equal to oth
+// this is equivalent to:
+// `u == oth`
+func (u Uint) EQUint64(oth uint64) bool {
+	return u.u.Eq(uint256.NewInt(oth))
+}
+
+// NEQ with check if the value stored in u is
+// different than oth
+// this is equivalent to:
+// `u != oth`
+func (u Uint) NEQ(oth *Uint) bool {
+	return !u.u.Eq(&oth.u)
+}
+
+// NEQUint64 with check if the value stored in u is
+// different than oth
+// this is equivalent to:
+// `u != oth`
+func (u Uint) NEQUint64(oth uint64) bool {
+	return !u.u.Eq(uint256.NewInt(oth))
+}
+
+// GT with check if the value stored in u is
+// greater than oth
+// this is equivalent to:
+// `u > oth`
+func (u Uint) GT(oth *Uint) bool {
+	return u.u.Gt(&oth.u)
+}
+
+// GTUint64 with check if the value stored in u is
+// greater than oth
+// this is equivalent to:
+// `u > oth`
+func (u Uint) GTUint64(oth uint64) bool {
+	return u.u.GtUint64(oth)
+}
+
+// GTE with check if the value stored in u is
+// greath than or equal to oth
+// this is equivalent to:
+// `u >= oth`
+func (u Uint) GTE(oth *Uint) bool {
+	return u.u.Gt(&oth.u) || u.u.Eq(&oth.u)
+}
+
+// GTEUint64 with check if the value stored in u is
+// greath than or equal to oth
+// this is equivalent to:
+// `u >= oth`
+func (u Uint) GTEUint64(oth uint64) bool {
+	return u.u.GtUint64(oth) || u.EQUint64(oth)
+}
+
+// IsZero return wether u == 0 or not
+func (u Uint) IsZero() bool {
+	return u.u.IsZero()
+}
+
+// Copy create a copy of the uint
+// this if the equivalenht to:
+// z = x
+func (z *Uint) Copy(x *Uint) *Uint {
+	z.u = x.u
+	return z
+}
+
+// Clone create copy of this value
+// this is the equivalent to:
+// x := z
+func (z Uint) Clone() *Uint {
+	return &Uint{z.u}
+}
+
+// Hex returns the hexadecimal representation
+// of the stored value
+func (u Uint) Hex() string {
+	return u.u.Hex()
+}
+
+// String returns the stored value as a string
+// this is internally using big.Int.String()
+func (u Uint) String() string {
+	return u.u.ToBig().String()
+}
+
+// Format implement fmt.Formatter
+func (u Uint) Format(s fmt.State, ch rune) {
+	u.u.Format(s, ch)
+}
