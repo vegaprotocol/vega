4a0b92c61fa262d998bac1afa1634ddb1a575edc
Pete Barrow <pete@vega.xyz>
2021-06-08 18:10:21 +0100

    Fixed unit tests

--- matching/order_status_test.go
+++ matching/order_status_test.go
@@ -745,10 +745,10 @@ type marketPositionFake struct {
 	party string
 }
 
-func (m marketPositionFake) Party() string  { return m.party }
-func (m marketPositionFake) Size() int64    { return 0 }
-func (m marketPositionFake) Buy() int64     { return 0 }
-func (m marketPositionFake) Sell() int64    { return 0 }
-func (m marketPositionFake) Price() uint64  { return 0 }
-func (m marketPositionFake) VWBuy() uint64  { return 0 }
-func (m marketPositionFake) VWSell() uint64 { return 0 }
+func (m marketPositionFake) Party() string     { return m.party }
+func (m marketPositionFake) Size() int64       { return 0 }
+func (m marketPositionFake) Buy() int64        { return 0 }
+func (m marketPositionFake) Sell() int64       { return 0 }
+func (m marketPositionFake) Price() *num.Uint  { return num.NewUint(0) }
+func (m marketPositionFake) VWBuy() *num.Uint  { return num.NewUint(0) }
+func (m marketPositionFake) VWSell() *num.Uint { return num.NewUint(0) }
