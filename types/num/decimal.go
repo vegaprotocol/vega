4d3fa150cc5b71f33583774ab481d952a6066a14
Elias Van Ootegem <eliasvo@gmail.com>
2021-06-11 11:23:01 +0100

    Remove decimal import from target engine

--- types/num/decimal.go
+++ types/num/decimal.go
@@ -19,3 +19,7 @@ func NewDecimalFromBigInt(value *big.Int, exp int32) Decimal {
 func DecimalFromUint(u *Uint) Decimal {
 	return decimal.NewFromBigInt(u.BigInt(), 0)
 }
+
+func DecimalFromFloat(v float64) Decimal {
+	return decimal.NewFromFloat(v)
+}
