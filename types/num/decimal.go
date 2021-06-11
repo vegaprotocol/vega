1a3c468d04c8bc71b45ff2b96386f28bbfe52f9b
Elias Van Ootegem <eliasvo@gmail.com>
2021-06-11 11:01:24 +0100

    Move decimal stuff in alias file, use num.Decimal

--- types/num/decimal.go
+++ types/num/decimal.go
@@ -1,7 +1,17 @@
 package num
 
 import (
+	"math/big"
+
 	"github.com/shopspring/decimal"
 )
 
 type Decimal = decimal.Decimal
+
+func NewDecimalFromBigInt(value *big.Int, exp int32) Decimal {
+	return decimal.NewFromBigInt(value, exp)
+}
+
+func DecimalFromUint(u *Uint) Decimal {
+	return decimal.NewFromBigInt(u.BigInt(), 0)
+}
