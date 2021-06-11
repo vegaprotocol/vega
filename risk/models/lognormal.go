bc1b0c5e21051f9aae3cc860dd7240ee686d4d64
Elias Van Ootegem <eliasvo@gmail.com>
2021-06-11 12:03:52 +0100

    Update risk models, mocks cannot be generated ATM

--- risk/models/lognormal.go
+++ risk/models/lognormal.go
@@ -9,6 +9,7 @@ import (
 	pd "code.vegaprotocol.io/quant/pricedistribution"
 	"code.vegaprotocol.io/quant/riskmodelbs"
 	types "code.vegaprotocol.io/vega/proto"
+	"code.vegaprotocol.io/vega/types/num"
 )
 
 var (
@@ -17,13 +18,13 @@ var (
 
 // LogNormal represent a future risk model
 type LogNormal struct {
-	riskAversionParameter, tau float64
+	riskAversionParameter, tau num.Decimal
 	params                     riskmodelbs.ModelParamsBS
 	asset                      string
 
 	distCache    interfaces.AnalyticalDistribution
-	cachePrice   float64
-	cacheHorizon float64
+	cachePrice   *num.Uint
+	cacheHorizon num.Decimal
 }
 
 // NewBuiltinFutures instantiate a new builtin future
@@ -32,8 +33,8 @@ func NewBuiltinFutures(pf *types.LogNormalRiskModel, asset string) (*LogNormal, 
 		return nil, ErrMissingLogNormalParameter
 	}
 	return &LogNormal{
-		riskAversionParameter: pf.RiskAversionParameter,
-		tau:                   pf.Tau,
+		riskAversionParameter: num.DecimalFromFloat(pf.RiskAversionParameter),
+		tau:                   num.DecimalFromFloat(pf.Tau),
 		params: riskmodelbs.ModelParamsBS{
 			Mu:    pf.Params.Mu,
 			R:     pf.Params.R,
@@ -53,7 +54,9 @@ func (f *LogNormal) CalculationInterval() time.Duration {
 // the new risk models
 func (f *LogNormal) CalculateRiskFactors(
 	current *types.RiskResult) (bool, *types.RiskResult) {
-	rawrf := riskmodelbs.RiskFactorsForward(f.riskAversionParameter, f.tau, f.params)
+	rav, _ := f.riskAversionParameter.Float64()
+	tau, _ := f.tau.Float64()
+	rawrf := riskmodelbs.RiskFactorsForward(rav, tau, f.params)
 	rf := &types.RiskResult{
 		RiskFactors: map[string]*types.RiskFactor{
 			f.asset: {
@@ -72,28 +75,36 @@ func (f *LogNormal) CalculateRiskFactors(
 }
 
 // PriceRange returns the minimum and maximum price as implied by the model's probability distribution with horizon given by yearFraction (e.g. 0.5 for half a year) and probability level (e.g. 0.95 for 95%).
-func (f *LogNormal) PriceRange(currentPrice, yearFraction, probabilityLevel float64) (float64, float64) {
-	dist := f.getDistribution(currentPrice, yearFraction)
-	return pd.PriceRange(dist, probabilityLevel)
+func (f *LogNormal) PriceRange(currentP *num.Uint, yFrac, probabilityLevel num.Decimal) (*num.Uint, *num.Uint) {
+	dist := f.getDistribution(currentP, yFrac)
+	// damn you quant!
+	pl, _ := probabilityLevel.Float64()
+	minF, maxF := pd.PriceRange(dist, pl)
+	min, _ := num.UintFromDecimal(num.DecimalFromFloat(minF))
+	max, _ := num.UintFromDecimal(num.DecimalFromFloat(maxF))
+	return min, max
 }
 
 // ProbabilityOfTrading of trading returns the probability of trading given current mark price, projection horizon expressed as year fraction, order price and side (isBid).
 // Additional arguments control optional truncation of probability density outside the [minPrice,maxPrice] range.
-func (f *LogNormal) ProbabilityOfTrading(currentPrice, yearFraction, orderPrice float64, isBid bool, applyMinMax bool, minPrice float64, maxPrice float64) float64 {
-	dist := f.getDistribution(currentPrice, yearFraction)
-	//Floor min price at zero since lognormal distribution has support [0, inf)
-	minPrice = math.Max(minPrice, 0)
-	return pd.ProbabilityOfTrading(dist, orderPrice, isBid, applyMinMax, minPrice, maxPrice)
+func (f *LogNormal) ProbabilityOfTrading(currentP, orderP, minP, maxP *num.Uint, yFrac num.Decimal, isBid, applyMinMax bool) num.Decimal {
+	dist := f.getDistribution(currentP, yFrac)
+	min := math.Max(minP.Float64(), 0)
+	// still, quant uses floats
+	prob := pd.ProbabilityOfTrading(dist, orderP.Float64(), isBid, applyMinMax, min, maxP.Float64())
+	return num.DecimalFromFloat(prob)
 }
 
-func (f *LogNormal) getDistribution(currentPrice, yearFraction float64) interfaces.AnalyticalDistribution {
-	if f.cachePrice != currentPrice || f.cacheHorizon != yearFraction || f.distCache == nil {
-		f.distCache = f.params.GetProbabilityDistribution(currentPrice, yearFraction)
+func (f *LogNormal) getDistribution(currentP *num.Uint, yFrac num.Decimal) interfaces.AnalyticalDistribution {
+	if f.distCache == nil || f.cachePrice.NEQ(currentP) || !f.cacheHorizon.Equal(yFrac) {
+		// quant still uses floats... sad
+		yf, _ := yFrac.Float64()
+		f.distCache = f.params.GetProbabilityDistribution(currentP.Float64(), yf)
 	}
 	return f.distCache
 }
 
 // GetProjectionHorizon returns the projection horizon used by the model for margin calculation pruposes
-func (f *LogNormal) GetProjectionHorizon() float64 {
+func (f *LogNormal) GetProjectionHorizon() num.Decimal {
 	return f.tau
 }
