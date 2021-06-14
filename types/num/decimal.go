4e483e404071eddc804edf18ec9ff39bee7cb610
Valentin Trinque <val.trinque@gmail.com>
2021-06-14 15:24:51 +0200

    Move call to Vote.IntoProto() to NewEvent constructor

--- types/num/decimal.go
+++ types/num/decimal.go
@@ -9,16 +9,3 @@ type Decimal = decimal.Decimal
 func NewDecimalFromFloat(f float64) Decimal {
 	return decimal.NewFromFloat(f)
 }
-
-func DivDec(a, b *Uint) Decimal {
-	aDec := decimal.NewFromBigInt(a.u.ToBig(), 0)
-	bDec := decimal.NewFromBigInt(b.u.ToBig(), 0)
-
-	return aDec.Div(bDec)
-}
-
-func MulDec(a *Uint, b Decimal) Decimal {
-	aDec := decimal.NewFromBigInt(a.u.ToBig(), 0)
-
-	return b.Mul(aDec)
-}
