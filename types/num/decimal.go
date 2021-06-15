32fb026c32f011c9e81ab48b2ba25601267f969e
Pete Barrow <pete@vega.xyz>
2021-06-15 11:04:04 +0100

    Updated liquidity provision types

--- types/num/decimal.go
+++ types/num/decimal.go
@@ -23,3 +23,7 @@ func DecimalFromUint(u *Uint) Decimal {
 func DecimalFromFloat(v float64) Decimal {
 	return decimal.NewFromFloat(v)
 }
+
+func DecimalFromString(s string) (Decimal, error) {
+	return decimal.NewFromString(s)
+}
