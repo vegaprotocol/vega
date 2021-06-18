3251fa5049186ef60714636aea02207d0f4d1510
Elias Van Ootegem <eliasvo@gmail.com>
2021-06-18 13:03:21 +0100

    Move governance related types over to domain types

--- types/governance.go
+++ types/governance.go
@@ -4,24 +4,13 @@ package types
 
 import (
 	"code.vegaprotocol.io/vega/proto"
+	v1 "code.vegaprotocol.io/vega/proto/oracles/v1"
 	"code.vegaprotocol.io/vega/types/num"
 )
 
-type Proposal = proto.Proposal
-type ProposalTerms = proto.ProposalTerms
-type InstrumentConfiguration = proto.InstrumentConfiguration
-type NewMarketConfiguration = proto.NewMarketConfiguration
-type FutureProduct = proto.FutureProduct
-type UpdateMarket = proto.UpdateMarket
-type ProposalTerms_NewMarket = proto.ProposalTerms_NewMarket
-type ProposalTerms_UpdateNetworkParameter = proto.ProposalTerms_UpdateNetworkParameter
-type ProposalTerms_NewAsset = proto.ProposalTerms_NewAsset
-type InstrumentConfiguration_Future = proto.InstrumentConfiguration_Future
+type ContinuousTrading = proto.ContinuousTrading
 type NewMarketConfiguration_Continuous = proto.NewMarketConfiguration_Continuous
 type NewMarketConfiguration_Discrete = proto.NewMarketConfiguration_Discrete
-type NewMarketConfiguration_Simple = proto.NewMarketConfiguration_Simple
-type NewMarketConfiguration_LogNormal = proto.NewMarketConfiguration_LogNormal
-type NewMarketCommitment = proto.NewMarketCommitment
 
 type Vote_Value = proto.Vote_Value
 
@@ -114,7 +103,6 @@ const (
 	Proposal_STATE_WAITING_FOR_NODE_VOTE Proposal_State = 7
 )
 
-
 // Vote represents a governance vote casted by a party for a given proposal.
 type Vote struct {
 	// PartyID is the party that casted the vote.
@@ -134,6 +122,30 @@ type Vote struct {
 	TotalGovernanceTokenWeight num.Decimal
 }
 
+type Proposal struct {
+	Id           string
+	Reference    string
+	PartyId      string
+	State        Proposal_State
+	Timestamp    int64
+	Terms        *ProposalTerms
+	Reason       ProposalError
+	ErrorDetails string
+}
+
+func (p Proposal) IntoProto() *proto.Proposal {
+	return &proto.Proposal{
+		Id:           p.Id,
+		Reference:    p.Reference,
+		PartyId:      p.PartyId,
+		State:        p.State,
+		Timestamp:    p.Timestamp,
+		Terms:        p.Terms.IntoProto(),
+		Reason:       p.Reason,
+		ErrorDetails: p.ErrorDetails,
+	}
+}
+
 func (v Vote) IntoProto() *proto.Vote {
 	return &proto.Vote{
 		PartyId:                     v.PartyID,
@@ -143,4 +155,245 @@ func (v Vote) IntoProto() *proto.Vote {
 		TotalGovernanceTokenBalance: v.TotalGovernanceTokenBalance.Uint64(),
 		TotalGovernanceTokenWeight:  v.TotalGovernanceTokenWeight.String(),
 	}
+}
+
+type NewMarketCommitment struct {
+	CommitmentAmount *num.Uint
+	Fee              num.Decimal
+	Sells            []*LiquidityOrder
+	Buys             []*LiquidityOrder
+	Reference        string
+}
+
+type ProposalTerms struct {
+	ClosingTimestamp    int64
+	EnactmentTimestamp  int64
+	ValidationTimestamp int64
+	// *ProposalTerms_UpdateMarket
+	// *ProposalTerms_NewMarket
+	// *ProposalTerms_UpdateNetworkParameter
+	// *ProposalTerms_NewAsset
+	Change pterms
+}
+
+type NewMarketConfiguration = proto.NewMarketConfiguration
+type NewMarket struct {
+	Changes             *NewMarketConfiguration
+	LiquidityCommitment *NewMarketCommitment
+}
+
+type ProposalTerms_NewMarket struct {
+	NewMarket *NewMarket
+}
+
+type UpdateMarket = proto.UpdateMarket
+type ProposalTerms_UpdateMarket struct {
+	UpdateMarket *UpdateMarket
+}
+
+type UpdateNetworkParameter = proto.UpdateNetworkParameter
+type ProposalTerms_UpdateNetworkParameter struct {
+	UpdateNetworkParameter *UpdateNetworkParameter
+}
+
+type NewAsset struct {
+	Changes *AssetSource
+}
+
+type ProposalTerms_NewAsset struct {
+	NewAsset *NewAsset
+}
+
+type pterms interface {
+	isPTerm()
+	oneOfProto() interface{} // calls IntoProto
+}
+
+func (n NewMarket) IntoProto() *proto.NewMarket {
+	return &proto.NewMarket{
+		Changes:             n.Changes,
+		LiquidityCommitment: n.LiquidityCommitment.IntoProto(),
+	}
+}
+
+func (p ProposalTerms) IntoProto() *proto.ProposalTerms {
+	change := p.Change.oneOfProto()
+	r := &proto.ProposalTerms{
+		ClosingTimestamp:    p.ClosingTimestamp,
+		EnactmentTimestamp:  p.EnactmentTimestamp,
+		ValidationTimestamp: p.ValidationTimestamp,
+	}
+	switch ch := change.(type) {
+	case *proto.ProposalTerms_NewMarket:
+		r.Change = ch
+	case *proto.ProposalTerms_UpdateMarket:
+		r.Change = ch
+	case *proto.ProposalTerms_UpdateNetworkParameter:
+		r.Change = ch
+	case *proto.ProposalTerms_NewAsset:
+		r.Change = ch
+	}
+	return r
+}
+
+func (p ProposalTerms) String() string {
+	return p.IntoProto().String()
+}
+
+func (a ProposalTerms_NewMarket) IntoProto() *proto.ProposalTerms_NewMarket {
+	return &proto.ProposalTerms_NewMarket{
+		NewMarket: a.NewMarket.IntoProto(),
+	}
+}
+
+func (a ProposalTerms_NewMarket) isPTerm() {}
+func (a ProposalTerms_NewMarket) oneOfProto() interface{} {
+	return a.IntoProto()
+}
+
+func (a ProposalTerms_UpdateMarket) IntoProto() *proto.ProposalTerms_UpdateMarket {
+	return &proto.ProposalTerms_UpdateMarket{
+		UpdateMarket: a.UpdateMarket,
+	}
+}
+
+func (a ProposalTerms_UpdateMarket) isPTerm() {}
+func (a ProposalTerms_UpdateMarket) oneOfProto() interface{} {
+	return a.IntoProto()
+}
+
+func (a ProposalTerms_UpdateNetworkParameter) IntoProto() *proto.ProposalTerms_UpdateNetworkParameter {
+	return &proto.ProposalTerms_UpdateNetworkParameter{
+		UpdateNetworkParameter: a.UpdateNetworkParameter,
+	}
+}
+
+func (a ProposalTerms_UpdateNetworkParameter) isPTerm() {}
+func (a ProposalTerms_UpdateNetworkParameter) oneOfProto() interface{} {
+	return a.IntoProto()
+}
+
+func (a ProposalTerms_NewAsset) IntoProto() *proto.ProposalTerms_NewAsset {
+	return &proto.ProposalTerms_NewAsset{
+		NewAsset: a.NewAsset.IntoProto(),
+	}
+}
+
+func (a ProposalTerms_NewAsset) isPTerm() {}
+func (a ProposalTerms_NewAsset) oneOfProto() interface{} {
+	return a.IntoProto()
+}
+
+func (n NewAsset) IntoProto() *proto.NewAsset {
+	return &proto.NewAsset{
+		Changes: n.Changes.IntoProto(),
+	}
+}
+
+func (n NewAsset) String() string {
+	return n.IntoProto().String()
+}
+
+func (n NewMarketCommitment) IntoProto() *proto.NewMarketCommitment {
+	r := &proto.NewMarketCommitment{
+		CommitmentAmount: n.CommitmentAmount.Uint64(),
+		Fee:              n.Fee.String(),
+		Sells:            make([]*proto.LiquidityOrder, 0, len(n.Sells)),
+		Buys:             make([]*proto.LiquidityOrder, 0, len(n.Buys)),
+		Reference:        n.Reference,
+	}
+	for _, s := range n.Sells {
+		r.Sells = append(r.Sells, s.IntoProto())
+	}
+	for _, b := range n.Buys {
+		r.Buys = append(r.Buys, b.IntoProto())
+	}
+	return r
+}
+
+func (n NewMarketCommitment) String() string {
+	return n.IntoProto().String()
+}
+
+type NewMarketConfiguration_LogNormal struct {
+	LogNormal *LogNormalRiskModel
+}
+
+type NewMarketConfiguration_Simple struct {
+	Simple *SimpleModelParams
+}
+
+func (n NewMarketConfiguration_LogNormal) IntoProto() *proto.NewMarketConfiguration_LogNormal {
+	return &proto.NewMarketConfiguration_LogNormal{
+		LogNormal: n.LogNormal.IntoProto(),
+	}
+}
+
+func (n NewMarketConfiguration_Simple) IntoProto() *proto.NewMarketConfiguration_Simple {
+	return &proto.NewMarketConfiguration_Simple{
+		Simple: n.Simple.IntoProto(),
+	}
+}
+
+type InstrumentConfiguration struct {
+	Name string
+	Code string
+	// *InstrumentConfiguration_Future
+	Product icProd
+}
+
+type icProd interface {
+	isInstrumentConfiguration_Product()
+	icpIntoProto() interface{}
+}
+
+type InstrumentConfiguration_Future struct {
+	Future *FutureProduct
+}
+
+type FutureProduct struct {
+	Maturity          string
+	SettlementAsset   string
+	QuoteName         string
+	OracleSpec        *v1.OracleSpecConfiguration
+	OracleSpecBinding *OracleSpecToFutureBinding
+}
+
+func (i InstrumentConfiguration) IntoProto() *proto.InstrumentConfiguration {
+	p := i.Product.icpIntoProto()
+	r := &proto.InstrumentConfiguration{
+		Name: i.Name,
+		Code: i.Code,
+	}
+	switch pr := p.(type) {
+	case *proto.InstrumentConfiguration_Future:
+		r.Product = pr
+	}
+	return r
+}
+
+func (i InstrumentConfiguration_Future) IntoProto() *proto.InstrumentConfiguration_Future {
+	return &proto.InstrumentConfiguration_Future{
+		Future: i.Future.IntoProto(),
+	}
+}
+
+func (i InstrumentConfiguration_Future) icpIntoProto() interface{} {
+	return i.IntoProto()
+}
+
+func (InstrumentConfiguration_Future) isInstrumentConfiguration_Product() {}
+
+func (f FutureProduct) IntoProto() *proto.FutureProduct {
+	return &proto.FutureProduct{
+		Maturity:          f.Maturity,
+		SettlementAsset:   f.SettlementAsset,
+		QuoteName:         f.QuoteName,
+		OracleSpec:        f.OracleSpec.DeepClone(), // @TODO
+		OracleSpecBinding: f.OracleSpecBinding.IntoProto(),
+	}
+}
+
+func (f FutureProduct) String() string {
+	return f.IntoProto().String()
 }
