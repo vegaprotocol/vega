4fc28eb193a3c6f1b9c2ffdf9d574d6f8a6c986e
Elias Van Ootegem <eliasvo@gmail.com>
2021-06-22 14:58:27 +0100

    More work on migration

--- types/num/uint.go
+++ types/num/uint.go
@@ -19,7 +19,23 @@ func NewUint(val uint64) *Uint {
 	return &Uint{*uint256.NewInt(val)}
 }
 
-// UintFromBig construct a new Uint with a big.Int
+// Min returns the smallest of the 2 numbers
+func Min(a, b *Uint) *Uint {
+	if a.LT(b) {
+		return a
+	}
+	return b
+}
+
+// Max returns the largest of the 2 numbers
+func Max(a, b *Uint) *Uint {
+	if a.GT(b) {
+		return a
+	}
+	return b
+}
+
+// FromBig construct a new Uint with a big.Int
 // returns true if overflow happened
 func UintFromBig(b *big.Int) (*Uint, bool) {
 	u, ok := uint256.FromBig(b)
