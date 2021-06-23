5e8a9fb70a9719f1dedd19243454b0b1583227a0
Pete Barrow <pete@vega.xyz>
2021-06-23 20:35:25 +0100

    More proto to domain type migration

--- governance/asset.go
+++ governance/asset.go
@@ -1,9 +1,8 @@
 package governance
 
 import (
-	"strconv"
-
 	"code.vegaprotocol.io/vega/proto"
+	"code.vegaprotocol.io/vega/types"
 	"github.com/pkg/errors"
 )
 
@@ -13,35 +12,33 @@ var (
 	ErrInvalidAssetDetails         = errors.New("invalid asset details")
 )
 
-func validateNewAsset(ad *proto.AssetDetails) (proto.ProposalError, error) {
+func validateNewAsset(ad *types.AssetDetails) (proto.ProposalError, error) {
 	if perr, err := validateCommonAssetDetails(ad); err != nil {
 		return perr, err
 	}
 
 	switch s := ad.Source.(type) {
-	case *proto.AssetDetails_BuiltinAsset:
+	case *types.AssetDetails_BuiltinAsset:
 		return validateBuiltinAssetSource(s.BuiltinAsset)
-	case *proto.AssetDetails_Erc20:
+	case *types.AssetDetails_Erc20:
 		return validateERC20AssetSource(s.Erc20)
 	default:
-		return proto.ProposalError_PROPOSAL_ERROR_UNSPECIFIED, errors.New("unsupported asset source")
+		return types.ProposalError_PROPOSAL_ERROR_UNSPECIFIED, errors.New("unsupported asset source")
 	}
 }
 
-func validateCommonAssetDetails(ad *proto.AssetDetails) (proto.ProposalError, error) {
-	if len(ad.Name) <= 0 || len(ad.Symbol) <= 0 || ad.Decimals == 0 || len(ad.TotalSupply) <= 0 || len(ad.MinLpStake) <= 0 {
+func validateCommonAssetDetails(ad *types.AssetDetails) (proto.ProposalError, error) {
+	if len(ad.Name) <= 0 || len(ad.Symbol) <= 0 || ad.Decimals == 0 || ad.TotalSupply.LTEUint64(0) || ad.MinLpStake.LTEUint64(0) {
 		return proto.ProposalError_PROPOSAL_ERROR_INVALID_ASSET_DETAILS,
 			ErrInvalidAssetDetails
 	}
 
-	u, err := strconv.ParseUint(ad.TotalSupply, 10, 64)
-	if err != nil || u == 0 {
+	if ad.TotalSupply.IsZero() {
 		return proto.ProposalError_PROPOSAL_ERROR_INVALID_ASSET_DETAILS,
 			ErrInvalidAssetDetails
 	}
 
-	u, err = strconv.ParseUint(ad.MinLpStake, 10, 64)
-	if err != nil || u == 0 {
+	if ad.MinLpStake.IsZero() {
 		return proto.ProposalError_PROPOSAL_ERROR_INVALID_ASSET_DETAILS,
 			ErrInvalidAssetDetails
 	}
@@ -49,16 +46,15 @@ func validateCommonAssetDetails(ad *proto.AssetDetails) (proto.ProposalError, er
 	return proto.ProposalError_PROPOSAL_ERROR_UNSPECIFIED, nil
 }
 
-func validateBuiltinAssetSource(ba *proto.BuiltinAsset) (proto.ProposalError, error) {
-	u, err := strconv.ParseUint(ba.MaxFaucetAmountMint, 10, 64)
-	if err != nil || u == 0 {
+func validateBuiltinAssetSource(ba *types.BuiltinAsset) (proto.ProposalError, error) {
+	if ba.MaxFaucetAmountMint.IsZero() {
 		return proto.ProposalError_PROPOSAL_ERROR_MISSING_BUILTIN_ASSET_FIELD, ErrMissingBuiltinAssetField
 	}
 
 	return proto.ProposalError_PROPOSAL_ERROR_UNSPECIFIED, nil
 }
 
-func validateERC20AssetSource(ba *proto.ERC20) (proto.ProposalError, error) {
+func validateERC20AssetSource(ba *types.ERC20) (proto.ProposalError, error) {
 	if len(ba.ContractAddress) <= 0 {
 		return proto.ProposalError_PROPOSAL_ERROR_MISSING_ERC20_CONTRACT_ADDRESS, ErrMissingERC20ContractAddress
 	}
