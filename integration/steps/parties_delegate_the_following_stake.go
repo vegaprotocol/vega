5a5e56c0e3ae7ecc9bcb6c0c7c93a1fd9c9062b3
Pete Barrow <pete@vega.xyz>
2021-07-20 16:05:20 +0100

    Adding feature test handlers (#3763)

---
+++ integration/steps/parties_delegate_the_following_stake.go
@@ -0,0 +1,58 @@
+package steps
+
+import (
+	"fmt"
+
+	"github.com/cucumber/godog/gherkin"
+
+	"code.vegaprotocol.io/vega/execution"
+	"code.vegaprotocol.io/vega/types"
+)
+
+func PartiesDelegateTheFollowingStake(
+	exec *execution.Engine,
+	table *gherkin.DataTable,
+) error {
+	for _, r := range parseDelegationTable(table) {
+		row := newDelegationRow(r)
+
+		delegateStake := types.Delegate{
+			NodeID: row.NodeID(),
+			Amount: row.Amount(),
+		}
+
+		fmt.Println("Delegate type", delegateStake)
+
+		/*		resp, err := exec.Delegate(context.Background(), &delegateStake)
+				if err := checkExpectedError(row, err); err != nil {
+					return err
+				}*/
+	}
+	return nil
+}
+
+func parseDelegationTable(table *gherkin.DataTable) []RowWrapper {
+	return StrictParseTable(table, []string{
+		"node id",
+		"amount",
+	}, nil)
+}
+
+type delegationRow struct {
+	row RowWrapper
+}
+
+func newDelegationRow(r RowWrapper) delegationRow {
+	row := delegationRow{
+		row: r,
+	}
+	return row
+}
+
+func (r delegationRow) NodeID() string {
+	return r.row.MustStr("node id")
+}
+
+func (r delegationRow) Amount() uint64 {
+	return r.row.MustU64("market id")
+}
---
+++ integration/steps/parties_undelegate_the_following_stake.go
@@ -0,0 +1,58 @@
+package steps
+
+import (
+	"fmt"
+
+	"github.com/cucumber/godog/gherkin"
+
+	"code.vegaprotocol.io/vega/execution"
+	"code.vegaprotocol.io/vega/types"
+)
+
+func PartiesUndelegateTheFollowingStake(
+	exec *execution.Engine,
+	table *gherkin.DataTable,
+) error {
+	for _, r := range parseUndelegationTable(table) {
+		row := newDelegationRow(r)
+
+		undelegateStake := types.UndelegateAtEpochEnd{
+			NodeID: row.NodeID(),
+			Amount: row.Amount(),
+		}
+
+		fmt.Println("Undelegate type", undelegateStake)
+
+		/*		resp, err := exec.Undelegate(context.Background(), &undelegateStake)
+				if err := checkExpectedError(row, err); err != nil {
+					return err
+				}*/
+	}
+	return nil
+}
+
+func parseUndelegationTable(table *gherkin.DataTable) []RowWrapper {
+	return StrictParseTable(table, []string{
+		"node id",
+		"amount",
+	}, nil)
+}
+
+type undelegationRow struct {
+	row RowWrapper
+}
+
+func newUndelegationRow(r RowWrapper) delegationRow {
+	row := delegationRow{
+		row: r,
+	}
+	return row
+}
+
+func (r undelegationRow) NodeID() string {
+	return r.row.MustStr("node id")
+}
+
+func (r undelegationRow) Amount() uint64 {
+	return r.row.MustU64("market id")
+}
