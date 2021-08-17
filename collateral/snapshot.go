0085692d45ed11e6d88625791112d637e104d2ad
Elias Van Ootegem <elias@vega.xyz>
2021-08-17 12:11:02 +0100

    remove maps from collateral snapshot

--- collateral/snapshot.go
+++ collateral/snapshot.go
@@ -2,6 +2,7 @@ package collateral
 
 import (
 	"context"
+	"sort"
 
 	vpb "code.vegaprotocol.io/protos/vega"
 	"code.vegaprotocol.io/vega/logging"
@@ -16,18 +17,8 @@ func (e *Engine) Name() string {
 }
 
 func (e *Engine) Checkpoint() []byte {
-	balances := e.getPartyBalances()
 	msg := &vpb.Collateral{
-		Parties: make(map[string]*vpb.Balance, len(balances)),
-	}
-	for party, assets := range balances {
-		bal := &vpb.Balance{
-			Balance: make(map[string][]byte, len(assets)),
-		}
-		for a, b := range assets {
-			bal.Balance[a] = []byte(b.String()) // either update the proto to use string, or use [32]byte reliably
-		}
-		msg.Parties[party] = bal
+		Balances: e.getSnapshotBalances(),
 	}
 	ret, err := vpb.Marshal(msg)
 	if err != nil {
@@ -42,24 +33,24 @@ func (e *Engine) Load(checkpoint, _ []byte) error {
 	if err := vpb.Unmarshal(checkpoint, &msg); err != nil {
 		return err
 	}
-	for party, balances := range msg.Parties {
-		for a, bal := range balances.Balance {
-			ub := num.UintFromBytes(bal)
-			// ub, _ := num.UintFromString(bal, 10)
-			accID := e.accountID(noMarket, party, a, types.AccountTypeGeneral)
-			if _, err := e.GetAccountByID(accID); err != nil {
-				accID, _ = e.CreatePartyGeneralAccount(context.Background(), party, a)
-			}
-			e.UpdateBalance(context.Background(), accID, ub)
+	for _, balance := range msg.Balances {
+		ub, _ := num.UintFromString(balance.Balance, 10)
+		accID := e.accountID(noMarket, balance.Party, balance.Asset, types.AccountTypeGeneral)
+		if _, err := e.GetAccountByID(accID); err != nil {
+			accID, _ = e.CreatePartyGeneralAccount(context.Background(), balance.Party, balance.Asset)
 		}
+		e.UpdateBalance(context.Background(), accID, ub)
 	}
 	return nil
 }
 
-// get party balances per asset
-func (e *Engine) getPartyBalances() map[string]map[string]*num.Uint {
-	ret := make(map[string]map[string]*num.Uint, len(e.accs))
+// get all balances for snapshot
+func (e *Engine) getSnapshotBalances() []*vpb.AssetBalance {
+	parties := make([]string, 0, len(e.partiesAccs))
+	pbal := make(map[string][]*vpb.AssetBalance, len(e.partiesAccs))
+	entries := 0
 	for party, accs := range e.partiesAccs {
+		assets := make([]string, 0, len(accs))
 		balances := map[string]*num.Uint{}
 		for _, acc := range accs {
 			switch acc.Type {
@@ -69,6 +60,7 @@ func (e *Engine) getPartyBalances() map[string]map[string]*num.Uint {
 				if !ok {
 					assetBal = num.Zero()
 					balances[acc.Asset] = assetBal
+					assets = append(assets, acc.Asset)
 				}
 				assetBal.AddSum(acc.Balance)
 			case types.AccountTypeSettlement:
@@ -78,7 +70,28 @@ func (e *Engine) getPartyBalances() map[string]map[string]*num.Uint {
 				}
 			}
 		}
-		ret[party] = balances
+		ln := len(assets)
+		if ln == 0 {
+			continue
+		}
+		entries += ln
+		pbal[party] = make([]*vpb.AssetBalance, 0, len(assets))
+		parties = append(parties, party)
+		// sort by asset -> each party will have their balances appended in alphabetic order
+		sort.Strings(assets)
+		for _, a := range assets {
+			bal := balances[a]
+			pbal[party] = append(pbal[party], &vpb.AssetBalance{
+				Party:   party,
+				Asset:   a,
+				Balance: bal.String(),
+			})
+		}
+	}
+	ret := make([]*vpb.AssetBalance, 0, entries)
+	sort.Strings(parties)
+	for _, party := range parties {
+		ret = append(ret, pbal[party]...)
 	}
 	return ret
 }
