2b6683437a6fd11673bdd74de2aae409868c625c
Elias Van Ootegem <elias@vega.xyz>
2021-08-24 19:12:29 +0100

    Remove pointless over-complication of checkpoint file/path flags

--- cmd/vega/checkpoint.go
+++ cmd/vega/checkpoint.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"path/filepath"
 
 	commandspb "code.vegaprotocol.io/protos/vega/commands/v1"
 	"code.vegaprotocol.io/vega/config"
@@ -30,8 +29,7 @@ type CheckpointCmd struct {
 type checkpointRestore struct {
 	nodewallet.Config
 	// opts for command
-	CPDir  string `short:"d" long:"checkpoint-dir" description:"path where checkpoint file is located"`
-	CPFile string `short:"c" long:"checkpoint-name" description:"name of the file containing the checkpoint data"`
+	CPFile string `short:"f" long:"checkpoint-file" description:"name of the file containing the checkpoint data"`
 }
 
 var checkpointCmd CheckpointCmd
@@ -39,13 +37,10 @@ var checkpointCmd CheckpointCmd
 // Checkpoint - This function is invoked from `Register` in main.go
 func Checkpoint(ctx context.Context, parser *flags.Parser) error {
 
-	rootP := config.NewRootPathFlag()
 	// here we initialize the global exampleCmd with needed default values.
 	checkpointCmd = CheckpointCmd{
 		RootPathFlag: rootP,
-		Restore: checkpointRestore{
-			CPDir: fmt.Sprintf("%s/checkpoints/", rootP.RootPath), // @TODO use filepath Join method
-		},
+		Restore:      checkpointRestore{},
 	}
 	_, err := parser.AddCommand("checkpoint", "Restore checkpoint", "Submits restore transaction to the chain to quickly restart the node from a given state", &checkpointCmd)
 	return err
@@ -58,15 +53,11 @@ func (c *checkpointRestore) Execute(args []string) error {
 	log := logging.NewLoggerFromConfig(logging.NewDefaultConfig())
 	defer log.AtExit()
 
-	fPath, err := c.GetPath()
-	if err != nil {
+	if ok, err := fsutil.FileExists(c.CPFile); !ok {
 		return fmt.Errorf("checkpoint file not found: %w", err)
 	}
-	if ok, err := fsutil.PathExists(checkpointCmd.RootPath); !ok {
-		return fmt.Errorf("invalid root directory path: %w", err)
-	}
 
-	cpData, err := ioutil.ReadFile(fPath)
+	cpData, err := ioutil.ReadFile(c.CPFile)
 	if err != nil {
 		return fmt.Errorf("failed to read checkpoint file: %w", err)
 	}
@@ -86,23 +77,6 @@ func (c *checkpointRestore) Execute(args []string) error {
 		close(ch)
 	})
 	return <-ch
-}
-
-func (c checkpointRestore) GetPath() (string, error) {
-	if ok, _ := fsutil.FileExists(c.CPFile); ok {
-		return c.CPFile, nil
-	}
-	if ok, _ := fsutil.PathExists(c.CPDir); !ok {
-		c.CPDir = filepath.Join(checkpointCmd.RootPath, c.CPDir)
-	}
-	if ok, err := fsutil.PathExists(c.CPDir); !ok {
-		return "", fmt.Errorf("invalid path for checkpoint file: %w", err)
-	}
-	fPath := filepath.Join(c.CPDir, c.CPFile)
-	if ok, err := fsutil.FileExists(fPath); !ok {
-		return "", fmt.Errorf("checkpoint file not found: %w", err)
-	}
-	return fPath, nil
 }
 
 func (c checkpointRestore) getCommander(log *logging.Logger) (*nodewallet.Commander, error) {
