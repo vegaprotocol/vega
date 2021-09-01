99aeb766f9489c66398f183041f860438437dc14
jeremyletang <letang.jeremy@gmail.com>
2021-09-01 10:49:39 +0100

    regen mocks

--- api/mocks/trading_service_client_mock.go
+++ api/mocks/trading_service_client_mock.go
@@ -35,6 +35,66 @@ func (m *MockTradingServiceClient) EXPECT() *MockTradingServiceClientMockRecorde
 	return m.recorder
 }
 
+// GetVegaTime mocks base method
+func (m *MockTradingServiceClient) GetVegaTime(arg0 context.Context, arg1 *api.GetVegaTimeRequest, arg2 ...grpc.CallOption) (*api.GetVegaTimeResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "GetVegaTime", varargs...)
+	ret0, _ := ret[0].(*api.GetVegaTimeResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetVegaTime indicates an expected call of GetVegaTime
+func (mr *MockTradingServiceClientMockRecorder) GetVegaTime(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetVegaTime", reflect.TypeOf((*MockTradingServiceClient)(nil).GetVegaTime), varargs...)
+}
+
+// LastBlockHeight mocks base method
+func (m *MockTradingServiceClient) LastBlockHeight(arg0 context.Context, arg1 *api.LastBlockHeightRequest, arg2 ...grpc.CallOption) (*api.LastBlockHeightResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "LastBlockHeight", varargs...)
+	ret0, _ := ret[0].(*api.LastBlockHeightResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// LastBlockHeight indicates an expected call of LastBlockHeight
+func (mr *MockTradingServiceClientMockRecorder) LastBlockHeight(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "LastBlockHeight", reflect.TypeOf((*MockTradingServiceClient)(nil).LastBlockHeight), varargs...)
+}
+
+// ObserveEventBus mocks base method
+func (m *MockTradingServiceClient) ObserveEventBus(arg0 context.Context, arg1 ...grpc.CallOption) (api.TradingService_ObserveEventBusClient, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0}
+	for _, a := range arg1 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "ObserveEventBus", varargs...)
+	ret0, _ := ret[0].(api.TradingService_ObserveEventBusClient)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ObserveEventBus indicates an expected call of ObserveEventBus
+func (mr *MockTradingServiceClientMockRecorder) ObserveEventBus(arg0 interface{}, arg1 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0}, arg1...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ObserveEventBus", reflect.TypeOf((*MockTradingServiceClient)(nil).ObserveEventBus), varargs...)
+}
+
 // PropagateChainEvent mocks base method
 func (m *MockTradingServiceClient) PropagateChainEvent(arg0 context.Context, arg1 *api.PropagateChainEventRequest, arg2 ...grpc.CallOption) (*api.PropagateChainEventResponse, error) {
 	m.ctrl.T.Helper()
@@ -53,6 +113,26 @@ func (mr *MockTradingServiceClientMockRecorder) PropagateChainEvent(arg0, arg1 i
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PropagateChainEvent", reflect.TypeOf((*MockTradingServiceClient)(nil).PropagateChainEvent), varargs...)
+}
+
+// Statistics mocks base method
+func (m *MockTradingServiceClient) Statistics(arg0 context.Context, arg1 *api.StatisticsRequest, arg2 ...grpc.CallOption) (*api.StatisticsResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "Statistics", varargs...)
+	ret0, _ := ret[0].(*api.StatisticsResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Statistics indicates an expected call of Statistics
+func (mr *MockTradingServiceClientMockRecorder) Statistics(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Statistics", reflect.TypeOf((*MockTradingServiceClient)(nil).Statistics), varargs...)
 }
 
 // SubmitTransactionV2 mocks base method
