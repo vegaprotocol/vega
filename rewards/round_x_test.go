19b64f11893c6bb226c269b2b31efb3a450940a2
Zohar Etzioni <zohar.etzioni@gmail.com>
2021-10-12 13:28:29 +0100

    added more testing for reward consistency across epochs

--- rewards/round_x_test.go
+++ rewards/round_x_test.go
@@ -10,7 +10,66 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// test a 100 rounds of reward distribution, make sure that the ratio the delegators get out of the reward balance for the epoch is always the same and doesn't drift
+const tolerance = 1e-12
+
+func TestRepeatedRounds(t *testing.T) {
+	testEngine := getEngine(t)
+	engine := testEngine.engine
+	engine.registerStakingAndDelegationRewardScheme()
+	engine.UpdateDelegatorShareForStakingRewardScheme(context.Background(), 0.3)
+	engine.UpdateAssetForStakingAndDelegationRewardScheme(context.Background(), "ETH")
+	engine.UpdateMinimumValidatorStakeForStakingRewardScheme(context.Background(), num.NewDecimalFromFloat(0))
+	engine.UpdateMaxPayoutPerEpochStakeForStakingRewardScheme(context.Background(), num.NewDecimalFromFloat(1000000000))
+	engine.UpdateCompetitionLevelForStakingRewardScheme(context.Background(), 1.1)
+	engine.UpdatePayoutFractionForStakingRewardScheme(context.Background(), 0.1)
+	engine.UpdateMinValidatorsStakingRewardScheme(context.Background(), 5)
+	rs := engine.rewardSchemes[stakingAndDelegationSchemeID]
+
+	//start with 10 VEGA
+	rewardBalance, _ := num.UintFromString("10000000000000000000", 10)
+	expectedParty1Ratio := 0.104571
+	expectedParty2Ratio := 0.024
+	expectedNode1Ratio := 0.14
+	expectedNode2Ratio := 0.4
+	expectedNode3Ratio := 0.331428
+
+	// run for 10000 epochs and verify that the ratio each party gets remains constant
+	for round := uint64(0); round < 10000; round++ {
+		epoch := types.Epoch{Seq: round}
+		rewardForEpoch, _ := rs.GetReward(rewardBalance, epoch)
+		println(round, rewardForEpoch.String())
+		testEngine.delegation.EXPECT().ProcessEpochDelegations(gomock.Any(), gomock.Any()).Return(testEngine.validatorData)
+
+		res := engine.calculateRewards(context.Background(), "ETH", rs.RewardPoolAccountIDs[0], rs, rewardForEpoch, epoch)
+		// node1, node2, node3, party1, party2
+		require.Equal(t, 5, len(res.partyToAmount))
+
+		party1Ratio, _ := res.partyToAmount["party1"].ToDecimal().Div(rewardForEpoch.ToDecimal()).Float64()
+		party2Ratio, _ := res.partyToAmount["party2"].ToDecimal().Div(rewardForEpoch.ToDecimal()).Float64()
+		node1Ratio, _ := res.partyToAmount["node1"].ToDecimal().Div(rewardForEpoch.ToDecimal()).Float64()
+		node2Ratio, _ := res.partyToAmount["node2"].ToDecimal().Div(rewardForEpoch.ToDecimal()).Float64()
+		node3Ratio, _ := res.partyToAmount["node3"].ToDecimal().Div(rewardForEpoch.ToDecimal()).Float64()
+
+		if !res.partyToAmount["party1"].IsZero() {
+			require.True(t, party1Ratio-expectedParty1Ratio < tolerance)
+		}
+		if !res.partyToAmount["party2"].IsZero() {
+			require.True(t, party2Ratio-expectedParty2Ratio < tolerance)
+		}
+		if !res.partyToAmount["node1"].IsZero() {
+			require.True(t, node1Ratio-expectedNode1Ratio < tolerance)
+		}
+		if !res.partyToAmount["node2"].IsZero() {
+			require.True(t, node2Ratio-expectedNode2Ratio < tolerance)
+		}
+		if !res.partyToAmount["node3"].IsZero() {
+			require.True(t, node3Ratio-expectedNode3Ratio < tolerance)
+		}
+		rewardBalance.Sub(rewardBalance, res.totalReward)
+	}
+}
+
+// test a 10000 rounds of reward distribution, make sure that the ratio the delegators get out of the reward balance for the epoch is always the same and doesn't drift
 func TestNoDriftdRounds(t *testing.T) {
 	testEngine := getEngine(t)
 	engine := testEngine.engine
@@ -34,9 +93,11 @@ func TestNoDriftdRounds(t *testing.T) {
 	}
 	validatorData := []*types.ValidatorData{validator1}
 
-	rewardBalance := num.NewUint(1000000)
-	var round uint64 = 0
-	for ; round < 100; round++ {
+	// start with 10 VEGA reward balance
+	rewardBalance, _ := num.UintFromString("10000000000000000000", 10)
+
+	// run for 10000 epochs and verify that the ratio each party gets remains constant
+	for round := uint64(0); round < 10000; round++ {
 		epoch := types.Epoch{Seq: round}
 		rewardForEpoch, _ := rs.GetReward(rewardBalance, epoch)
 		println(round, rewardForEpoch.String())
@@ -47,8 +108,8 @@ func TestNoDriftdRounds(t *testing.T) {
 
 		party1, _ := res.partyToAmount["party1"].ToDecimal().Div(rewardForEpoch.ToDecimal()).Float64()
 		node1, _ := res.partyToAmount["node1"].ToDecimal().Div(rewardForEpoch.ToDecimal()).Float64()
-		require.True(t, party1-0.883 < 1e-4)
-		require.True(t, node1-0.117 < 1e-4)
+		require.True(t, party1-0.883 < tolerance)
+		require.True(t, node1-0.117 < tolerance)
 		rewardBalance.Sub(rewardBalance, res.totalReward)
 	}
 
