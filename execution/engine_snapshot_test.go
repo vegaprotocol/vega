338c1c99c62d210076adc8fb417bc97e7841726e
Pete Barrow <pete@vega.xyz>
2021-10-15 10:26:44 +0100

    Upgraded golang and gofmt

--- execution/engine_snapshot_test.go
+++ execution/engine_snapshot_test.go
@@ -53,7 +53,7 @@ func getMarketConfig() *types.Market {
 		PriceMonitoringSettings: &types.PriceMonitoringSettings{
 			Parameters: &types.PriceMonitoringParameters{
 				Triggers: []*types.PriceMonitoringTrigger{
-					&types.PriceMonitoringTrigger{
+					{
 						Horizon:          1000,
 						HorizonDec:       num.DecimalFromFloat(1000.0),
 						Probability:      num.DecimalFromFloat(0.3),
