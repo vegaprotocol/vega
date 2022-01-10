b17ee381d19b3484d1d6a488cf6f5f5d3e06dff9
Thomas Guinot <thomas.guinot@gmail.com>
2022-01-10 14:10:14 +0100

    Add commands to tx_v2

--- processor/tx_v2.go
+++ processor/tx_v2.go
@@ -92,6 +92,10 @@ func (t TxV2) Command() txn.Command {
 		return txn.WithdrawCommand
 	case *commandspb.InputData_LiquidityProvisionSubmission:
 		return txn.LiquidityProvisionCommand
+	case *commandspb.InputData_LiquidityProvisionCancellation:
+		return txn.CancelLiquidityProvisionCommand
+	case *commandspb.InputData_LiquidityProvisionAmendment:
+		return txn.AmendLiquidityProvisionCommand
 	case *commandspb.InputData_ProposalSubmission:
 		return txn.ProposeCommand
 	case *commandspb.InputData_NodeRegistration:
@@ -133,6 +137,10 @@ func (t TxV2) GetCmd() interface{} {
 		return cmd.WithdrawSubmission
 	case *commandspb.InputData_LiquidityProvisionSubmission:
 		return cmd.LiquidityProvisionSubmission
+	case *commandspb.InputData_LiquidityProvisionCancellation:
+		return cmd.LiquidityProvisionCancellation
+	case *commandspb.InputData_LiquidityProvisionAmendment:
+		return cmd.LiquidityProvisionAmendment
 	case *commandspb.InputData_ProposalSubmission:
 		return cmd.ProposalSubmission
 	case *commandspb.InputData_NodeRegistration:
