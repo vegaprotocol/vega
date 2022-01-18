381bdba5e875140e7569f335a8ef49f3aa1c9d32
Zohar Etzioni <zohar.etzioni@gmail.com>
2022-01-18 07:37:59 +0000

    restore prevent leaving auction logic until consensus is reached

--- execution/auction.go
+++ execution/auction.go
@@ -39,15 +39,15 @@ func (m *Market) checkAuction(ctx context.Context, now time.Time) {
 			m.log.Panic("Leaving opening auction somehow triggered price monitoring to extend the auction")
 		}
 		// only do this once
-		// if !m.as.CanLeave() {
-		m.OnOpeningAuctionFirstUncrossingPrice()
-		// }
+		if !m.as.CanLeave() {
+			m.OnOpeningAuctionFirstUncrossingPrice()
+		}
 		m.as.SetReadyToLeave()
 		// if we don't have yet consensus for the floating point parameters, stay in the opening auction
-		// if !m.CanLeaveOpeningAuction() {
-		// 	m.log.Info("cannot leave opening auction - waiting for floating point to complete the first round")
-		// 	return
-		// }
+		if !m.CanLeaveOpeningAuction() {
+			m.log.Info("cannot leave opening auction - waiting for floating point to complete the first round")
+			return
+		}
 		m.log.Info("leaving opening auction for market", logging.String("market-id", m.mkt.ID))
 		m.LeaveAuction(ctx, now)
 		// the market is now in a ACTIVE state
