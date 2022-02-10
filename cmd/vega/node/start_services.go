a99ac6eddde9467d3f22d3d7a69e363f61259fcd
wwestgarth <william@vega.xyz>
2022-02-10 16:28:04 +0000

    force non-validator if in nullchain mode, and return empty validator list

--- cmd/vega/node/start_services.go
+++ cmd/vega/node/start_services.go
@@ -208,6 +208,8 @@ func (n *NodeCommand) stopServices(_ []string) error {
 
 func (n *NodeCommand) startBlockchain() (*processor.App, error) {
 	// if tm chain, setup the client
+
+	var null *nullchain.NullBlockchain
 	switch n.conf.Blockchain.ChainProvider {
 	case blockchain.ProviderTendermint:
 		a, err := abci.NewClient(n.conf.Blockchain.Tendermint.ClientAddr)
@@ -215,6 +217,9 @@ func (n *NodeCommand) startBlockchain() (*processor.App, error) {
 			return nil, err
 		}
 		n.blockchainClient = blockchain.NewClient(a)
+	case blockchain.ProviderNullChain:
+		null = nullchain.NewClient(n.Log, n.conf.Blockchain.Null)
+		n.blockchainClient = blockchain.NewClient(null)
 	}
 
 	app := processor.NewApp(
@@ -274,12 +279,9 @@ func (n *NodeCommand) startBlockchain() (*processor.App, error) {
 			return nil, err
 		}
 	case blockchain.ProviderNullChain:
-		abciApp := app.Abci()
-		null := nullchain.NewClient(n.Log, n.conf.Blockchain.Null, abciApp)
-
+		null.SetABCIApp(app.Abci())
 		// nullchain acts as both the client and the server because its does everything
 		n.blockchainServer = blockchain.NewServer(null)
-		n.blockchainClient = blockchain.NewClient(null)
 	default:
 		return nil, ErrUnknownChainProvider
 	}
