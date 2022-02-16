320ebe2d65143914b70c3c3c4f5f6f6be9d00776
Philip Scott <phil@vega.xyz>
2022-02-16 16:59:01 +0000

    Add golangci-lint

--- script/gettools.sh
+++ script/gettools.sh
@@ -15,6 +15,7 @@ github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.7.3
 github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.7.3
 github.com/gordonklaus/ineffassign@v0.0.0-20190601041439-ed7b1b5ee0f8
 golang.org/x/lint/golint
+github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
 golang.org/x/tools/cmd/goimports@v0.0.0-20190329200012-0ec5c269d481
 honnef.co/go/tools/cmd/staticcheck@2019.2.3"
 	# Note: Make sure the above tools and versions match the ones in devops-infra/docker/cipipeline/Dockerfile
