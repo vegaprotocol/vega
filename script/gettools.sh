76d03ea5ce2736bbe6d70313dd4338baa9070b44
Philip Scott <phil@vega.xyz>
2022-02-17 13:14:32 +0000

    Don't need protoc stuff

--- script/gettools.sh
+++ script/gettools.sh
@@ -9,10 +9,6 @@ gettools_develop() {
 
 	# tools = "golocation@version"
 	tools="github.com/bufbuild/buf/cmd/buf@v$BUF_VER
-google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
-google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
-github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.7.3
-github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.7.3
 github.com/gordonklaus/ineffassign@v0.0.0-20190601041439-ed7b1b5ee0f8
 golang.org/x/lint/golint
 github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
