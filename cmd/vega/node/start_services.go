02913b5742fbb26abb99399de34bc19b1816f283
wwestgarth <william@vega.xyz>
2022-02-24 15:55:12 +0000

    fix: statevar engine's snapshotting is now properly plumbed in (#4854)

--- cmd/vega/node/start_services.go
+++ cmd/vega/node/start_services.go
@@ -182,7 +182,7 @@ func (n *NodeCommand) startServices(_ []string) (err error) {
 	n.topology.NotifyOnKeyChange(n.delegation.ValidatorKeyChanged, n.stakingAccounts.ValidatorKeyChanged, n.governance.ValidatorKeyChanged)
 
 	n.snapshot.AddProviders(n.checkpoint, n.collateral, n.governance, n.delegation, n.netParams, n.epochService, n.assets, n.banking,
-		n.notary, n.spam, n.stakingAccounts, n.stakeVerifier, n.limits, n.topology, n.eventForwarder, n.executionEngine, n.feesTracker, marketTracker)
+		n.notary, n.spam, n.stakingAccounts, n.stakeVerifier, n.limits, n.topology, n.eventForwarder, n.executionEngine, n.feesTracker, marketTracker, n.statevar)
 
 	// setup config reloads for all engines / services /etc
 	n.setupConfigWatchers()
--- monitor/price/snapshot.go
+++ monitor/price/snapshot.go
@@ -6,12 +6,16 @@ import (
 	"code.vegaprotocol.io/vega/logging"
 	"code.vegaprotocol.io/vega/types"
 	"code.vegaprotocol.io/vega/types/num"
+	"code.vegaprotocol.io/vega/types/statevar"
 )
 
 func NewMonitorFromSnapshot(
+	marketID string,
+	asset string,
 	pm *types.PriceMonitor,
 	settings *types.PriceMonitoringSettings,
 	riskModel RangeProvider,
+	stateVarEngine StateVarEngine,
 	log *logging.Logger,
 ) (*Engine, error) {
 	if riskModel == nil {
@@ -38,6 +42,7 @@ func NewMonitorFromSnapshot(
 		stateChanged:        true,
 	}
 	e.boundFactorsInitialised = pm.PriceBoundsConsensusReached
+	stateVarEngine.RegisterStateVariable(asset, marketID, "bound-factors", boundFactorsConverter{}, e.startCalcPriceRanges, []statevar.StateVarEventType{statevar.StateVarEventTypeTimeTrigger, statevar.StateVarEventTypeAuctionEnded, statevar.StateVarEventTypeOpeningAuctionFirstUncrossingPrice}, e.updatePriceBounds)
 	return e, nil
 }
 
