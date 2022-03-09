f4ffae45771f5e0a13f96a83a378e22d7e52ffaf
Philip Scott <94362991+pscott31@users.noreply.github.com>
2022-03-09 10:09:32 +0000

    feat: Add option to stream events to file

---
+++ broker/file_client.go
@@ -0,0 +1,74 @@
+package broker
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"code.vegaprotocol.io/vega/events"
+	"code.vegaprotocol.io/vega/logging"
+	"github.com/golang/protobuf/proto"
+)
+
+type fileClient struct {
+	log *logging.Logger
+
+	config *FileConfig
+
+	file *os.File
+	mut  sync.RWMutex
+}
+
+func newFileClient(ctx context.Context, log *logging.Logger, config *FileConfig) (*fileClient, error) {
+	fc := &fileClient{
+		log:    log,
+		config: config,
+	}
+
+	filePath, err := filepath.Abs(config.File)
+	if err != nil {
+		return nil, fmt.Errorf("unable to determine absolute path of file %s: %w", config.File, err)
+	}
+
+	fc.file, err = os.Create(filePath)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to create file %s: %w", filePath, err)
+	}
+
+	log.Infof("persisting events to: %s\n", filePath)
+
+	return fc, nil
+}
+
+func (fc *fileClient) SendBatch(evts []events.Event) error {
+	for _, evt := range evts {
+		if err := fc.Send(evt); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (fc *fileClient) Send(event events.Event) error {
+	fc.mut.RLock()
+	defer fc.mut.RUnlock()
+
+	busEvent := event.StreamMessage()
+
+	size := uint32(proto.Size(busEvent))
+	sizeBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(sizeBytes, size)
+
+	protoBytes, err := proto.Marshal(busEvent)
+	if err != nil {
+		return fmt.Errorf("failed to marshal bus event:" + busEvent.String())
+	}
+
+	allBytes := append(sizeBytes, protoBytes...) // nozero
+	fc.file.Write(allBytes)
+	return nil
+}
