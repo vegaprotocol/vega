6b450dc82da558d4d637d2e9f9dbbf7274d3a725
Elias Van Ootegem <elias@vega.xyz>
2022-03-14 13:07:31 +0000

    fix: replicate LP order uncrossing panic and fix

--- execution/special_orders.go
+++ execution/special_orders.go
@@ -45,12 +45,15 @@ func (m *Market) repricePeggedOrders(
 					order.UpdatedAt = m.currentTime.UnixNano()
 					order.Status = types.OrderStatusParked
 					order.Price = num.Zero()
+					order.OriginalPrice = num.Zero()
 					m.broker.Send(events.NewOrderEvent(ctx, order))
 					parked = append(parked, order)
 				}
 			} else {
 				// Repriced so all good make sure status is correct
 				order.Price = price.Clone()
+				order.OriginalPrice = price.Clone()
+				order.OriginalPrice.Div(order.OriginalPrice, m.priceFactor)
 				order.Status = types.OrderStatusParked
 				toSubmit = append(toSubmit, order)
 			}
