ee57421c5316aa7ae023fb66daa116ff33787739
Jeremy Letang <me@jeremyletang.com>
2022-03-29 15:42:21 +0100

    fix: instantiate the idgen when call RemoveExpiredOrders

--- execution/market.go
+++ execution/market.go
@@ -2855,6 +2855,10 @@ func (m *Market) RemoveExpiredOrders(
 		return nil, ErrTradingNotAllowed
 	}
 
+	_, blockHash := vegacontext.TraceIDFromContext(ctx)
+	m.idgen = idgeneration.New(blockHash)
+	defer func() { m.idgen = nil }()
+
 	expired := []*types.Order{}
 	evts := []events.Event{}
 	for _, orderID := range m.expiringOrders.Expire(timestamp) {
