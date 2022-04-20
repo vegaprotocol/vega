866131fce027f655a57f8245e60d2c652d17326c
Jeremy Letang <me@jeremyletang.com>
2022-04-20 11:20:52 +0200

    feat: move enabled from limits (assets / markets) to network parameters)

--- limits/genesis_state.go
+++ limits/genesis_state.go
@@ -3,17 +3,14 @@ package limits
 import (
 	"encoding/json"
 	"errors"
-	"time"
 )
 
 var ErrNoLimitsGenesisState = errors.New("no limits genesis state")
 
 type GenesisState struct {
-	ProposeMarketEnabled     bool       `json:"propose_market_enabled"`
-	ProposeAssetEnabled      bool       `json:"propose_asset_enabled"`
-	ProposeMarketEnabledFrom *time.Time `json:"propose_market_enabled_from,omitempty"`
-	ProposeAssetEnabledFrom  *time.Time `json:"propose_asset_enabled_from,omitempty"`
-	BootstrapBlockCount      uint16     `json:"bootstrap_block_count"`
+	ProposeMarketEnabled bool   `json:"propose_market_enabled"`
+	ProposeAssetEnabled  bool   `json:"propose_asset_enabled"`
+	BootstrapBlockCount  uint16 `json:"bootstrap_block_count"`
 }
 
 func DefaultGenesisState() GenesisState {
