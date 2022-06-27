2c0d94d6b1ce03e733bed95f0fdf5efdaacbc69f
Valentin Trinque <val.trinque@gmail.com>
2022-06-27 16:11:36 +0200

    feat: add REST endpoint to list OracleData

--- service/mocks/position_mock.go
+++ service/mocks/position_mock.go
@@ -118,25 +118,24 @@ func (m *MockPositionStore) GetByParty(arg0 context.Context, arg1 entities.Party
 	return ret0, ret1
 }
 
-// GetByPartyConnection mocks base method.
-func (m *MockPositionStore) GetByPartyConnection(arg0 context.Context, arg1 entities.PartyID, arg2 entities.MarketID, arg3 entities.CursorPagination) ([]entities.Position, entities.PageInfo, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GetByPartyConnection", arg0, arg1)
-	ret0, _ := ret[0].([]entities.Position)
-	ret1, _ := ret[1].(entities.PageInfo)
-	ret2, _ := ret[2].(error)
-	return ret0, ret1, ret2
-}
-
 // GetByParty indicates an expected call of GetByParty.
 func (mr *MockPositionStoreMockRecorder) GetByParty(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetByParty", reflect.TypeOf((*MockPositionStore)(nil).GetByParty), arg0, arg1)
 }
 
+// GetByPartyConnection mocks base method.
+func (m *MockPositionStore) GetByPartyConnection(arg0 context.Context, arg1 entities.PartyID, arg2 entities.MarketID, arg3 entities.CursorPagination) ([]entities.Position, entities.PageInfo, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetByPartyConnection", arg0, arg1, arg2, arg3)
+	ret0, _ := ret[0].([]entities.Position)
+	ret1, _ := ret[1].(entities.PageInfo)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
 
 // GetByPartyConnection indicates an expected call of GetByPartyConnection.
-func (mr *MockPositionStoreMockRecorder) GetByPartyConnection(arg0, arg1 interface{}) *gomock.Call {
+func (mr *MockPositionStoreMockRecorder) GetByPartyConnection(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetByPartyConnection", reflect.TypeOf((*MockPositionStore)(nil).GetByPartyConnection), arg0, arg1)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetByPartyConnection", reflect.TypeOf((*MockPositionStore)(nil).GetByPartyConnection), arg0, arg1, arg2, arg3)
 }
--- sqlsubscribers/mocks/node_mock.go
+++ sqlsubscribers/mocks/node_mock.go
@@ -35,6 +35,20 @@ func (m *MockNodeStore) EXPECT() *MockNodeStoreMockRecorder {
 	return m.recorder
 }
 
+// AddNodeAnnoucedEvent mocks base method.
+func (m *MockNodeStore) AddNodeAnnoucedEvent(arg0 context.Context, arg1 entities.NodeID, arg2 *entities.ValidatorUpdateAux) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "AddNodeAnnoucedEvent", arg0, arg1, arg2)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// AddNodeAnnoucedEvent indicates an expected call of AddNodeAnnoucedEvent.
+func (mr *MockNodeStoreMockRecorder) AddNodeAnnoucedEvent(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AddNodeAnnoucedEvent", reflect.TypeOf((*MockNodeStore)(nil).AddNodeAnnoucedEvent), arg0, arg1, arg2)
+}
+
 // UpdatePublicKey mocks base method.
 func (m *MockNodeStore) UpdatePublicKey(arg0 context.Context, arg1 *entities.KeyRotation) error {
 	m.ctrl.T.Helper()
