a8ab3f7cd562dedba3ff0bf677d41482eacc0c8c
Jeremy Letang <me@jeremyletang.com>
2022-07-01 11:23:43 +0200

    chore: fix version

--- cmd/vega/main.go
+++ cmd/vega/main.go
@@ -29,7 +29,7 @@ import (
 
 var (
 	CLIVersionHash = ""
-	CLIVersion     = "v0.53.0-dev"
+	CLIVersion     = "v0.53.0+dev"
 )
 
 // Subcommand is the signature of a sub command that can be registered.
