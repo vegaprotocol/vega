197f3c544ea2f9c9a923b07ab9d905736d4d2c1f
Valentin Trinque <val.trinque@gmail.com>
2022-07-06 16:00:58 +0200

    feat: Align `vegawallet` and core versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@
 - [5428](https://github.com/vegaprotocol/vega/pull/5428) - Update contributor information
 - [5519](https://github.com/vegaprotocol/vega/pull/5519) - Add `--genesis-file` option to the `load_checkpoint` command
 - [5525](https://github.com/vegaprotocol/vega/pull/5525) - Release `vegawallet` from the core
+- [5524](https://github.com/vegaprotocol/vega/pull/5524) - Align `vegawallet` and core versions
 
 ### 🐛 Fixes
 - [5476](https://github.com/vegaprotocol/vega/issues/5476) - Include settlement price in snapshot
--- wallet/version/version.go
+++ wallet/version/version.go
@@ -1,26 +1,24 @@
 package version
 
 import (
+	"runtime/debug"
+
 	vgversion "code.vegaprotocol.io/shared/libs/version"
 
 	"github.com/blang/semver/v4"
 )
 
 const (
-	ReleasesAPI        = "https://api.github.com/repos/vegaprotocol/vegawallet/releases"
-	ReleasesURL        = "https://github.com/vegaprotocol/vegawallet/releases"
-	defaultVersionHash = "unknown"
-	defaultVersion     = "v0.16.2+dev"
+	ReleasesAPI = "https://api.github.com/repos/vegaprotocol/vega/releases"
+	ReleasesURL = "https://github.com/vegaprotocol/vega/releases"
 )
 
 var (
-	// VersionHash specifies the git commit used to build the application.
-	// See VERSION_HASH in Makefile for details.
-	VersionHash = defaultVersionHash
+	// GitHash specifies the git commit used to build the application.
+	GitHash = "unknown"
 
 	// Version specifies the version used to build the application.
-	// See VERSION in Makefile for details.
-	Version = defaultVersion
+	Version = "v0.53.0+dev"
 )
 
 func IsUnreleased() bool {
@@ -35,10 +33,27 @@ type GetVersionResponse struct {
 func GetVersionInfo() *GetVersionResponse {
 	return &GetVersionResponse{
 		Version: Version,
-		GitHash: VersionHash,
+		GitHash: GitHash,
 	}
 }
 
 func Check(releasesGetterFn vgversion.ReleasesGetter, currentRelease string) (*semver.Version, error) {
 	return vgversion.Check(releasesGetterFn, currentRelease)
+}
+
+func init() {
+	info, _ := debug.ReadBuildInfo()
+	modified := false
+
+	for _, v := range info.Settings {
+		if v.Key == "vcs.revision" {
+			GitHash = v.Value
+		}
+		if v.Key == "vcs.modified" {
+			modified = true
+		}
+	}
+	if modified {
+		GitHash += "-modified"
+	}
 }
