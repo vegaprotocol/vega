bb3f72a1fd3428e38a55d21c4a9c4a91b4c4ec7f
Jeremy Letang <me@jeremyletang.com>
2022-07-28 15:47:56 +0200

    chore: remove all possible conflictings objects

--- .github/PULL_REQUEST_TEMPLATE/pull_request_template.md
+++
@@ -1,3 +0,0 @@
-Please remember to create a new branch on github.com/vegaprotocol/system-tests in order to create/fix/update the matching tests for your changes.
-
-Closes #XXX
--- script/postgres-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run --rm \
-           -e POSTGRES_USER=vega \
-           -e POSTGRES_PASSWORD=vega \
-           -e POSTGRES_DB=vega \
-           -p 5432:5432 \
-           timescale/timescaledb:2.6.0-pg14
