8bd874798a2c2b4adba40c29d88552266ef5e97e
Tan Quach <tan@vegaprotocol.io>
2022-10-17 16:52:05 +0100

    fix: ledger entries graphql query errors

--- datanode/sqlstore/ledger.go
+++ datanode/sqlstore/ledger.go
@@ -190,31 +190,32 @@ type ledgerEntriesScanned struct {
 	VegaTime time.Time
 
 	Quantity     decimal.Decimal
-	AccountType  *types.AccountType
+	AccountType  types.AccountType
 	TransferType entities.LedgerMovementType
 
-	PartyID  *entities.PartyID
-	AssetID  *entities.AssetID
-	MarketID *entities.MarketID
+	PartyID   entities.PartyID
+	AssetID   entities.AssetID
+	MarketID  entities.MarketID
+	AccountID entities.AccountID
 }
 
 func parseScanned(scanned []ledgerEntriesScanned) []entities.AggregatedLedgerEntries {
 	ledgerEntries := []entities.AggregatedLedgerEntries{}
 	if len(scanned) > 0 {
-		for i, s := range scanned {
+		for i := range scanned {
 			ledgerEntries = append(ledgerEntries, entities.AggregatedLedgerEntries{
-				VegaTime: s.VegaTime,
-				Quantity: s.Quantity,
-				PartyID:  s.PartyID,
-				AssetID:  s.AssetID,
-				MarketID: s.MarketID,
+				VegaTime: scanned[i].VegaTime,
+				Quantity: scanned[i].Quantity,
+				PartyID:  &scanned[i].PartyID,
+				AssetID:  &scanned[i].AssetID,
+				MarketID: &scanned[i].MarketID,
 			})
 
-			if s.AccountType != nil {
-				ledgerEntries[i].AccountType = s.AccountType
+			if scanned[i].AccountType != types.AccountTypeUnspecified {
+				ledgerEntries[i].AccountType = &scanned[i].AccountType
 			}
 
-			tt := s.TransferType
+			tt := scanned[i].TransferType
 			ledgerEntries[i].TransferType = &tt
 		}
 	}
