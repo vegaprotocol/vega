3346905107aa02f40942d09fcdb3e19a6dbebb68
Elias Van Ootegem <elias@vega.xyz>
2022-11-12 11:07:04 +0000

    chore: add next MTM to gql schema
    Signed-off-by: Elias Van Ootegem <elias@vega.xyz>

--- datanode/gateway/graphql/generated.go
+++ datanode/gateway/graphql/generated.go
@@ -756,6 +756,7 @@ type ComplexityRoot struct {
 		MarketTradingMode         func(childComplexity int) int
 		MarketValueProxy          func(childComplexity int) int
 		MidPrice                  func(childComplexity int) int
+		NextMarkToMarket          func(childComplexity int) int
 		OpenInterest              func(childComplexity int) int
 		PriceMonitoringBounds     func(childComplexity int) int
 		StaticMidPrice            func(childComplexity int) int
@@ -1887,6 +1888,7 @@ type MarketDataResolver interface {
 	PriceMonitoringBounds(ctx context.Context, obj *vega.MarketData) ([]*PriceMonitoringBounds, error)
 
 	LiquidityProviderFeeShare(ctx context.Context, obj *vega.MarketData) ([]*LiquidityProviderFeeShare, error)
+	NextMarkToMarket(ctx context.Context, obj *vega.MarketData) (string, error)
 }
 type MarketDepthResolver interface {
 	Market(ctx context.Context, obj *vega.MarketDepth) (*vega.Market, error)
@@ -4782,6 +4784,13 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.MarketData.MidPrice(childComplexity), true
+
+	case "MarketData.nextMarkToMarket":
+		if e.complexity.MarketData.NextMarkToMarket == nil {
+			break
+		}
+
+		return e.complexity.MarketData.NextMarkToMarket(childComplexity), true
 
 	case "MarketData.openInterest":
 		if e.complexity.MarketData.OpenInterest == nil {
@@ -27270,6 +27279,8 @@ func (ec *executionContext) fieldContext_Market_data(ctx context.Context, field 
 				return ec.fieldContext_MarketData_marketValueProxy(ctx, field)
 			case "liquidityProviderFeeShare":
 				return ec.fieldContext_MarketData_liquidityProviderFeeShare(ctx, field)
+			case "nextMarkToMarket":
+				return ec.fieldContext_MarketData_nextMarkToMarket(ctx, field)
 			}
 			return nil, fmt.Errorf("no field named %q was found under type MarketData", field.Name)
 		},
@@ -28822,6 +28833,50 @@ func (ec *executionContext) fieldContext_MarketData_liquidityProviderFeeShare(ct
 	return fc, nil
 }
 
+func (ec *executionContext) _MarketData_nextMarkToMarket(ctx context.Context, field graphql.CollectedField, obj *vega.MarketData) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_MarketData_nextMarkToMarket(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return ec.resolvers.MarketData().NextMarkToMarket(rctx, obj)
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(string)
+	fc.Result = res
+	return ec.marshalNString2string(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_MarketData_nextMarkToMarket(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "MarketData",
+		Field:      field,
+		IsMethod:   true,
+		IsResolver: true,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			return nil, errors.New("field of type String does not have child fields")
+		},
+	}
+	return fc, nil
+}
+
 func (ec *executionContext) _MarketDataCommitments_sells(ctx context.Context, field graphql.CollectedField, obj *MarketDataCommitments) (ret graphql.Marshaler) {
 	fc, err := ec.fieldContext_MarketDataCommitments_sells(ctx, field)
 	if err != nil {
@@ -29109,6 +29164,8 @@ func (ec *executionContext) fieldContext_MarketDataEdge_node(ctx context.Context
 				return ec.fieldContext_MarketData_marketValueProxy(ctx, field)
 			case "liquidityProviderFeeShare":
 				return ec.fieldContext_MarketData_liquidityProviderFeeShare(ctx, field)
+			case "nextMarkToMarket":
+				return ec.fieldContext_MarketData_nextMarkToMarket(ctx, field)
 			}
 			return nil, fmt.Errorf("no field named %q was found under type MarketData", field.Name)
 		},
@@ -44262,6 +44319,8 @@ func (ec *executionContext) fieldContext_Query_getMarketDataHistoryByID(ctx cont
 				return ec.fieldContext_MarketData_marketValueProxy(ctx, field)
 			case "liquidityProviderFeeShare":
 				return ec.fieldContext_MarketData_liquidityProviderFeeShare(ctx, field)
+			case "nextMarkToMarket":
+				return ec.fieldContext_MarketData_nextMarkToMarket(ctx, field)
 			}
 			return nil, fmt.Errorf("no field named %q was found under type MarketData", field.Name)
 		},
@@ -65635,6 +65694,26 @@ func (ec *executionContext) _MarketData(ctx context.Context, sel ast.SelectionSe
 					}
 				}()
 				res = ec._MarketData_liquidityProviderFeeShare(ctx, field, obj)
+				return res
+			}
+
+			out.Concurrently(i, func() graphql.Marshaler {
+				return innerFunc(ctx)
+
+			})
+		case "nextMarkToMarket":
+			field := field
+
+			innerFunc := func(ctx context.Context) (res graphql.Marshaler) {
+				defer func() {
+					if r := recover(); r != nil {
+						ec.Error(ctx, ec.Recover(ctx, r))
+					}
+				}()
+				res = ec._MarketData_nextMarkToMarket(ctx, field, obj)
+				if res == graphql.Null {
+					atomic.AddUint32(&invalids, 1)
+				}
 				return res
 			}
 
