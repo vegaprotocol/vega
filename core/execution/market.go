3392360337381ea49afab41f823fd2de84017c04
Elias Van Ootegem <elias@vega.xyz>
2022-11-18 10:59:55 +0000

    chore: remove dead/commented code
    Signed-off-by: Elias Van Ootegem <elias@vega.xyz>

--- core/execution/market.go
+++ core/execution/market.go
@@ -1677,17 +1677,6 @@ func (m *Market) handleConfirmation(ctx context.Context, conf *types.OrderConfir
 			ctx, orderUpdates, false)
 	}
 
-	// if !end {
-	// orderUpdates = m.confirmMTM(ctx, conf.Order)
-	// }
-	// } else {
-	// we had no trade, but still want to register this position in the settlement
-	// engine I guess
-	// party := conf.Order.Party
-	// if pos, ok := m.position.GetPositionByPartyID(party); ok {
-	// m.settlement.AddPosition(party, pos)
-	// }
-
 	return orderUpdates
 }
 
