2eceb3e869f6660e5ab3d9bd843b6b472d5ba5a9
Matthew Pendrey <matthew@vegaprotocol.io>
2022-12-09 13:35:17 +0000

    fix: dehistory load was using incorrect database version when selecting constraints meta data

--- datanode/dehistory/testdata/testmigration.sql
+++ datanode/dehistory/testdata/testmigration.sql
@@ -5,5 +5,18 @@ Update trades set testref = 'nondefaulttestref';
 ALTER TABLE trades ALTER COLUMN testref SET DEFAULT 'defaulttestref';
 
 
+create table transfers2 (
+                            id bytea not null,
+                            tx_hash bytea not null,
+                            vega_time timestamp with time zone not null,
+                            from_account_id bytea NOT NULL REFERENCES accounts(id),
+                            to_account_id bytea NOT NULL REFERENCES accounts(id),
+                            primary key (id, vega_time)
+);
+
+create index on transfers2 (from_account_id);
+create index on transfers2 (to_account_id);
+
 -- +goose Down
 ALTER TABLE trades DROP COLUMN testref;
+DROP TABLE transfers2;
