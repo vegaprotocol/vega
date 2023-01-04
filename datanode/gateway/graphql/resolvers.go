28e812aa458f1ec25b155e6ded52024e7504b531
Tan Quach <tan@vegaprotocol.io>
2023-01-04 13:54:13 +0000

    feat: support filter by market id in party ordersConnection

--- datanode/gateway/graphql/resolvers.go
+++ datanode/gateway/graphql/resolvers.go
@@ -1328,7 +1328,7 @@ func (r *myPartyResolver) MarginsConnection(ctx context.Context, party *types.Pa
 }
 
 func (r *myPartyResolver) OrdersConnection(ctx context.Context, party *types.Party, dateRange *v2.DateRange,
-	pagination *v2.Pagination, filter *v2.OrderFilter,
+	pagination *v2.Pagination, filter *v2.OrderFilter, marketId *string,
 ) (*v2.OrderConnection, error) {
 	if party == nil {
 		return nil, errors.New("party is required")
@@ -1338,6 +1338,7 @@ func (r *myPartyResolver) OrdersConnection(ctx context.Context, party *types.Par
 		Pagination: pagination,
 		DateRange:  dateRange,
 		Filter:     filter,
+		MarketId:   marketId,
 	}
 	res, err := r.tradingDataClientV2.ListOrders(ctx, &req)
 	if err != nil {
