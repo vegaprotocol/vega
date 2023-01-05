fbc4af5259c0fb6e2cac1853a1eaeb39742e7ab9
Karel Moravec <moravec.wdd@gmail.com>
2023-01-05 15:28:11 +0100

    Update visor/config/visor_config.go
    Co-authored-by: candida-d <62548908+candida-d@users.noreply.github.com>

--- visor/config/visor_config.go
+++ visor/config/visor_config.go
@@ -84,9 +84,9 @@ type VisorConfigFile struct {
 	MaxNumberOfFirstConnectionRetries int `toml:"maxNumberOfFirstConnectionRetries,optional"`
 	/*
 		description: |
-			Visor at it's core starts and manages processes of provided binaries.
-			This alows to define maximum number of restarts in case that any of
-			the processes has failed before the Visor process fails.
+			Visor starts and manages the processes of provided binaries.
+			This allows a user to define the maximum number of restarts in case any of
+			the processes have failed before the Visor process fails.
 		note: |
 			The amount of time Visor should wait between restarts can be set by `maxNumberOfRestarts`.
 		default: 3
