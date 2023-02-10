4fec00fa57b41e9a2e92b5221dfd814c802e9794
Aleksandar Sukovic <aleksandar.sukovic@gmail.com>
2023-02-10 14:07:59 +0000

    fix: Remove some unused loggers

--- datanode/service/stubs.go
+++ datanode/service/stubs.go
@@ -14,7 +14,6 @@ package service
 
 import (
 	"code.vegaprotocol.io/vega/datanode/sqlstore"
-	"code.vegaprotocol.io/vega/logging"
 )
 
 type (
@@ -44,78 +43,78 @@ type (
 	Node         struct{ *sqlstore.Node }
 )
 
-func NewAsset(store *sqlstore.Assets, log *logging.Logger) *Asset {
+func NewAsset(store *sqlstore.Assets) *Asset {
 	return &Asset{Assets: store}
 }
 
-func NewBlock(store *sqlstore.Blocks, log *logging.Logger) *Block {
+func NewBlock(store *sqlstore.Blocks) *Block {
 	return &Block{Blocks: store}
 }
 
-func NewParty(store *sqlstore.Parties, log *logging.Logger) *Party {
+func NewParty(store *sqlstore.Parties) *Party {
 	return &Party{Parties: store}
 }
 
-func NewNetworkLimits(store *sqlstore.NetworkLimits, log *logging.Logger) *NetworkLimits {
+func NewNetworkLimits(store *sqlstore.NetworkLimits) *NetworkLimits {
 	return &NetworkLimits{NetworkLimits: store}
 }
 
-func NewEpoch(store *sqlstore.Epochs, log *logging.Logger) *Epoch {
+func NewEpoch(store *sqlstore.Epochs) *Epoch {
 	return &Epoch{Epochs: store}
 }
 
-func NewDeposit(store *sqlstore.Deposits, log *logging.Logger) *Deposit {
+func NewDeposit(store *sqlstore.Deposits) *Deposit {
 	return &Deposit{Deposits: store}
 }
 
-func NewWithdrawal(store *sqlstore.Withdrawals, log *logging.Logger) *Withdrawal {
+func NewWithdrawal(store *sqlstore.Withdrawals) *Withdrawal {
 	return &Withdrawal{Withdrawals: store}
 }
 
-func NewRiskFactor(store *sqlstore.RiskFactors, log *logging.Logger) *RiskFactor {
+func NewRiskFactor(store *sqlstore.RiskFactors) *RiskFactor {
 	return &RiskFactor{RiskFactors: store}
 }
 
-func NewNetworkParameter(store *sqlstore.NetworkParameters, log *logging.Logger) *NetworkParameter {
+func NewNetworkParameter(store *sqlstore.NetworkParameters) *NetworkParameter {
 	return &NetworkParameter{NetworkParameters: store}
 }
 
-func NewCheckpoint(store *sqlstore.Checkpoints, log *logging.Logger) *Checkpoint {
+func NewCheckpoint(store *sqlstore.Checkpoints) *Checkpoint {
 	return &Checkpoint{Checkpoints: store}
 }
 
-func NewOracleSpec(store *sqlstore.OracleSpec, log *logging.Logger) *OracleSpec {
+func NewOracleSpec(store *sqlstore.OracleSpec) *OracleSpec {
 	return &OracleSpec{OracleSpec: store}
 }
 
-func NewOracleData(store *sqlstore.OracleData, log *logging.Logger) *OracleData {
+func NewOracleData(store *sqlstore.OracleData) *OracleData {
 	return &OracleData{OracleData: store}
 }
 
-func NewLiquidityProvision(store *sqlstore.LiquidityProvision, log *logging.Logger) *LiquidityProvision {
+func NewLiquidityProvision(store *sqlstore.LiquidityProvision) *LiquidityProvision {
 	return &LiquidityProvision{LiquidityProvision: store}
 }
 
-func NewTransfer(store *sqlstore.Transfers, log *logging.Logger) *Transfer {
+func NewTransfer(store *sqlstore.Transfers) *Transfer {
 	return &Transfer{Transfers: store}
 }
 
-func NewStakeLinking(store *sqlstore.StakeLinking, log *logging.Logger) *StakeLinking {
+func NewStakeLinking(store *sqlstore.StakeLinking) *StakeLinking {
 	return &StakeLinking{StakeLinking: store}
 }
 
-func NewNotary(store *sqlstore.Notary, log *logging.Logger) *Notary {
+func NewNotary(store *sqlstore.Notary) *Notary {
 	return &Notary{Notary: store}
 }
 
-func NewMultiSig(store *sqlstore.ERC20MultiSigSignerEvent, log *logging.Logger) *MultiSig {
+func NewMultiSig(store *sqlstore.ERC20MultiSigSignerEvent) *MultiSig {
 	return &MultiSig{ERC20MultiSigSignerEvent: store}
 }
 
-func NewKeyRotations(store *sqlstore.KeyRotations, log *logging.Logger) *KeyRotations {
+func NewKeyRotations(store *sqlstore.KeyRotations) *KeyRotations {
 	return &KeyRotations{KeyRotations: store}
 }
 
-func NewNode(store *sqlstore.Node, log *logging.Logger) *Node {
+func NewNode(store *sqlstore.Node) *Node {
 	return &Node{Node: store}
 }
