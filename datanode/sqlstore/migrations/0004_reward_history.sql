d73d8a6902174a673f8d32ad5a5f58198dfa47a3
Tan Quach <tan@vegaprotocol.io>
2023-02-13 13:05:31 +0000

    chore: remove unnecessary foreign key constraint on reward table

--- datanode/sqlstore/migrations/0004_reward_history.sql
+++ datanode/sqlstore/migrations/0004_reward_history.sql
@@ -1,3 +1,5 @@
 -- +goose Up
 
-select create_hypertable('rewards', 'vega_time', chunk_time_interval => INTERVAL '1 day');
+ALTER TABLE rewards DROP CONSTRAINT IF EXISTS rewards_party_id_fkey;
+
+SELECT create_hypertable('rewards', 'vega_time', chunk_time_interval => INTERVAL '1 day', migrate_data => true);
