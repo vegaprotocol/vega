786002d46a312afc1bfa34a059cba67f3226bbc6
Valentin Trinque <val.trinque@gmail.com>
2023-02-23 16:54:20 +0100

    feat: Add `vega wallet service config locate` CLI that returns the location of the service configuration file

---
+++ cmd/vegawallet/commands/service_config.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"io"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCmdServiceConfig(w io.Writer, rf *RootFlags) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "config",
+		Short: "Manage the Vega wallet's service configuration",
+		Long:  "Manage the Vega wallet's service configuration",
+	}
+
+	cmd.AddCommand(NewCmdLocateServiceConfig(w, rf))
+	return cmd
+}
