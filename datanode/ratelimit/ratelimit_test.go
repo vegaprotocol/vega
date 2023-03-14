0f2aff7f009ac199f2bde89829b17d8210ebd40a
Tan Quach <tan@vegaprotocol.io>
2023-03-14 15:29:07 +0000

    fix: flaky race conditions in test

--- datanode/ratelimit/ratelimit_test.go
+++ datanode/ratelimit/ratelimit_test.go
@@ -3,6 +3,7 @@ package ratelimit
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 	"time"
 
@@ -12,13 +13,20 @@ import (
 )
 
 func TestRateLimit_HTTPMiddleware(t *testing.T) {
+	mu := sync.Mutex{}
 	count := 0
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
 		count++
 	})
+
 	req := httptest.NewRequest(http.MethodGet, "http://localhost:8080/test", nil)
 
 	cfg := NewDefaultConfig()
+	const burstSize = 20
+	cfg.Burst = burstSize
+
 	r := NewFromConfig(&cfg, logging.NewTestLogger())
 
 	limiter := r.HTTPMiddleware(handler)
@@ -33,7 +41,7 @@ func TestRateLimit_HTTPMiddleware(t *testing.T) {
 		res := httptest.NewRecorder()
 		limiter.ServeHTTP(res, req)
 		assert.Equal(t, http.StatusTooManyRequests, res.Code)
-		assert.Equal(t, 100, count)
+		assert.Equal(t, burstSize, count)
 	}
 
 	// We should have been banned after this so wait a second, then request again,
