d768f508b6bdf0f73c5c4906d8ec159edc2d3621
wwestgarth <william@vega.xyz>
2023-03-16 10:04:33 +0000

    chore: improve interaction text when checking transaction

--- cmd/vegawallet/commands/service_run.go
+++ cmd/vegawallet/commands/service_run.go
@@ -576,7 +576,7 @@ func handleAPIv2Request(interaction interactor.Interaction, responseChan chan<- 
 			},
 		}
 	case interactor.RequestTransactionReviewForChecking:
-		str := p.String().BlueArrow().Text("The application \"").InfoText(data.Hostname).Text("\" wants to send the following transaction to be checked:").NextLine()
+		str := p.String().BlueArrow().Text("The application \"").InfoText(data.Hostname).Text("\" wants to check the following transaction:").NextLine()
 		str.Pad().Text("Using the key: ").InfoText(data.PublicKey).NextLine()
 		str.Pad().Text("From the wallet: ").InfoText(data.Wallet).NextLine()
 		fmtCmd := strings.Replace("  "+data.Transaction, "\n", "\n  ", -1)
@@ -584,12 +584,12 @@ func handleAPIv2Request(interaction interactor.Interaction, responseChan chan<- 
 		p.Print(str)
 		approved := true
 		if !enableAutomaticConsent {
-			approved = yesOrNo(p.String().QuestionMark().Text("Do you want to sign this transaction?"), p)
+			approved = yesOrNo(p.String().QuestionMark().Text("Do you allow the network to check this transaction?"), p)
 		}
 		if approved {
-			p.Print(p.String().CheckMark().Text("Signing approved.").NextLine())
+			p.Print(p.String().CheckMark().Text("Checking approved.").NextLine())
 		} else {
-			p.Print(p.String().CrossMark().Text("Signing rejected.").NextLine())
+			p.Print(p.String().CrossMark().Text("Checking rejected.").NextLine())
 		}
 		responseChan <- interactor.Interaction{
 			TraceID: interaction.TraceID,
