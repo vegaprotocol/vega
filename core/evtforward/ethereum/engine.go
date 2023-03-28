214c6af2f2d47ed7410b2227370d86b712d4980b
Jeremy Letang <me@jeremyletang.com>
2023-03-28 16:11:38 +0100

    fix: increase polling delay blocks in evtforwads
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- core/evtforward/ethereum/engine.go
+++ core/evtforward/ethereum/engine.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	engineLogger      = "engine"
-	maxEthereumBlocks = 300 // an hour worth of blocks?
+	maxEthereumBlocks = 1000 // 3+ hour worth of blocks?
 )
 
 //go:generate go run github.com/golang/mock/mockgen -destination mocks/forwarder_mock.go -package mocks code.vegaprotocol.io/vega/core/evtforward/ethereum Forwarder
