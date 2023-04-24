ffab8ed34a71d2d721ef28918069253868be0acb
Jiajia-Cui <jiajia@vega.xyz>
2023-04-24 14:06:22 +0100

    feat: datanode integration test

---
+++ datanode/integration/estimate_position_test.go
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Gobalsky Labs Limited
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE.DATANODE file and at https://www.mariadb.com/bsl11.
+//
+// Change Date: 18 months from the later of the date of the first publicly
+// available Distribution of this version of the repository, and 25 June 2022.
+//
+// On the date above, in accordance with the Business Source License, use
+// of this software will be governed by version 3 or later of the GNU General
+// Public License.
+
+package integration_test
+
+import "testing"
+
+func TestEstimatePosition(t *testing.T) {
+  queries := map[string]string{
+    "Estimate_Position": `{ partiesConnection{ edges { node { id accountsConnection { edges { node { type asset{ id } market{ id } balance } } } } } } }`,
+}
+
+for name, query := range queries {
+  t.Run(name, func(t *testing.T) {
+    assertGraphQLQueriesReturnSame(t, query)
+  })
+}
+}
