47c042f8188ef7d34193d32d55e71c6cb6b5d05b
Jeremy Letang <me@jeremyletang.com>
2023-05-02 16:52:11 +0100

    feat: add stop_orders_submission command validation
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

---
+++ commands/stop_order_submission_test.go
@@ -0,0 +1,249 @@
+package commands_test
+
+import (
+	"testing"
+
+	"code.vegaprotocol.io/vega/commands"
+	"code.vegaprotocol.io/vega/libs/ptr"
+	"code.vegaprotocol.io/vega/protos/vega"
+	commandspb "code.vegaprotocol.io/vega/protos/vega/commands/v1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckStopOrdersStubmission(t *testing.T) {
+	cases := []struct {
+		submission commandspb.StopOrdersSubmission
+		errStr     string
+	}{
+		{
+			submission: commandspb.StopOrdersSubmission{},
+			errStr:     "must have at least one of rises above or falls bellow",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.trigger (must have a stop order trigger)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_Price{
+						Price: "",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.trigger.price (is required)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_Price{
+						Price: "-1",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.trigger.price (must be positive)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_Price{
+						Price: "asdsad",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.trigger.price (not a valid integer)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_Price{
+						Price: "100",
+					},
+				},
+			},
+			errStr: "order_submission (is required)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.trigger.trailing_percent_offset (is required)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.trigger.trailing_percent_offset (must be > 0 and < 1)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "1",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.trigger.trailing_percent_offset (must be > 0 and < 1)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.89",
+					},
+				},
+			},
+			errStr: "order_submission (is required)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "132213ds",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.trigger.trailing_percent_offset (not a valid float)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					ExpiresAt: ptr.From(int64(1000)),
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.1",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.expiry_strategy (expiry strategy required when expires_at set)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					ExpiresAt:      ptr.From(int64(-1000)),
+					ExpiryStrategy: ptr.From(vega.StopOrder_EXPIRY_STRATEGY_CANCELS),
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.1",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.expires_at (must be positive)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					ExpiresAt:      ptr.From(int64(1000)),
+					ExpiryStrategy: ptr.From(vega.StopOrder_ExpiryStrategy(-1)),
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.1",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.expiry_strategy (is not a valid value)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					ExpiresAt:      ptr.From(int64(1000)),
+					ExpiryStrategy: ptr.From(vega.StopOrder_ExpiryStrategy(-1)),
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.1",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.expiry_strategy (is not a valid value)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					ExpiresAt:      ptr.From(int64(1000)),
+					ExpiryStrategy: ptr.From(vega.StopOrder_EXPIRY_STRATEGY_UNSPECIFIED),
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.1",
+					},
+				},
+			},
+			errStr: "order_submission (is required), stop_orders_submission.rises_below.expiry_strategy (is required)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					ExpiresAt:      ptr.From(int64(1000)),
+					ExpiryStrategy: ptr.From(vega.StopOrder_EXPIRY_STRATEGY_CANCELS),
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.1",
+					},
+				},
+			},
+			errStr: "order_submission (is required)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					OrderSubmission: &commandspb.OrderSubmission{
+						MarketId:    "f9982447fb4128f9968f9981612c5ea85d19b62058ec2636efc812dcbbc745ca",
+						Side:        vega.Side_SIDE_BUY,
+						Size:        100,
+						TimeInForce: vega.Order_TIME_IN_FORCE_IOC,
+						Type:        vega.Order_TYPE_MARKET,
+					},
+					ExpiresAt:      ptr.From(int64(1000)),
+					ExpiryStrategy: ptr.From(vega.StopOrder_EXPIRY_STRATEGY_CANCELS),
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.1",
+					},
+				},
+			},
+			errStr: "stop_orders_submission.rises_below.order_submission.reduce_only (must be reduce only)",
+		},
+		{
+			submission: commandspb.StopOrdersSubmission{
+				RisesAbove: &commandspb.StopOrderSetup{
+					OrderSubmission: &commandspb.OrderSubmission{
+						MarketId:    "f9982447fb4128f9968f9981612c5ea85d19b62058ec2636efc812dcbbc745ca",
+						Side:        vega.Side_SIDE_BUY,
+						Size:        100,
+						TimeInForce: vega.Order_TIME_IN_FORCE_IOC,
+						Type:        vega.Order_TYPE_MARKET,
+						ReduceOnly:  true,
+					},
+					ExpiresAt:      ptr.From(int64(1000)),
+					ExpiryStrategy: ptr.From(vega.StopOrder_EXPIRY_STRATEGY_CANCELS),
+					Trigger: &commandspb.StopOrderSetup_TrailingPercentOffset{
+						TrailingPercentOffset: "0.1",
+					},
+				},
+			},
+			errStr: "",
+		},
+	}
+
+	for n, c := range cases {
+		if len(c.errStr) <= 0 {
+			assert.NoError(t, commands.CheckStopOrdersSubmission(&c.submission), n)
+			continue
+		}
+
+		assert.Contains(t, checkStopOrdersSubmission(&c.submission).Error(), c.errStr, n)
+	}
+}
+
+func checkStopOrdersSubmission(cmd *commandspb.StopOrdersSubmission) commands.Errors {
+	err := commands.CheckStopOrdersSubmission(cmd)
+
+	e, ok := err.(commands.Errors)
+	if !ok {
+		return commands.NewErrors()
+	}
+
+	return e
+}
---
+++ commands/stop_orders_submission.go
@@ -0,0 +1,95 @@
+package commands
+
+import (
+	"fmt"
+	"math/big"
+
+	"code.vegaprotocol.io/vega/libs/num"
+	types "code.vegaprotocol.io/vega/protos/vega"
+	commandspb "code.vegaprotocol.io/vega/protos/vega/commands/v1"
+)
+
+func CheckStopOrdersSubmission(cmd *commandspb.StopOrdersSubmission) error {
+	return checkStopOrdersSubmission(cmd).ErrorOrNil()
+}
+
+func checkStopOrdersSubmission(cmd *commandspb.StopOrdersSubmission) Errors {
+	errs := NewErrors()
+
+	if cmd == nil {
+		return errs.FinalAddForProperty("stop_orders_submission", ErrIsRequired)
+	}
+
+	if cmd.FallsBelow != nil {
+		checkStopOrderSetup(
+			"stop_orders_submission.falls_below", errs, cmd.FallsBelow)
+	}
+	if cmd.RisesAbove != nil {
+		checkStopOrderSetup(
+			"stop_orders_submission.rises_below", errs, cmd.RisesAbove)
+	}
+
+	if cmd.FallsBelow == nil && cmd.RisesAbove == nil {
+		return errs.FinalAdd(ErrMustHaveAtLeastOneOfRisesAboveOrFallsBelow)
+	}
+
+	return errs
+}
+
+func checkStopOrderSetup(
+	fieldName string,
+	errs Errors,
+	setup *commandspb.StopOrderSetup) Errors {
+	if err := checkOrderSubmission(setup.OrderSubmission); !err.Empty() {
+		errs.Merge(err)
+	} else {
+		if !setup.OrderSubmission.ReduceOnly {
+			errs.AddForProperty(fmt.Sprintf("%s.order_submission.reduce_only", fieldName), ErrMustBeReduceOnly)
+		}
+	}
+
+	if setup.ExpiresAt != nil {
+		if *setup.ExpiresAt < 0 {
+			errs.AddForProperty(fmt.Sprintf("%s.expires_at", fieldName), ErrMustBePositive)
+		}
+		if setup.ExpiryStrategy == nil {
+			errs.AddForProperty(fmt.Sprintf("%s.expiry_strategy", fieldName), ErrExpiryStrategyRequiredWhenExpiresAtSet)
+		} else {
+			if *setup.ExpiryStrategy == types.StopOrder_EXPIRY_STRATEGY_UNSPECIFIED {
+				errs.AddForProperty(fmt.Sprintf("%s.expiry_strategy", fieldName), ErrIsRequired)
+			}
+			if _, ok := types.StopOrder_ExpiryStrategy_name[int32(*setup.ExpiryStrategy)]; !ok {
+				errs.AddForProperty(fmt.Sprintf("%s.expiry_strategy", fieldName), ErrIsNotValid)
+			}
+		}
+	}
+
+	if setup.Trigger != nil {
+		switch t := setup.Trigger.(type) {
+		case *commandspb.StopOrderSetup_Price:
+			if len(t.Price) <= 0 {
+				errs.AddForProperty(fmt.Sprintf("%s.trigger.price", fieldName), ErrIsRequired)
+			} else {
+				if price, ok := big.NewInt(0).SetString(t.Price, 10); !ok {
+					errs.AddForProperty(fmt.Sprintf("%s.trigger.price", fieldName), ErrNotAValidInteger)
+				} else if price.Cmp(big.NewInt(0)) != 1 {
+					errs.AddForProperty(fmt.Sprintf("%s.trigger.price", fieldName), ErrMustBePositive)
+				}
+			}
+		case *commandspb.StopOrderSetup_TrailingPercentOffset:
+			if len(t.TrailingPercentOffset) <= 0 {
+				errs.AddForProperty(fmt.Sprintf("%s.trigger.trailing_percent_offset", fieldName), ErrIsRequired)
+			} else {
+				if poffset, err := num.DecimalFromString(t.TrailingPercentOffset); err != nil {
+					errs.AddForProperty(fmt.Sprintf("%s.trigger.trailing_percent_offset", fieldName), ErrNotAValidFloat)
+				} else if poffset.LessThanOrEqual(num.DecimalZero()) || poffset.GreaterThanOrEqual(num.DecimalOne()) {
+					errs.AddForProperty(fmt.Sprintf("%s.trigger.trailing_percent_offset", fieldName), ErrMustBeWithinRange01)
+				}
+			}
+		}
+	} else {
+		errs.AddForProperty(fmt.Sprintf("%s.trigger", fieldName), ErrMustHaveAStopOrderTrigger)
+	}
+
+	return errs
+}
