a3a493132cd4776b73c69bd4ea412fd1f00469ff
Philip Scott <phil@vega.xyz>
2023-06-14 16:57:07 +0100

    chore: simplify eth call result type

--- core/oracles/mocks/ethcallengine_mock.go
+++ core/oracles/mocks/ethcallengine_mock.go
@@ -8,7 +8,7 @@ import (
 	context "context"
 	reflect "reflect"
 
-	oracles "code.vegaprotocol.io/vega/core/oracles"
+	ethcall "code.vegaprotocol.io/vega/core/evtforward/ethcall"
 	gomock "github.com/golang/mock/gomock"
 )
 
@@ -36,10 +36,10 @@ func (m *MockEthCallEngine) EXPECT() *MockEthCallEngineMockRecorder {
 }
 
 // CallSpec mocks base method.
-func (m *MockEthCallEngine) CallSpec(arg0 context.Context, arg1 string, arg2 uint64) (oracles.EthCallResult, error) {
+func (m *MockEthCallEngine) CallSpec(arg0 context.Context, arg1 string, arg2 uint64) (ethcall.Result, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CallSpec", arg0, arg1, arg2)
-	ret0, _ := ret[0].(oracles.EthCallResult)
+	ret0, _ := ret[0].(ethcall.Result)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
@@ -51,10 +51,10 @@ func (mr *MockEthCallEngineMockRecorder) CallSpec(arg0, arg1, arg2 interface{}) 
 }
 
 // MakeResult mocks base method.
-func (m *MockEthCallEngine) MakeResult(arg0 string, arg1 []byte) (oracles.EthCallResult, error) {
+func (m *MockEthCallEngine) MakeResult(arg0 string, arg1 []byte) (ethcall.Result, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "MakeResult", arg0, arg1)
-	ret0, _ := ret[0].(oracles.EthCallResult)
+	ret0, _ := ret[0].(ethcall.Result)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
