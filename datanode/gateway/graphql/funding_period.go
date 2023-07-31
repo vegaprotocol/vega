81db9f3def628fb93a7911c8673ca87c8c2ab741
Tan Quach <tan@vegaprotocol.io>
2023-07-31 11:13:15 +0100

    chore: make funding periods a top level GQL query

--- datanode/gateway/graphql/funding_period.go
+++ datanode/gateway/graphql/funding_period.go
@@ -3,7 +3,6 @@ package gql
 import (
 	"context"
 
-	v2 "code.vegaprotocol.io/vega/protos/data-node/api/v2"
 	v1 "code.vegaprotocol.io/vega/protos/vega/events/v1"
 )
 
@@ -19,24 +18,6 @@ func (r *fundingPeriodResolver) StartTime(ctx context.Context, obj *v1.FundingPe
 
 func (r *fundingPeriodResolver) EndTime(ctx context.Context, obj *v1.FundingPeriod) (*int64, error) {
 	return obj.End, nil
-}
-
-func (r *fundingPeriodResolver) DataPoints(ctx context.Context, obj *v1.FundingPeriod, dateRange *v2.DateRange, source *v1.FundingPeriodDataPoint_Source,
-	pagination *v2.Pagination,
-) (*v2.FundingPeriodDataPointConnection, error) {
-	req := &v2.ListFundingPeriodDataPointsRequest{
-		MarketId:   obj.MarketId,
-		DateRange:  dateRange,
-		Source:     source,
-		Pagination: pagination,
-	}
-
-	res, err := r.tradingDataClientV2.ListFundingPeriodDataPoints(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-
-	return res.FundingPeriodDataPoints, nil
 }
 
 type fundingPeriodDataPointResolver VegaResolverRoot
