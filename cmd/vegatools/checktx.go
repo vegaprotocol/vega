b1346123e086d3d137526f551ab8553fd5490fba
dalebennett1992 <dalebennett1992@hotmail.co.uk>
2023-08-09 11:15:57 +0100

    test: add capability to check multiple transactions through use of files

--- cmd/vegatools/checktx.go
+++ cmd/vegatools/checktx.go
@@ -1,66 +1,37 @@
 package tools
 
 import (
-	"encoding/base64"
 	"fmt"
+
+	"code.vegaprotocol.io/vega/vegatools/checktx"
 
 	"github.com/sirupsen/logrus"
 
 	"code.vegaprotocol.io/vega/core/config"
-	"code.vegaprotocol.io/vega/libs/proto"
-	commandspb "code.vegaprotocol.io/vega/protos/vega/commands/v1"
 )
 
 type checkTxCmd struct {
 	config.OutputFlag
-	EncodedTransaction string `description:"The encoded transaction string to compare with vega's own encoding" long:"tx" required:"true" short:"t"`
+	EncodedTransaction string `description:"The encoded transaction string to compare with vega's own encoding" long:"tx" short:"t"`
+	TransactionDir     string `description:"directory containing files with encoded transaction data. One encoded transaction per file" long:"tx-dir" short:"d"`
 }
 
 func (opts *checkTxCmd) Execute(_ []string) error {
-	logrus.Infof("checking transaction...")
-	unmarshalledTransaction, err := decodeAndUnmarshalTransaction(opts.EncodedTransaction)
-	if err != nil {
-		return fmt.Errorf("error occurred when attempting to unmarshal the encoded transaction data.\nerr: %w", err)
+	if opts.EncodedTransaction != "" {
+		return checktx.CheckTransaction(opts.EncodedTransaction)
 	}
 
-	logrus.Infof("reencoding the tranaction...")
-	reEncodedTransaction, err := marshalAndEncodeTransaction(unmarshalledTransaction)
-	if err != nil {
-		return fmt.Errorf("error occurred when attempting to marshal and re-encode the transaction")
+	if opts.TransactionDir != "" {
+		result, err := checktx.CheckTransactionsInDirectory(opts.TransactionDir)
+		if err != nil {
+			return fmt.Errorf("there was an issue when checking transactions\nerr: %w", err)
+		}
+
+		logrus.Infof("transactions analysed %d, transactions passed: %d, transactions failed: %d", result.TransactionsAnalysed, result.TransactionsPassed, result.TransactionsFailed)
+		if result.TransactionsFailed > 0 {
+			return fmt.Errorf("one or more transactions failed comparison")
+		}
 	}
 
-	if reEncodedTransaction != opts.EncodedTransaction {
-		logrus.Errorf("transactions not equal!")
-		return fmt.Errorf("the reencoded transaction was not equal. \nOriginal: %s\nVegaEncoded: %s", opts.EncodedTransaction, reEncodedTransaction)
-	}
-
-	logrus.Infof("transactions equal!")
-	return nil
-}
-
-func decodeAndUnmarshalTransaction(encodedTransaction string) (*commandspb.Transaction, error) {
-	unmarshalledTransaction := &commandspb.Transaction{}
-	decodedBytes, err := base64.StdEncoding.DecodeString(encodedTransaction)
-	if err != nil {
-		return unmarshalledTransaction, fmt.Errorf("error occurred when decoding the encoded data.\nerr: %w", err)
-	}
-
-	if err := proto.Unmarshal(decodedBytes, unmarshalledTransaction); err != nil {
-		return unmarshalledTransaction, fmt.Errorf("unable to unmarshal the decided bytes to a transaction: %w", err)
-	}
-
-	logrus.Infof("successfully decoded and unmarshalled the transaction")
-
-	return unmarshalledTransaction, nil
-}
-
-func marshalAndEncodeTransaction(transaction *commandspb.Transaction) (string, error) {
-	pb, err := proto.Marshal(transaction)
-	if err != nil {
-		return "", fmt.Errorf("error when attempting to marshal Transaction struct back to a proto.\nerr: %w", err)
-	}
-
-	reEncodedTransaction := base64.StdEncoding.EncodeToString(pb)
-	logrus.Infof("successfully marshalled to proto and encoded")
-	return reEncodedTransaction, nil
+	return fmt.Errorf("no valid arg provided")
 }
---
+++ vegatools/checktx/check_test.go
@@ -0,0 +1,121 @@
+package checktx
+
+import (
+	"encoding/base64"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/golang/protobuf/jsonpb"
+
+	v1 "code.vegaprotocol.io/vega/protos/vega/commands/v1"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	txVersion   = 3
+	testFileDir = "./testfiles"
+)
+
+func createTestDataFile(t *testing.T, fileName string, encodedTransaction string) {
+	err := os.MkdirAll(testFileDir, 0o755)
+	assert.NoErrorf(t, err, "error occurred when attempting to make a directory for the valid test data")
+
+	filePath := path.Join(testFileDir, fileName)
+
+	err = os.WriteFile(filePath, []byte(encodedTransaction), 0o644)
+	assert.NoErrorf(t, err, "error when creating transaction.json file.\nerr: %v", err)
+}
+
+func clearTestData(t *testing.T) {
+	t.Helper()
+	err := os.RemoveAll(testFileDir)
+	assert.NoErrorf(t, err, "error occurred when attempting to clean valid test data dir")
+}
+
+func TestCheckTransactionsInDirectoryThrowsNoErrAndReturnsAccurateMetrics(t *testing.T) {
+	defer clearTestData(t)
+	encodedTransaction, err := CreatedEncodedTransactionData()
+	assert.NoErrorf(t, err, "error occurred when attempting to create encoded test data")
+
+	createTestDataFile(t, "transaction1.txt", encodedTransaction)
+	createTestDataFile(t, "transaction2.txt", encodedTransaction)
+
+	resultData, err := CheckTransactionsInDirectory(testFileDir)
+
+	assert.Equalf(t, 2, resultData.TransactionsAnalysed, "expected 2 transactions to have been analysed, instead there was %d", resultData.TransactionsAnalysed)
+	assert.Equalf(t, 2, resultData.TransactionsPassed, "expected 2 transactions to have passed, instead there was %d", resultData.TransactionsPassed)
+	assert.Equalf(t, 0, resultData.TransactionsFailed, "expected 0 transactions to have failed, instead there was %d", resultData.TransactionsFailed)
+}
+
+func TestCheckTransactionsInDirectoryThrowsErrIfFileContainsInvalidBase64Data(t *testing.T) {
+	defer clearTestData(t)
+	encodedTransaction, err := CreatedEncodedTransactionData()
+	assert.NoErrorf(t, err, "error occurred when attempting to create encoded test data")
+
+	createTestDataFile(t, "transaction1.txt", "12345")
+	createTestDataFile(t, "transaction2.txt", encodedTransaction)
+
+	resultData, err := CheckTransactionsInDirectory(testFileDir)
+	assert.Errorf(t, err, "expected to exit CheckTransactionsInDirectory with an err when one of the files has invalid data, no error was thrown")
+	assert.Equalf(t, 0, resultData.TransactionsAnalysed, "expected 0 transactions to have been analysed, instead there was %d", resultData.TransactionsAnalysed)
+	assert.Equalf(t, 0, resultData.TransactionsPassed, "expected 0 transactions to have passed, instead there was %d", resultData.TransactionsPassed)
+	assert.Equalf(t, 0, resultData.TransactionsFailed, "expected 0 transactions to have failed, instead there was %d", resultData.TransactionsFailed)
+}
+
+func TestCheckTransactionsInDirectoryAccuratelyReportsFailures(t *testing.T) {
+	defer clearTestData(t)
+	encodedTransaction, err := CreatedEncodedTransactionData()
+	assert.NoErrorf(t, err, "error occurred when attempting to create encoded test data")
+
+	transactionForFailScenario, err := CreateTransaction()
+	assert.NoErrorf(t, err, "error occurred when attempting to create encoded test data")
+
+	marshaller := jsonpb.Marshaler{
+		OrigName:     false,
+		EnumsAsInts:  false,
+		EmitDefaults: false,
+		Indent:       "",
+		AnyResolver:  nil,
+	}
+
+	failScenarioJson, err := marshaller.MarshalToString(transactionForFailScenario)
+	failScenarioNonProtoEncode := base64.StdEncoding.EncodeToString([]byte(failScenarioJson))
+
+	createTestDataFile(t, "transaction1.txt", encodedTransaction)
+	createTestDataFile(t, "transaction2.txt", failScenarioNonProtoEncode)
+
+	resultData, err := CheckTransactionsInDirectory(testFileDir)
+
+	assert.Equalf(t, 2, resultData.TransactionsAnalysed, "expected 2 transactions to have been analysed, instead there was %d", resultData.TransactionsAnalysed)
+	assert.Equalf(t, 1, resultData.TransactionsPassed, "expected 1 transactions to have passed, instead there was %d", resultData.TransactionsPassed)
+	assert.Equalf(t, 1, resultData.TransactionsFailed, "expected 1 transactions to have failed, instead there was %d", resultData.TransactionsFailed)
+}
+
+func TestMarshalAndEncodeTransaction(t *testing.T) {
+	encoded, err := marshalAndEncodeTransaction(&v1.Transaction{})
+	assert.NoError(t, err)
+	assertIsBase64Encoded(t, encoded)
+}
+
+func TestDecodeAndUnmarshalTransactionUnmarshalsEncodedData(t *testing.T) {
+	encodedTransaction, err := CreatedEncodedTransactionData()
+	assert.NoError(t, err)
+
+	unmarshalled, err := decodeAndUnmarshalTransaction(encodedTransaction)
+	assert.NoError(t, err)
+	assert.Equalf(t, v1.TxVersion(txVersion), unmarshalled.Version, "expected version to be set in the unmarshalled data")
+	assert.Equalf(t, TestAlgoName, unmarshalled.Signature.Algo, "algo to be set in the unmarshalled data")
+}
+
+func TestDecodeAndUnmarshalTransactionThrowsErrorWithInvalidData(t *testing.T) {
+	_, err := decodeAndUnmarshalTransaction("invalid")
+	assert.Error(t, err)
+}
+
+func assertIsBase64Encoded(t *testing.T, encodedStr string) {
+	t.Helper()
+	_, err := base64.StdEncoding.DecodeString(encodedStr)
+	assert.NoError(t, err, "Expected the string to be base64 encoded")
+}
