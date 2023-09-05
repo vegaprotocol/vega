6cd62e38742e81527b9449f43585855efd40212c
Elias Van Ootegem <elias@vega.xyz>
2023-09-05 10:40:57 +0100

    test: add step to check the current epoch
    Signed-off-by: Elias Van Ootegem <elias@vega.xyz>

---
+++ core/integration/steps/epoch.go
@@ -0,0 +1,19 @@
+package steps
+
+import (
+	"fmt"
+
+	"code.vegaprotocol.io/vega/core/integration/stubs"
+)
+
+func TheCurrentEpochIs(broker *stubs.BrokerStub, epoch string) error {
+	seq, err := U64(epoch)
+	if err != nil {
+		return err
+	}
+	last := broker.GetCurrentEpoch()
+	if last.Sequence() != seq {
+		return fmt.Errorf("expected current epoch to be %d, instead saw %d", seq, last.Sequence())
+	}
+	return nil
+}
