7fd3c0e624ad9297e6676a82115594d772af6a62
Jeremy Letang <me@jeremyletang.com>
2023-11-16 10:58:32 +0000

    chore: start handling commands / moving funds in colateral engine
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- core/processor/processor.go
+++ core/processor/processor.go
@@ -109,6 +109,10 @@ type ExecutionEngine interface {
 	UpdateMarginMode(ctx context.Context, party, marketID string, marginMode types.MarginMode, marginFactor num.Decimal) error
 	// default chain ID, can be removed once we've upgraded to v0.74
 	OnChainIDUpdate(uint64) error
+
+	SubmitAMM(ctx context.Context, sub *types.SubmitAMM, deterministicID string) error
+	AmendAMM(ctx context.Context, sub *types.AmendAMM) error
+	CancelAMM(ctx context.Context, sub *types.CancelAMM) error
 }
 
 type GovernanceEngine interface {
