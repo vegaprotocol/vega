683cd94e98e06031613de5bee011870dab726774
Peter Barrow <pete@vega.xyz>
2024-01-08 17:52:10 +0000

    feat: Adding datanode changes to support new rejection value

---
+++ datanode/sqlstore/migrations/0069_stop_order_reject_reason_oco_expiry_times_much_not_be_the_same.sql
@@ -0,0 +1,7 @@
+-- +goose Up
+
+ALTER TYPE stop_order_rejection_reason ADD VALUE IF NOT EXISTS 'REJECTION_REASON_STOP_ORDER_CANNOT_HAVE_MATCHING_OCO_EXPIRY_TIMES';
+
+-- +goose Down
+
+-- Do nothing, if it already exists it won't matter and won't be recreated by the up migration.
