7bbaad9d328f15b3177b259584143b76d8024035
gordsport <gordon@vegaprotocol.io>
2024-02-06 16:08:27 +0000

    fix: remove scope rules

--- commitlint.config-ci.js
+++ commitlint.config-ci.js
@@ -3,7 +3,5 @@ const { utils } = require('@commitlint/config-nx-scopes');
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
   rules: {
-    'scope-empty': [2, 'never'],
-    'scope-enum': [],
   },
 };
