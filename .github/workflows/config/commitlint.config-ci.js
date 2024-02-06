33096b07e37bbfe9b7cc4f04429f7d87bfc222c8
gordsport <gordon@vegaprotocol.io>
2024-02-06 21:51:50 +0000

    chore: tidy up config

--- .github/workflows/config/commitlint.config-ci.js
+++ .github/workflows/config/commitlint.config-ci.js
@@ -1,7 +1,5 @@
-const { utils } = require('@commitlint/config-nx-scopes');
-
 module.exports = {
-  extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
   },
 };
