20c726a9042d54808ca71969933811ca7a56a2f3
gordsport <gordon@vegaprotocol.io>
2024-02-06 16:06:09 +0000

    fix: make scope optional

--- commitlint.config-ci.js
+++ commitlint.config-ci.js
@@ -4,10 +4,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
   rules: {
     'scope-empty': [2, 'never'],
-    'scope-enum': async (ctx) => [
-      2,
-      'always',
-      ['ci', 'docs', 'specs', ...(await utils.getProjects(ctx))],
-    ],
+    'scope-enum': [],
   },
 };
