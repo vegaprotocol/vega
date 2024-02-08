5918c50f8140bb29b181e09dcacf097991d0eed7
daniel1302 <daniel.1302@gmail.com>
2024-02-08 22:54:45 +0100

    feat: add vega datanode migrate-ipfs subcommand

---
+++ datanode/networkhistory/ipfs/ipfsfetcher.go
@@ -0,0 +1,90 @@
+package ipfs
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"path"
+	"strings"
+	"time"
+
+	kuboClient "github.com/ipfs/kubo/client/rpc"
+	"github.com/ipfs/kubo/repo/fsrepo/migrations"
+)
+
+const (
+	shellUpTimeout    = 2 * time.Second
+	defaultFetchLimit = 1024 * 1024 * 512
+
+	// Local IPFS API
+	apiFile = "api"
+)
+
+type ipfsFetcher struct {
+	distPath string
+	ipfsDir  string
+	limit    int64
+}
+
+// newIpfsFetcher creates a new IpfsFetcher
+//
+// Specifying "" for distPath sets the default IPNS path.
+// Specifying 0 for fetchLimit sets the default, -1 means no limit.
+func newIpfsFetcher(distPath string, ipfsDir string, fetchLimit int64) *ipfsFetcher {
+	f := &ipfsFetcher{
+		limit:    defaultFetchLimit,
+		distPath: migrations.LatestIpfsDist,
+		ipfsDir:  ipfsDir,
+	}
+
+	if distPath != "" {
+		if !strings.HasPrefix(distPath, "/") {
+			distPath = "/" + distPath
+		}
+		f.distPath = distPath
+	}
+
+	if fetchLimit != 0 {
+		if fetchLimit == -1 {
+			fetchLimit = 0
+		}
+		f.limit = fetchLimit
+	}
+
+	return f
+}
+
+func (f *ipfsFetcher) Close() error {
+	return nil
+}
+
+// Fetch attempts to fetch the file at the given path, from the distribution
+// site configured for this HttpFetcher.  Returns io.ReadCloser on success,
+// which caller must close.
+func (f *ipfsFetcher) Fetch(ctx context.Context, filePath string) ([]byte, error) {
+	sh, err := kuboClient.NewPathApi(f.ipfsDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a ipfs shell migration: %w", err)
+	}
+	resp, err := sh.Request("cat", path.Join(f.distPath, filePath)).Send(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file from the ipfs node: %w", err)
+	}
+	if resp.Error != nil {
+		return nil, resp.Error
+	}
+	defer resp.Close()
+
+	var output io.Reader
+	if f.limit != 0 {
+		output = migrations.NewLimitReadCloser(resp.Output, f.limit)
+	} else {
+		output = resp.Output
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(output)
+
+	return buf.Bytes(), nil
+}
---
+++ datanode/networkhistory/ipfs/migration.go
@@ -0,0 +1,64 @@
+package ipfs
+
+import (
+	"context"
+	"fmt"
+
+	"code.vegaprotocol.io/vega/logging"
+	"github.com/ipfs/kubo/repo/fsrepo"
+	"github.com/ipfs/kubo/repo/fsrepo/migrations"
+)
+
+func createFetcher(distPath string, ipfsDir string) migrations.Fetcher {
+	const userAgent = "fs-repo-migrations"
+
+	if distPath == "" {
+		distPath = migrations.GetDistPathEnv(migrations.LatestIpfsDist)
+	}
+
+	return migrations.NewMultiFetcher(
+		newIpfsFetcher(distPath, ipfsDir, 0),
+		migrations.NewHttpFetcher(distPath, "", userAgent, 0))
+}
+
+// LatestSupportedVersion returns the latest version supported by the kubo library
+func latestSupportedVersion() int {
+	// TODO: Maybe We should hardcode it to be safe and control when the migration happens?
+	return fsrepo.RepoVersion
+}
+
+// IsMigrationNeeded check if migration of the IPFS repository is needed
+func isMigrationNeeded(ipfsDir string) (bool, error) {
+	repoVersion, err := migrations.RepoVersion(ipfsDir)
+	if err != nil {
+		return false, fmt.Errorf("failed to check version for the %s IPFS repository: %w", ipfsDir, err)
+	}
+
+	return repoVersion < latestSupportedVersion(), nil
+}
+
+func MigrateIpfsStorageVersion(log *logging.Logger, ipfsDir string) error {
+	isMigrationNeeded, err := isMigrationNeeded(ipfsDir)
+	if err != nil {
+		return fmt.Errorf("failed to check if the ipfs migration is needed: %w", err)
+	}
+	if !isMigrationNeeded {
+		if log != nil {
+			log.Info("The IPFS for the network-history is up to date. Migration not needed")
+		}
+		return nil
+	}
+
+	localIpfsDir, err := migrations.IpfsDir(ipfsDir)
+	if err != nil {
+		return fmt.Errorf("failed to find local ipfs directory: %w", err)
+	}
+
+	fetcher := createFetcher("", localIpfsDir)
+	err = migrations.RunMigration(context.Background(), fetcher, latestSupportedVersion(), localIpfsDir, false)
+	if err != nil {
+		return fmt.Errorf("failed to execute the ipfs migration: %w", err)
+	}
+
+	return nil
+}
