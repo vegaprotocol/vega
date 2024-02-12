e40ccce7f108308bbe78ac76a454e130c77e4f71
Jeremy Letang <me@jeremyletang.com>
2024-02-12 18:37:38 +0000

    fix: ensure to set chainID in ethcall event in l2_verifier snapshot
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- core/datasource/external/ethcall/engine.go
+++ core/datasource/external/ethcall/engine.go
@@ -298,7 +298,7 @@ func (e *Engine) Poll(ctx context.Context, wallTime time.Time) {
 			if call.triggered(prevEthBlock, nextEthBlockIsh) {
 				res, err := call.Call(ctx, e.client, nextEthBlock.Number.Uint64())
 				if err != nil {
-					e.log.Error("failed to call contract", logging.Error(err))
+					e.log.Error("failed to call contract", logging.Error(err), logging.Uint64("chain-id", e.chainID))
 					event := makeErrorChainEvent(err.Error(), specID, nextEthBlockIsh, e.chainID)
 					e.forwarder.ForwardFromSelf(event)
 					continue
