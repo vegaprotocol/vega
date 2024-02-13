2ee594ee41b61c239000fecb868f28a060a86f34
Karel Moravec <moravec.wdd@gmail.com>
2024-02-13 19:55:18 +0100

    feat: fix genesis test

--- core/netparams/keys.go
+++ core/netparams/keys.go
@@ -317,6 +317,7 @@ var AllKeys = map[string]struct{}{
 	MarketLiquiditySLANonPerformanceBondPenaltyMax:               {},
 	MarketLiquidityStakeToCCYVolume:                              {},
 	MarketLiquidityProvidersFeeCalculationTimeStep:               {},
+	MarketLiquidityEquityLikeShareFeeFraction:                    {},
 	MarketTargetStakeTimeWindow:                                  {},
 	MarketTargetStakeScalingFactor:                               {},
 	MarketPriceMonitoringDefaultParameters:                       {},
