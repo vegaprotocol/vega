2c07a8ae1ccc4432d5d4a84a2353b7a65d03a8cc
Karel Moravec <moravec.wdd@gmail.com>
2024-02-13 19:08:10 +0100

    feat: fix tests and use sub accounts

--- core/collateral/engine.go
+++ core/collateral/engine.go
@@ -3575,6 +3575,11 @@ func (e *Engine) CreatePartyAMMsSubAccounts(
 		return nil, nil, err
 	}
 
+	_, err = e.CreatePartyLiquidityFeeAccount(ctx, subAccount, market, asset)
+	if err != nil {
+		return nil, nil, err
+	}
+
 	return e.accs[generalID].Clone(), e.accs[marginID].Clone(), nil
 }
 
