292f345351437717d007bc529562850319d1f11b
Elias Van Ootegem <elias@vega.xyz>
2024-02-16 15:25:21 +0000

    chore: linter
    Signed-off-by: Elias Van Ootegem <elias@vega.xyz>

--- core/execution/common/amm_score_stake.go
+++ core/execution/common/amm_score_stake.go
@@ -1,3 +1,18 @@
+// Copyright (C) 2023 Gobalsky Labs Limited
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package common
 
 import "code.vegaprotocol.io/vega/libs/num"
@@ -53,7 +68,7 @@ func (a *AMMState) IncrementTick() {
 	a.ltD.Add(num.DecimalOne())
 }
 
-// StartEpoch resets the internal tick counter, ready for the new epoch to start
+// StartEpoch resets the internal tick counter, ready for the new epoch to start.
 func (a *AMMState) StartEpoch() {
 	a.lastTick = 0
 	a.ltD = num.DecimalZero()
