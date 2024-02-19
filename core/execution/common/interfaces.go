9436ecdba5d6afb99393f22b8275f3d625cde5ad
wwestgarth <william@vega.xyz>
2024-02-19 13:34:58 +0000

    fix: return both ledger entries during subaccount-release so we can send them to the data-node

--- core/execution/common/interfaces.go
+++ core/execution/common/interfaces.go
@@ -197,7 +197,7 @@ type Collateral interface {
 	SubAccountRelease(
 		ctx context.Context,
 		party, subAccount, asset, market string, pos events.MarketPosition,
-	) (*types.LedgerMovement, events.Margin, error)
+	) ([]*types.LedgerMovement, events.Margin, error)
 	CreatePartyAMMsSubAccounts(
 		ctx context.Context,
 		party, subAccount, asset, market string,
