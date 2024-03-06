7f9b1365be7a5d10e16d6f63c36dfcf5eae8c882
Elias Van Ootegem <elias@vega.xyz>
2024-03-06 10:32:38 +0000

    fix: reduce-only should only be cancelled next MTM when position hits zero
    Signed-off-by: Elias Van Ootegem <elias@vega.xyz>

--- core/execution/amm/engine_test.go
+++ core/execution/amm/engine_test.go
@@ -475,6 +475,7 @@ func testClosingReduceOnlyPool(t *testing.T) {
 	mevt, err := tst.engine.CancelAMM(ctx, cancel)
 	require.NoError(t, err)
 	assert.Nil(t, mevt) // no closeout necessary so not event
+	tst.engine.OnMTM(ctx)
 	assert.Len(t, tst.engine.pools, 0)
 }
 
@@ -514,6 +515,7 @@ func testAmendMakesClosingPoolActive(t *testing.T) {
 	closeout, err := tst.engine.CancelAMM(ctx, cancel)
 	require.NoError(t, err)
 	assert.Nil(t, closeout)
+	tst.engine.OnMTM(ctx)
 	assert.Len(t, tst.engine.pools, 1)
 	assert.True(t, tst.engine.poolsCpy[0].closing())
 
@@ -544,6 +546,7 @@ func testClosingPoolRemovedWhenPositionZero(t *testing.T) {
 	closeout, err := tst.engine.CancelAMM(ctx, cancel)
 	require.NoError(t, err)
 	assert.Nil(t, closeout)
+	tst.engine.OnMTM(ctx)
 	assert.True(t, tst.engine.poolsCpy[0].closing())
 
 	// position is lower but non-zero
