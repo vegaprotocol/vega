537de178180f20cd6f5e9d2871c97651e52dd7bd
Elias Van Ootegem <elias@vega.xyz>
2024-04-08 17:15:59 +0100

    fix: network should not reduce its position during auction
    Signed-off-by: Elias Van Ootegem <elias@vega.xyz>

--- core/execution/future/liquidation.go
+++ core/execution/future/liquidation.go
@@ -26,6 +26,9 @@ import (
 )
 
 func (m *Market) checkNetwork(ctx context.Context, now time.Time) error {
+	if m.as.InAuction() {
+		return nil
+	}
 	// debug
 	// this only returns an error if we couldn't get the price range, incidating no orders on book
 	order, _ := m.liquidation.OnTick(ctx, now)
