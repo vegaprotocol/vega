ab68be45593a090212fb55925a7ed870d8061c6b
wwestgarth <william@vega.xyz>
2024-09-09 08:53:29 +0100

    exp: don't refresh materialised view, see how quick fuzz tests would be

--- datanode/broker/sqlstore_broker.go
+++ datanode/broker/sqlstore_broker.go
@@ -272,10 +272,10 @@ func (b *SQLStoreBroker) processBlock(ctx context.Context, dbContext context.Con
 				// so we need to call the refresh materialized views function here.
 				if refreshMaterializedViews {
 					// We need to refresh the materialized views as we have reached the end of an epoch
-					err = b.transactionManager.RefreshMaterializedViews(blockCtx)
-					if err != nil {
-						return nil, fmt.Errorf("failed to refresh materialized views:%w", err)
-					}
+					//err = b.transactionManager.RefreshMaterializedViews(blockCtx)
+					//if err != nil {
+					//	return nil, fmt.Errorf("failed to refresh materialized views:%w", err)
+					//}
 					refreshMaterializedViews = false
 				}
 
